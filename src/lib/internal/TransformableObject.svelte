2092e8a293d3d966eb7463f5d108b5b906645a44
Grischa Erbe <grischa.erbe@googlemail.com>
2022-01-16 22:38:45 +0100

    lookAt bugfix

--- src/lib/internal/TransformableObject.svelte
+++ src/lib/internal/TransformableObject.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import type { Object3D } from 'three'
+	import { useThrelte } from '../hooks/useThrelte'
 	import { defaults } from '../lib/defaults'
 	import type { PositionProp, RotationProp, ScaleProp } from '../lib/types'
-	import { useThrelte } from '../hooks/useThrelte'
 
 	export let object: Object3D
 	export let position: PositionProp
@@ -11,42 +11,47 @@
 	export let lookAt: PositionProp
 
 	const { render } = useThrelte()
-
-	$: if (position) {
-		object.position.set(
-			position.x ?? defaults.position.x,
-			position.y ?? defaults.position.y,
-			position.z ?? defaults.position.z
-		)
-		render('TransformableObject: position')
-	}
-	$: if (scale) {
-		if (typeof scale === 'number') {
-			object.scale.set(scale, scale, scale)
-		} else {
-			object.scale.set(
-				scale.x ?? defaults.scale.x,
-				scale.y ?? defaults.scale.y,
-				scale.z ?? defaults.scale.z
+	$: {
+		if (position) {
+			object.position.set(
+				position.x ?? defaults.position.x,
+				position.y ?? defaults.position.y,
+				position.z ?? defaults.position.z
 			)
+			render('TransformableObject: position')
+		}
+		if (lookAt && !rotation) {
+			object.lookAt(
+				lookAt.x ?? defaults.position.x,
+				lookAt.y ?? defaults.position.y,
+				lookAt.z ?? defaults.position.z
+			)
+			render('TransformableObject: lookAt')
+		}
+	}
+	$: {
+		if (scale) {
+			if (typeof scale === 'number') {
+				object.scale.set(scale, scale, scale)
+			} else {
+				object.scale.set(
+					scale.x ?? defaults.scale.x,
+					scale.y ?? defaults.scale.y,
+					scale.z ?? defaults.scale.z
+				)
+			}
 		}
 		render('TransformableObject: scale')
 	}
-	$: if (rotation) {
-		object.rotation.set(
-			rotation.x ?? defaults.rotation.x,
-			rotation.y ?? defaults.rotation.y,
-			rotation.z ?? defaults.rotation.z,
-			rotation.order ?? defaults.rotation.order
-		)
+	$: {
+		if (rotation) {
+			object.rotation.set(
+				rotation.x ?? defaults.rotation.x,
+				rotation.y ?? defaults.rotation.y,
+				rotation.z ?? defaults.rotation.z,
+				rotation.order ?? defaults.rotation.order
+			)
+		}
 		render('TransformableObject: rotation')
-	}
-	$: if (lookAt) {
-		object.lookAt(
-			lookAt.x ?? defaults.position.x,
-			lookAt.y ?? defaults.position.y,
-			lookAt.z ?? defaults.position.z
-		)
-		render('TransformableObject: lookAt')
 	}
 </script>
