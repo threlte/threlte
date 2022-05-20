ac6d090699ca757900b04232f80c18490352a151
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-05-20 17:55:52 +0200

    brigher intro scene

--- src/examples/Introduction/Scene.svelte
+++ src/examples/Introduction/Scene.svelte
@@ -36,4 +36,4 @@
 
 <DirectionalLight shadow position={{ x: 3, y: 10, z: 10 }} />
 <DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
-<AmbientLight intensity={0.2} />
+<AmbientLight intensity={0.5} />
