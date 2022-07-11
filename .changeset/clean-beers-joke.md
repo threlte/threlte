0c4045686e1d93e70b15a0046b725595e068566c
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-07-11 10:58:33 +0200

    adde clenaup scripts

---
+++ .changeset/clean-beers-joke.md
@@ -0,0 +1,6 @@
+---
+'@threlte/core': patch
+'@threlte/extras': patch
+---
+
+added scripts to clean up packages
---
+++ packages/extras/scripts/cleanupPackage.ts
@@ -0,0 +1,27 @@
+import { readFileSync, writeFileSync } from 'fs'
+
+/**
+ * This script cleans up the package to not include
+ * a .publishConfig property and to set the proper exports
+ */
+const cleanPackage = () => {
+  const packagedJson = JSON.parse(
+    readFileSync('./package/package.json', {
+      encoding: 'utf-8'
+    })
+  )
+  const originalJson = JSON.parse(
+    readFileSync('./package.json', {
+      encoding: 'utf-8'
+    })
+  )
+
+  delete packagedJson.publishConfig
+  packagedJson.exports = originalJson.publishConfig.exports
+
+  writeFileSync('./package/package.json', JSON.stringify(packagedJson, null, 2), {
+    encoding: 'utf-8'
+  })
+}
+
+cleanPackage()
