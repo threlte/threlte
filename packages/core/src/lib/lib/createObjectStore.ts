e93efbdaa62df28479ff7beec9071682d9d9368f
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-07-26 15:47:51 +0200

    Bugfix: component <HierarchicalObject> didn't update parent attachment on object change

--- packages/core/src/lib/lib/createObjectStore.ts
+++ packages/core/src/lib/lib/createObjectStore.ts
@@ -1,9 +1,10 @@
 import { onDestroy } from 'svelte'
-import { writable, type Writable } from 'svelte/store'
-import type { Object3D } from 'three'
+import { writable, type Updater, type Writable } from 'svelte/store'
 
 /**
  * A store that only ever updates if the objects are actually different.
+ * Accepts any object that has a property "uuid" (i.e. most Three.js objects).
+ *
  * ### Example
  *
  * ```ts
@@ -11,25 +12,45 @@ import type { Object3D } from 'three'
  * store.set(sameObject) // will not update
  * store.set(otherObject) // will update
  * ```
+ *
  * @param object
  * @returns store
  */
-export function createObjectStore<T extends Object3D | undefined>(object: T): Writable<T>
-export function createObjectStore<T extends Object3D>(object: T): Writable<T>
-export function createObjectStore<T extends Object3D | undefined>(object: T): Writable<T> {
+export function createObjectStore<T extends { uuid: string } | undefined>(
+  object: T,
+  onChange?: (newObject: T, oldObject: T) => void
+): Writable<T>
+export function createObjectStore<T extends { uuid: string }>(
+  object: T,
+  onChange?: (newObject: T, oldObject: T) => void
+): Writable<T>
+export function createObjectStore<T extends { uuid: string } | undefined>(
+  object: T,
+  onChange?: (newObject: T, oldObject: T) => void
+): Writable<T> {
   const objectStore = writable(object)
   let unwrappedObject = object
   const unsubscribeObjectStore = objectStore.subscribe((o) => (unwrappedObject = o))
   onDestroy(unsubscribeObjectStore)
 
   const set = (newObject: T) => {
-    if (!newObject || !unwrappedObject) return objectStore.set(newObject)
-    if (newObject.uuid === unwrappedObject.uuid) return
+    if (newObject?.uuid === unwrappedObject?.uuid) return
+    const oldObject = unwrappedObject
     objectStore.set(newObject)
+    onChange?.(newObject, oldObject)
+  }
+
+  const update = (callback: Updater<T>) => {
+    const newObject = callback(unwrappedObject)
+    if (newObject?.uuid === unwrappedObject?.uuid) return
+    const oldObject = unwrappedObject
+    objectStore.set(newObject)
+    onChange?.(newObject, oldObject)
   }
 
   return {
     ...objectStore,
-    set
+    set,
+    update
   }
 }
