b00b44c8a2eb6bad8950668e714d09aa3e1f9a96
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-08-14 11:19:41 +0200

    why is this not working…

--- packages/extras/src/lib/components/ContactShadows/ContactShadows.svelte
+++ packages/extras/src/lib/components/ContactShadows/ContactShadows.svelte
@@ -95,6 +95,8 @@
         }
       }
 
+      console.log(shader.fragmentShader)
+
       shader.fragmentShader = 'uniform vec3 uColor;\n' + shader.fragmentShader
 
       shader.fragmentShader = shader.fragmentShader.replace(
@@ -107,6 +109,8 @@
         'vec4(vec3(1.0-fragCoordZ),opacity);',
         'vec4(uColor,(1.0-fragCoordZ)*1.0);'
       )
+
+      console.log(shader.fragmentShader)
     }
     return dm
   })
