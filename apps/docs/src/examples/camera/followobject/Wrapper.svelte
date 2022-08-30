eaf580fe96246f414d135533ec86242017cace5c
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-31 10:34:24 +1200

    create follow object example

---
+++ apps/docs/src/examples/camera/followobject/Wrapper.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import { World } from "@threlte/rapier";
+	import Scene from './Scene.svelte'
+</script>
+
+<div class="relative h-full w-full">
+	<Canvas>
+		<World>
+			<Scene />
+		</World>
+	</Canvas>
+	<div class="pointer-events-none h-full w-full absolute top-0">
+		<div class="flex flex-col">
+			<span class="m-2">Use the 'wasd' keys to move around</span>
+		</div>
+	</div>
+</div>
