430b06f1f0c03d75a50f063c841463efddcb0d9d
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-31 11:05:54 +1200

    fmt and make spheres rounder

--- apps/docs/src/examples/camera/followobject/Character.svelte
+++ apps/docs/src/examples/camera/followobject/Character.svelte
@@ -1,96 +1,96 @@
 <script>
-	import { Euler, Vector3, SphereBufferGeometry, MeshStandardMaterial } from 'three';
-	import { useFrame, useThrelte, PerspectiveCamera, Group, Mesh } from '@threlte/core';
-	import { RigidBody, CollisionGroups, Collider } from '@threlte/rapier';
-	import { GLTF, useGltfAnimations } from '@threlte/extras';
-	import { createEventDispatcher } from 'svelte';
-	import Controller from './ThirdPersonControls.svelte';
+	import { Euler, Vector3, SphereBufferGeometry, MeshStandardMaterial } from 'three'
+	import { useFrame, useThrelte, PerspectiveCamera, Group, Mesh } from '@threlte/core'
+	import { RigidBody, CollisionGroups, Collider } from '@threlte/rapier'
+	import { GLTF, useGltfAnimations } from '@threlte/extras'
+	import { createEventDispatcher } from 'svelte'
+	import Controller from './ThirdPersonControls.svelte'
 
-	export let position = { x: 0, y: 0, z: 0 };
-	export let playerCollisionGroups = [0];
-	export let groundCollisionGroups = [15];
-	export let radius = 0.3;
-	export let height = 1.7;
-	export let speed = 6;
-	export let jumpStrength = 4;
+	export let position = { x: 0, y: 0, z: 0 }
+	export let playerCollisionGroups = [0]
+	export let groundCollisionGroups = [15]
+	export let radius = 0.3
+	export let height = 1.7
+	export let speed = 6
+	export let jumpStrength = 4
 
-	const name = 'Xbot';
-	const url = 'https://threejs.org/examples/models/gltf/Xbot.glb';
+	const name = 'Xbot'
+	const url = 'https://threejs.org/examples/models/gltf/Xbot.glb'
 
 	const { gltf, actions } = useGltfAnimations(({ actions }) => {
 		// socerer actions: Armature|mixamo.com|Layer0, Attack, Dance, Death, Idle, Run, Walk
 		// paladin actions: Attack, Dance, Death, Idle, Run, Walk
 		// zombie actions: Attack, Death, Idle, Walk
 		// Xbot actions: agree, headShake, idle, run, sad_pose, sneak_pose, walk
-		actions[currentAction]?.play();
-	});
-	let scene;
-	let currentAction = 'idle';
+		actions[currentAction]?.play()
+	})
+	let scene
+	let currentAction = 'idle'
 
-	let forward = 0;
-	let backward = 0;
-	let left = 0;
-	let right = 0;
+	let forward = 0
+	let backward = 0
+	let left = 0
+	let right = 0
 
-	let rigidBody;
+	let rigidBody
 
-	const { renderer } = useThrelte();
-	if (!renderer) throw new Error();
+	const { renderer } = useThrelte()
+	if (!renderer) throw new Error()
 
-	const temp = new Vector3();
-	const dispatch = createEventDispatcher();
+	const temp = new Vector3()
+	const dispatch = createEventDispatcher()
 
-	let grounded = false;
-	$: grounded ? dispatch('groundenter') : dispatch('groundexit');
+	let grounded = false
+	$: grounded ? dispatch('groundenter') : dispatch('groundexit')
 
 	useFrame(() => {
-		if (!rigidBody || !scene) return;
+		if (!rigidBody || !scene) return
 		// get direction
-		const velVec = temp.fromArray([0, 0, forward - backward]);// left - right
+		const velVec = temp.fromArray([0, 0, forward - backward]) // left - right
 
 		// // update camera is based on character so we rotation character first
 		// rotationQuat.setFromAxisAngle(axis, -rotateDelta.x * rotateSpeed * delta)
 		// object.quaternion.multiply(rotationQuat)
 
 		// sort rotate and multiply by speed
-		velVec.applyEuler(new Euler().copy(scene.rotation)).multiplyScalar(speed);
+		velVec.applyEuler(new Euler().copy(scene.rotation)).multiplyScalar(speed)
 
 		// don't override falling velocity
-		const linVel = rigidBody.linvel();
-		temp.y = linVel.y;
+		const linVel = rigidBody.linvel()
+		temp.y = linVel.y
 		// finally set the velocities and wake up the body
-		rigidBody.setLinvel(temp, true);
+		rigidBody.setLinvel(temp, true)
 
 		// when body position changes update camera position
-		const pos = rigidBody.translation();
-		position = { x: pos.x, y: pos.y, z: pos.z };
-	});
+		const pos = rigidBody.translation()
+		position = { x: pos.x, y: pos.y, z: pos.z }
+	})
 
 	/** @param {KeyboardEvent} e */
 	function onKeyDown(e) {
 		switch (e.key) {
 			case 's':
-				backward = 1;
-				break;
+				backward = 1
+				break
 			case 'w':
-				forward = 1;
-				break;
+				forward = 1
+				break
 			case 'a':
-				left = 1;
-				break;
+				left = 1
+				break
 			case 'd':
-				right = 1;
-				break;
+				right = 1
+				break
 			case ' ':
-				if (!rigidBody || !grounded) break;
-				rigidBody.applyImpulse({ x: 0, y: jumpStrength, z: 0 }, true);
-				break;
+				if (!rigidBody || !grounded) break
+				rigidBody.applyImpulse({ x: 0, y: jumpStrength, z: 0 }, true)
+				break
 			default:
-				break;
+				break
 		}
 
 		if (forward + backward > 0) {
-			transitionTo('run', 0.2);
+			transitionTo('run', 0.2)
 		}
 	}
 
@@ -98,40 +98,40 @@
 	function onKeyUp(e) {
 		switch (e.key) {
 			case 's':
-				backward = 0;
-				break;
+				backward = 0
+				break
 			case 'w':
-				forward = 0;
-				break;
+				forward = 0
+				break
 			case 'a':
-				left = 0;
-				break;
+				left = 0
+				break
 			case 'd':
-				right = 0;
-				break;
+				right = 0
+				break
 			default:
-				break;
+				break
 		}
 
 		if (forward + backward == 0) {
-			transitionTo('idle', 0.2);
+			transitionTo('idle', 0.2)
 		}
 	}
 
 	function transitionTo(newAction, duration = 1) {
-		const action = $actions[currentAction];
-		const nextAction = $actions[newAction];
-		if (action === nextAction) return;
+		const action = $actions[currentAction]
+		const nextAction = $actions[newAction]
+		if (action === nextAction) return
 		// Function inspired by: https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_blending.html
-		nextAction.enabled = true;
+		nextAction.enabled = true
 
 		if (action) {
-			action.crossFadeTo(nextAction, duration, true);
+			action.crossFadeTo(nextAction, duration, true)
 		}
 
 		// Not sure why I need this but the source code does not
-		nextAction.play();
-		currentAction = newAction;
+		nextAction.play()
+		currentAction = newAction
 	}
 </script>
 
@@ -153,22 +153,26 @@
 			on:sensorexit={() => (grounded = false)}
 			shape={'ball'}
 			args={[radius * 1.2]}
-			position={{ y: -height / 2 + radius }} />
+			position={{ y: -height / 2 + radius }}
+		/>
 	</CollisionGroups>
 
 	<Group position={{ y: -height / 2 }}>
 		<GLTF {url} receiveShadow castShadow bind:scene bind:gltf={$gltf}>
 			<Mesh
-				geometry={new SphereBufferGeometry(0.1, 5, 5)}
+				geometry={new SphereBufferGeometry(0.1, 10, 10)}
 				material={new MeshStandardMaterial({ color: 'red' })}
-				position={{ x: 0, y: 1, z: 5 }} />
+				position={{ x: 0, y: 1, z: 5 }}
+			/>
 			<Mesh
-				geometry={new SphereBufferGeometry(0.1, 5, 5)}
+				geometry={new SphereBufferGeometry(0.1, 10, 10)}
 				material={new MeshStandardMaterial({ color: 'green' })}
-				position={{ x: -1, y: 2, z: -3 }} />
+				position={{ x: -1, y: 2, z: -3 }}
+			/>
 			<Mesh
-				geometry={new SphereBufferGeometry(0.1, 5, 5)}
-				material={new MeshStandardMaterial({ color: 'blue' })} />
+				geometry={new SphereBufferGeometry(0.1, 10, 10)}
+				material={new MeshStandardMaterial({ color: 'blue' })}
+			/>
 		</GLTF>
 	</Group>
 </RigidBody>
--- apps/docs/src/examples/camera/followobject/Wrapper.svelte
+++ apps/docs/src/examples/camera/followobject/Wrapper.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core'
-	import { World } from "@threlte/rapier";
+	import { World } from '@threlte/rapier'
 	import Scene from './Scene.svelte'
 </script>
 
