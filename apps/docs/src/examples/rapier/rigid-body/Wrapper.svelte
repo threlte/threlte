7eef19c928da75bab3dc4e580dadf522724a71f2
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-10-19 13:49:44 +1300

    removed button, tailwindcss and extra dom

--- apps/docs/src/examples/rapier/rigid-body/Wrapper.svelte
+++ apps/docs/src/examples/rapier/rigid-body/Wrapper.svelte
@@ -2,27 +2,40 @@
 	import { Canvas } from '@threlte/core'
 	import { HTML } from '@threlte/extras'
 	import { World } from '@threlte/rapier'
-	import Button from './Button.svelte'
 	import { muted } from './Particle.svelte'
 	import Scene from './Scene.svelte'
+	import { useTweakpane } from "./useTweakpane";
+
+	const { addButton, action} = useTweakpane()
+
+	addButton({
+		title: 'toggle sound',
+		onClick: () => (
+			$muted = !$muted
+		)
+	})
+
 </script>
 
-<div class="relative w-full h-full">
-	<div class="flex flex-col gap-2 pointer-events-none items-start m-4 absolute">
-		<Button on:click={() => ($muted = !$muted)}>{$muted ? 'Unmute' : 'Mute'}</Button>
-	</div>
+<div use:action/>
 
-	<Canvas>
-		<World>
-			<Scene />
+<Canvas>
+	<World>
+		<Scene />
 
-			<HTML slot="fallback" transform>
-				<p class="text-xs">
-					It seems your browser<br />
-					doesn't support WASM.<br />
-					I'm sorry.
-				</p>
-			</HTML>
-		</World>
-	</Canvas>
-</div>
+		<HTML slot="fallback" transform>
+			<p>
+				It seems your browser<br />
+				doesn't support WASM.<br />
+				I'm sorry.
+			</p>
+		</HTML>
+	</World>
+</Canvas>
+
+<style>
+	p {
+		font-size: 0.75rem;
+		line-height: 1rem;
+	}
+</style>
