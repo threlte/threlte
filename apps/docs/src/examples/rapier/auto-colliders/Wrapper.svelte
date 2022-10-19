d31a323568ac659b228ac6b2dfa7f4c4a4327bb2
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-10-19 12:49:24 +0200

    Moved auto-colliders example

--- apps/docs/src/examples/rapier/auto-colliders/Wrapper.svelte
+++ apps/docs/src/examples/rapier/auto-colliders/Wrapper.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
+	import { useTweakpane } from './useTweakpane'
 	import { Canvas } from '@threlte/core'
 	import { HTML } from '@threlte/extras'
 	import { Debug, World } from '@threlte/rapier'
-	import Button from './Button.svelte'
 	import Scene from './Scene.svelte'
 
-	let debug = true
+	const { action, addInput } = useTweakpane({
+		title: 'Auto Colliders'
+	})
+
+	const debug = addInput({
+		label: 'Debug',
+		value: true
+	})
 </script>
 
-<div class="relative w-full h-full">
-	<div class="flex flex-col gap-2 pointer-events-none items-start m-4 absolute">
-		<Button on:click={() => (debug = !debug)}>Toggle Debug View</Button>
-	</div>
+<div use:action />
 
-	<Canvas>
-		<World>
-			{#if debug}
-				<Debug />
-			{/if}
+<Canvas>
+	<World>
+		{#if $debug}
+			<Debug />
+		{/if}
 
-			<Scene />
+		<Scene />
 
-			<HTML slot="fallback" transform>
-				<p class="text-xs">
-					It seems your browser<br />
-					doesn't support WASM.<br />
-					I'm sorry.
-				</p>
-			</HTML>
-		</World>
-	</Canvas>
-</div>
+		<HTML slot="fallback" transform>
+			<p class="text-xs">
+				It seems your browser<br />
+				doesn't support WASM.<br />
+				I'm sorry.
+			</p>
+		</HTML>
+	</World>
+</Canvas>
