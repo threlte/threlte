d14f07977446b6977190e207e524c8dfda51b9fc
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-10-31 01:20:24 +0100

    no extending needed

--- apps/docs/src/examples/core/three2-all/App.svelte
+++ apps/docs/src/examples/core/three2-all/App.svelte
@@ -2,11 +2,6 @@
 	import { Canvas } from '@threlte/core'
 	import { extend } from '@threlte/core'
 	import Scene from './Scene.svelte'
-	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
-	extend({
-		OrbitControls
-	})
 </script>
 
 <Canvas>
