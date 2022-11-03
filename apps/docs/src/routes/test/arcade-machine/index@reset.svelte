9d1b0150a0d507185f65fb28c7fbbb73f8337ab9
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-11-03 18:17:45 +0100

    ouch…

---
+++ apps/docs/src/routes/test/arcade-machine/index@reset.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import { World } from '@threlte/rapier'
+	import MainScene from '$examples/core/three-arcade-game/MainScene.svelte'
+	import { browser } from '$app/env'
+</script>
+
+{#if browser}
+	<Canvas>
+		<World gravity={{ x: 0, y: 0, z: 0 }}>
+			<MainScene />
+		</World>
+	</Canvas>
+{/if}
