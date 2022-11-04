eb54ed6b18c05b8a450d2e4ba59113155c68bc8f
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-11-04 14:37:16 +0100

    fi

--- apps/docs/src/routes/arcade-machine/index@reset.svelte
+++ apps/docs/src/routes/arcade-machine/index@reset.svelte
@@ -2,9 +2,11 @@
 	import App from '$examples/core/three-arcade-game/App.svelte'
 </script>
 
-<App />
+<div class="w-screen h-screen bg-black absolute">
+	<App />
 
-<div class="fixed bottom-6 right-6 text-white">
-	Made by <a class="underline" href="https://legrisch.com">Grischa Erbe</a> with
-	<a class="underline" href="https://threlte.xyz">threlte</a>
+	<div class="fixed bottom-6 right-6 text-white">
+		Made by <a class="underline" href="https://legrisch.com">Grischa Erbe</a> with
+		<a class="underline" href="https://threlte.xyz">threlte</a>
+	</div>
 </div>
