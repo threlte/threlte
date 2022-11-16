0fa8d16c30502c58fdeb9c5a6ae2ea1a4739e2ae
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-11-16 14:20:08 +0100

    added overload to useGltfAnimations

--- packages/extras/src/lib/hooks/useGltfAnimations.ts
+++ packages/extras/src/lib/hooks/useGltfAnimations.ts
@@ -1,8 +1,25 @@
 import { onDestroy } from 'svelte'
 import { writable, type Writable } from 'svelte/store'
-import { AnimationMixer, type AnimationAction } from 'three'
+import { AnimationMixer, type AnimationAction, type Object3D } from 'three'
 import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useFrame } from '@threlte/core'
+
+type UseGltfAnimationsReturnType<Actions extends any> = {
+  gltf: Writable<GLTF | undefined>
+  mixer: Writable<AnimationMixer | undefined>
+  actions: Writable<Actions>
+}
+
+type Root = Object3D
+const isRoot = (value: any): value is Root => !!value?.isObject3D
+
+type GltfStore = Writable<GLTF | undefined>
+const isGltfStore = (value: any): value is GltfStore =>
+  !!value?.subscribe && typeof value.subscribe === 'function'
+
+type Callback<Actions extends any> = (args: { actions: Actions; mixer: AnimationMixer }) => void
+const isCallback = <Actions extends any>(value: any): value is Callback<Actions> =>
+  typeof value === 'function'
 
 /**
  * Convenience hook to use animations loaded with a <GLTF> threlte component.
@@ -29,47 +46,52 @@ import { useFrame } from '@threlte/core'
  * @param callback
  * @returns
  */
+export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
+  root?: Root
+): UseGltfAnimationsReturnType<Actions>
+export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
+  gltf: GltfStore,
+  root?: Root
+): UseGltfAnimationsReturnType<Actions>
+export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
+  callback?: Callback<Actions>,
+  root?: Root
+): UseGltfAnimationsReturnType<Actions>
+export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
+  gltf: GltfStore,
+  callback?: Callback<Actions>,
+  root?: Root
+): UseGltfAnimationsReturnType<Actions>
 
 export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
-  callback?: (args: { actions: Actions; mixer: AnimationMixer }) => void
-): {
-  gltf: Writable<GLTF | undefined>
-  mixer: Writable<AnimationMixer | undefined>
-  actions: Writable<Actions>
-}
-export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
-  gltf: Writable<GLTF | undefined>,
-  callback?: (args: { actions: Actions; mixer: AnimationMixer }) => void
-): {
-  gltf: Writable<GLTF | undefined>
-  mixer: Writable<AnimationMixer | undefined>
-  actions: Writable<Actions>
-}
-export function useGltfAnimations<T extends string, Actions = Partial<Record<T, AnimationAction>>>(
-  gltfOrCallback?:
-    | Writable<GLTF | undefined>
-    | ((args: { actions: Actions; mixer: AnimationMixer }) => void),
-  callback?: (args: { actions: Actions; mixer: AnimationMixer }) => void
-): {
-  gltf: Writable<GLTF | undefined>
-  mixer: Writable<AnimationMixer | undefined>
-  actions: Writable<Actions>
-} {
-  const gltf =
-    gltfOrCallback && typeof gltfOrCallback !== 'function'
-      ? gltfOrCallback
-      : writable<GLTF | undefined>(undefined)
+  rootOrGltfOrCallback?: Root | GltfStore | Callback<Actions>,
+  callbackOrRoot?: Callback<Actions> | Root,
+  maybeRoot?: Root
+): UseGltfAnimationsReturnType<Actions> {
+  const gltf = isGltfStore(rootOrGltfOrCallback)
+    ? rootOrGltfOrCallback
+    : writable<GLTF | undefined>(undefined)
 
-  const cb = gltfOrCallback && typeof gltfOrCallback === 'function' ? gltfOrCallback : callback
+  const cb = isCallback(rootOrGltfOrCallback)
+    ? rootOrGltfOrCallback
+    : isCallback(callbackOrRoot)
+    ? callbackOrRoot
+    : undefined
+
+  const root = isRoot(rootOrGltfOrCallback)
+    ? rootOrGltfOrCallback
+    : isRoot(callbackOrRoot)
+    ? callbackOrRoot
+    : maybeRoot
 
   const mixerStore = writable<AnimationMixer | undefined>(undefined)
   const actions = writable<Actions>({} as Actions)
 
   const unsubscribe = gltf.subscribe((gltf) => {
     if (!gltf || !gltf.scene || !gltf.animations.length) return
-    const newMixer = new AnimationMixer(gltf.scene)
+    const newMixer = new AnimationMixer(undefined as unknown as Object3D)
     const newActions = gltf.animations.reduce((acc, clip) => {
-      const action = newMixer.clipAction(clip, newMixer.getRoot())
+      const action = newMixer.clipAction(clip, root ?? gltf.scene)
       return {
         ...acc,
         [clip.name as T]: action
