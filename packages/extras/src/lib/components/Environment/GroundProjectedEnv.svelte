f0c042379fdc395158a116ed43ba1697ec441219
jerzakm <jerzakm@gmail.com>
2022-12-21 01:48:16 +0100

    even more code removed!

--- packages/extras/src/lib/components/Environment/GroundProjectedEnv.svelte
+++ packages/extras/src/lib/components/Environment/GroundProjectedEnv.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
   import { Three } from '@threlte/core'
-  import type { Mesh } from 'three'
   import type { Texture } from 'three'
   import { GroundProjectedEnv } from 'three/examples/jsm/objects/GroundProjectedEnv'
-  import type { EnvironmentProps } from './Environment.svelte'
 
-  type Props = EnvironmentProps
-
-  export let groundProjection: Props['groundProjection']
   export let envMap: Texture
 </script>
 
@@ -15,6 +10,6 @@
   <Three
     type={GroundProjectedEnv}
     args={[envMap]}
-    {...groundProjection}
+    {...$$restProps}
   />
 {/if}
