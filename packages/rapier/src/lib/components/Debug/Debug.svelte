fd0e11b21631b7e015b47b17f276fbf3721531d8
jpaquim <joaopaquim@gmail.com>
2022-12-21 14:56:12 +0000

    Migrate Debug to v5 architecture

--- packages/rapier/src/lib/components/Debug/Debug.svelte
+++ packages/rapier/src/lib/components/Debug/Debug.svelte
@@ -1,41 +1,39 @@
 <script lang="ts">
   import { LineSegments, useFrame } from '@threlte/core'
-  import { onDestroy, } from 'svelte'
-  import type { LineBasicMaterialParameters } from 'three'
+  import { onDestroy } from 'svelte'
   import { BufferAttribute, BufferGeometry, LineBasicMaterial } from 'three'
   import { useRapier } from '../../hooks/useRapier'
+  import type { DebugProps } from './Debug.svelte'
 
-  type $$Props = LineBasicMaterialParameters
-
-  export let vertexColors: $$Props['vertexColors'] = true
+  export let vertexColors: DebugProps['vertexColors'] = true
 
   const material = new LineBasicMaterial({
     vertexColors,
-    ...($$props as $$Props)
+    ...($$props as DebugProps)
   })
 
   const { world, debug } = useRapier()
-  
+
   const buffers = world.debugRender()
   const vertices = new BufferAttribute(buffers.vertices, 3)
   const colors = new BufferAttribute(buffers.colors, 4)
-  
+
   const geometry = new BufferGeometry()
   geometry.setAttribute('position', vertices)
   geometry.setAttribute('color', colors)
-  
+
   debug.set(true)
-  
+
   useFrame(() => {
     const buffers = world.debugRender()
-    
+
     const vertices = new BufferAttribute(buffers.vertices, 3)
     const colors = new BufferAttribute(buffers.colors, 4)
-    
+
     geometry.setAttribute('position', vertices)
     geometry.setAttribute('color', colors)
   })
-  
+
   onDestroy(() => {
     geometry.dispose()
     material.dispose()
@@ -43,4 +41,8 @@
   })
 </script>
 
-<LineSegments renderOrder={Infinity} {geometry} {material} />
+<LineSegments
+  renderOrder={Infinity}
+  {geometry}
+  {material}
+/>
