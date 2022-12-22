bc6aa35d43e0ff267f9331ca4aaab3d0597bc86f
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-12-22 14:04:56 +0100

    adapted <HTML>

--- packages/extras/src/lib/types/components.ts
+++ packages/extras/src/lib/types/components.ts
@@ -21,36 +21,6 @@ export type EdgesProperties = Omit<LineSegmentsProperties, 'geometry' | 'materia
     threshold?: number
   }
 
-export type HTMLProperties = GroupProperties & {
-  transform?: boolean
-  calculatePosition: (
-    obj: Object3D,
-    camera: Camera,
-    size: { width: number; height: number }
-  ) => [number, number]
-  eps: number
-  occlude: boolean | Object3D[]
-  zIndexRange: [number, number]
-  sprite: boolean
-  pointerEvents:
-    | 'auto'
-    | 'none'
-    | 'visiblePainted'
-    | 'visibleFill'
-    | 'visibleStroke'
-    | 'visible'
-    | 'painted'
-    | 'fill'
-    | 'stroke'
-    | 'all'
-    | 'inherit'
-  center: boolean
-  fullscreen: boolean
-  distanceFactor?: number
-  as: keyof HTMLElementTagNameMap
-  portal?: HTMLElement
-}
-
 export type FloatProperties = GroupProperties & {
   speed: number
   rotationIntensity: number
