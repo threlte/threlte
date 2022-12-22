61751a4c9170a3c8278d2d0517f20ac20acada39
jerzakm <jerzakm@gmail.com>
2022-12-22 23:39:39 +0100

    - props optional - T.Group ref passed to slot - $$Props, $$Events, $$Slots

--- packages/extras/src/lib/components/Float/Float.svelte.d.ts
+++ packages/extras/src/lib/components/Float/Float.svelte.d.ts
@@ -3,11 +3,14 @@ import { SvelteComponentTyped } from 'svelte'
 import type { Group } from 'three'
 
 export type FloatProps = Props<Group> & {
-  speed: number | [x: number, y: number, z: number]
-  floatIntensity: number | [x: number, y: number, z: number]
-  floatingRange: [number, number] | [x: [number, number], y: [number, number], z: [number, number]]
-  rotationIntensity: number | [x: number, y: number, z: number]
-  rotationSpeed: number | [x: number, y: number, z: number]
+  speed?: number | [x: number, y: number, z: number]
+  floatIntensity?: number | [x: number, y: number, z: number]
+  floatingRange?: [number, number] | [x: [number, number], y: [number, number], z: [number, number]]
+  rotationIntensity?: number | [x: number, y: number, z: number]
+  rotationSpeed?: number | [x: number, y: number, z: number]
 }
 
-export default class Float extends SvelteComponentTyped<FloatProps, Events<Group>, Slots<Group>> {}
+export type FloatEvents = Events<Group>
+export type FloatSlots = Slots<Group>
+
+export default class Float extends SvelteComponentTyped<FloatProps, FloatEvents, FloatSlots> {}
