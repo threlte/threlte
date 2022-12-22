300cbe4e3548e4e4f61590e734f8fc1d5cad2c63
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-12-22 22:06:52 +0100

    Adapted <Edges> component, adapted examples, removed component types and removed component type export

---
+++ .changeset/dirty-clouds-teach.md
@@ -0,0 +1,5 @@
+---
+'@threlte/extras': patch
+---
+
+Converted <Edges> component to use Threlte v5 syntax. Changed prop name "threshold" to "thresholdAngle" (as it is used by three.js)
--- packages/extras/src/lib/components/Edges/Edges.svelte
+++ packages/extras/src/lib/components/Edges/Edges.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
-  import { DisposableObject, LineSegments, useParent, useThrelte } from '@threlte/core'
-  import { onDestroy } from 'svelte'
-  import { Color, EdgesGeometry, LineBasicMaterial, Mesh } from 'three'
-  import type { EdgesProperties } from '../../types/components'
+  import { T, useParent } from '@threlte/core'
+  import type { Mesh } from 'three'
+  import type { EdgesEvents, EdgesProps, EdgesSlots } from './Edges.svelte'
 
-  export let color: EdgesProperties['color'] = undefined
-  export let material: EdgesProperties['material'] = undefined
-  export let threshold: EdgesProperties['threshold'] = undefined
+  type $$Props = EdgesProps
+  type $$Events = EdgesEvents
+  type $$Slots = EdgesSlots
 
-  // TODO to v5
-
-  // LineInstance
-  export let position: EdgesProperties['position'] = undefined
-  export let scale: EdgesProperties['scale'] = undefined
-  export let rotation: EdgesProperties['rotation'] = undefined
-  export let viewportAware: EdgesProperties['viewportAware'] = false
-  export let inViewport: EdgesProperties['inViewport'] = false
-  export let castShadow: EdgesProperties['castShadow'] = undefined
-  export let receiveShadow: EdgesProperties['receiveShadow'] = undefined
-  export let frustumCulled: EdgesProperties['frustumCulled'] = undefined
-  export let renderOrder: EdgesProperties['renderOrder'] = undefined
-  export let visible: EdgesProperties['visible'] = undefined
-  export let userData: EdgesProperties['userData'] = undefined
-  export let dispose: EdgesProperties['dispose'] = undefined
-  export let interactive: EdgesProperties['interactive'] = false
-  export let ignorePointer: EdgesProperties['ignorePointer'] = false
-  export let lookAt: EdgesProperties['lookAt'] = undefined
+  export let thresholdAngle: $$Props['thresholdAngle']
+  export let color: $$Props['color']
 
   const parent = useParent()
   if (!$parent || $parent.type !== 'Mesh')
@@ -35,89 +18,13 @@
 
   const geometry =
     'clone' in parentMesh.geometry ? parentMesh.geometry.clone() : parentMesh.geometry
-
-  const edgesGeometry = new EdgesGeometry(geometry, threshold)
-
-  const { invalidate } = useThrelte()
-
-  let activeMaterial = !!material
-    ? material
-    : color
-    ? new LineBasicMaterial({
-        color
-      })
-    : new LineBasicMaterial()
-
-  let previousMaterial = material
-  $: {
-    if (previousMaterial !== material) {
-      // material changed
-      if (material) {
-        // there's actually a material, prioritize that
-        activeMaterial = material
-        invalidate('Edges: material changed')
-      }
-      previousMaterial = material
-    }
-  }
-
-  let previousColor = color
-  $: {
-    if (previousColor !== color) {
-      // color changed
-      if (material) break $ // break if there's a material set
-      const mat = activeMaterial as LineBasicMaterial
-      mat.color = new Color(color)
-      invalidate('Edges: color changed')
-      previousColor = color
-    }
-  }
-
-  onDestroy(() => {
-    edgesGeometry.dispose()
-  })
 </script>
 
-<!-- Force disposal: not user-provided -->
-<DisposableObject
-  dispose
-  object={edgesGeometry}
-/>
-
-<DisposableObject
-  {dispose}
-  object={activeMaterial}
-/>
-
-{#if edgesGeometry}
-  <LineSegments
-    geometry={edgesGeometry}
-    material={activeMaterial}
-    {position}
-    {scale}
-    {rotation}
-    {lookAt}
-    {castShadow}
-    {receiveShadow}
-    {frustumCulled}
-    {renderOrder}
-    {visible}
-    {userData}
-    {dispose}
-    {interactive}
-    {ignorePointer}
-    on:click
-    on:contextmenu
-    on:pointerup
-    on:pointerdown
-    on:pointerenter
-    on:pointerleave
-    on:pointermove
-    {viewportAware}
-    bind:inViewport
-    on:viewportenter
-    on:viewportleave
-  >
-    <slot />
-  </LineSegments>
-{/if}
+<T.LineSegments
+  let:ref
+  {...$$restProps}
+>
+  <T.EdgesGeometry args={[geometry, thresholdAngle]} />
+  <T.LineBasicMaterial {color} />
+  <slot {ref} />
+</T.LineSegments>
