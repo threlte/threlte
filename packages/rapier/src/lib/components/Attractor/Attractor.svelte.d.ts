34be0026ef964c642e23f77b345aa57aff393da8
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-12-22 09:22:24 +0100

    adapted Attractor

--- packages/rapier/src/lib/components/Attractor/Attractor.svelte.d.ts
+++ packages/rapier/src/lib/components/Attractor/Attractor.svelte.d.ts
@@ -1,12 +1,21 @@
 import { SvelteComponentTyped } from 'svelte'
 import type { AttractorProperties } from '../../types/components'
+import { Props, type Events, type Slots } from '@threlte/core'
+import type { Group } from 'three'
 
-type AttractorProps = {
-  position: AttractorProperties['position']
+type AttractorProps = Props<Group> & {
   strength: NonNullable<AttractorProperties['strength']>
   range: NonNullable<AttractorProperties['range']>
   gravityType: NonNullable<AttractorProperties['gravityType']>
   gravitationalConstant: NonNullable<AttractorProperties['gravitationalConstant']>
 }
 
-export default class Attractor extends SvelteComponentTyped<AttractorProps> {}
+type AttractorEvents = Events<Group>
+
+type AttractorSlots = Slots<Group>
+
+export default class Attractor extends SvelteComponentTyped<
+  AttractorProps,
+  AttractorEvents,
+  AttractorSlots
+> {}
