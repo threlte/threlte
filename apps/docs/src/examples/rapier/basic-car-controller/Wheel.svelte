3e6f9db556af58bf1cc44187177b986c3b665da1
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2022-12-22 12:54:46 +0100

    adapted basic vehicle controller

--- apps/docs/src/examples/rapier/basic-car-controller/Wheel.svelte
+++ apps/docs/src/examples/rapier/basic-car-controller/Wheel.svelte
@@ -4,16 +4,16 @@
 		type Collider as RapierCollider,
 		type RigidBody as RapierRigidBody
 	} from '@dimforge/rapier3d-compat'
-	import type { Position } from '@threlte/core'
-	import { Mesh } from '@threlte/core'
+	import { T } from '@threlte/core'
 	import { Collider, RigidBody, useRevoluteJoint } from '@threlte/rapier'
-	import { CylinderBufferGeometry, MeshStandardMaterial } from 'three'
+	import type { Vector3 } from 'three'
+	import { CylinderGeometry, MeshStandardMaterial } from 'three'
 	import { DEG2RAD } from 'three/src/math/MathUtils'
 	import { useWasd } from './useWasd'
 
-	export let position: Position | undefined = undefined
+	export let position: Parameters<Vector3['set']>
 	export let parentRigidBody: RapierRigidBody | undefined = undefined
-	export let anchor: Position
+	export let anchor: Parameters<Vector3['set']>
 	let collider: RapierCollider
 	export let isDriven = false
 
@@ -21,7 +21,7 @@
 
 	let isSpaceDown = false
 
-	const { rigidBodyA, rigidBodyB, joint } = useRevoluteJoint(anchor, {}, { z: 1 })
+	const { rigidBodyA, rigidBodyB, joint } = useRevoluteJoint(anchor, [0, 0, 0], [0, 0, 1])
 	$: if (parentRigidBody) rigidBodyA.set(parentRigidBody)
 	$: $joint?.configureMotorModel(MotorModel.AccelerationBased)
 	$: $joint?.configureMotorModel(
@@ -54,14 +54,14 @@
 		shape={'cylinder'}
 		args={[0.12, 0.3]}
 		bind:collider
-		rotation={{ x: 90 * DEG2RAD }}
+		rotation={[90 * DEG2RAD, 0, 0]}
 	/>
 
 	<!-- WHEEL MESH -->
-	<Mesh
+	<T.Mesh
 		castShadow
-		rotation={{ x: 90 * DEG2RAD }}
-		geometry={new CylinderBufferGeometry(0.3, 0.3, 0.24)}
+		rotation.x={90 * DEG2RAD}
+		geometry={new CylinderGeometry(0.3, 0.3, 0.24)}
 		material={new MeshStandardMaterial()}
 	/>
 </RigidBody>
