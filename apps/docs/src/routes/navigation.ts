097a4e5625355fbea933c3538f0b64845ff68bc3
jpaquim <joaopaquim@gmail.com>
2022-12-23 13:23:07 +0000

    Remove CameraInstance docs

--- apps/docs/src/routes/navigation.ts
+++ apps/docs/src/routes/navigation.ts
@@ -157,10 +157,6 @@ export const sidebar = {
 				slug: '/core/mesh-instance'
 			},
 			{
-				title: 'CameraInstance',
-				slug: '/core/camera-instance'
-			},
-			{
 				title: 'AudioInstance',
 				slug: '/core/audio-instance'
 			},
