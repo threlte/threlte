ad545a840c562c9d4400b5b345c670d901be0410
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-01-28 15:59:21 +0100

    new docs prototype

---
+++ apps/docs-new/src/components/Menu/LeftSidebar/LeftSidebar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import Tabs, { type Tab } from '$components/Tabs/Tabs.svelte'
+  import LeftSidebarCategory from './LeftSidebarCategory.svelte'
+  import { persistBrowserSession } from '@macfja/svelte-persistent-store'
+  import { writable } from 'svelte/store'
+
+  export let menu: Record<'learn' | 'reference', LeftSidebarMenu>
+
+  const activeMenu = persistBrowserSession(
+    writable<keyof typeof menu>('learn'),
+    'docs-left-sidebar-active-menu'
+  )
+
+  const onTabSelect = (e: CustomEvent<Tab>) => {
+    $activeMenu = e.detail.id as 'learn' | 'reference'
+  }
+</script>
+
+<nav>
+  <Tabs
+    on:select={onTabSelect}
+    tabs={[
+      {
+        id: 'learn',
+        title: 'Learn'
+      },
+      {
+        id: 'reference',
+        title: 'Reference'
+      }
+    ]}
+    activeTabId={$activeMenu}
+  />
+
+  <ul>
+    {#each menu[$activeMenu].categories as category}
+      <li>
+        <LeftSidebarCategory {category} />
+      </li>
+    {/each}
+  </ul>
+</nav>
---
+++ apps/docs-new/src/components/Tabs/Tabs.svelte
@@ -0,0 +1,38 @@
+<script
+  lang="ts"
+  context="module"
+>
+  export type Tab = {
+    title: string
+    id: string
+  }
+
+  export type Tabs = Tab[]
+</script>
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  import { c } from '$lib/classes'
+
+  export let tabs: Tabs
+  export let activeTabId: (typeof tabs)[number]['id']
+
+  const dispatch = createEventDispatcher<{
+    select: Tab
+  }>()
+
+  let _class = ''
+  export { _class as class }
+</script>
+
+<div class={c('w-full', _class)}>
+  {#each tabs as tab}
+    <button
+      class={c('border-b-2 border-b-transparent', activeTabId === tab.id && 'border-b-black')}
+      on:click={() => {
+        activeTabId === tab.id
+        dispatch('select', tab)
+      }}>{tab.title}</button
+    >
+  {/each}
+</div>
