6a3f487cc20cca66e19d196d5aacd517c8af1ade
jerzakm <jerzakm@gmail.com>
2023-02-10 23:01:36 +0100

    basic "On this page" styling for mobile. bug: loses focus before navigating wip

--- apps/docs-next/src/components/Menu/RightSidebar/RightSidebar.svelte
+++ apps/docs-next/src/components/Menu/RightSidebar/RightSidebar.svelte
@@ -15,6 +15,11 @@
   let intersectionObserver: IntersectionObserver | undefined = undefined
 
   let currentHeadingId: string | undefined = undefined
+  let currentHeading: MarkdownHeading | undefined = headings[0]
+
+  $: {
+    console.log(headings)
+  }
 
   onMount(() => {
     intersectionObserver = new IntersectionObserver(
@@ -23,6 +28,13 @@
           if (entry.isIntersecting) {
             const { id } = entry.target
             currentHeadingId = id
+
+            const newHeading = headings.find((heading) => {
+              return heading.slug == id
+            })
+
+            currentHeading = newHeading || currentHeading
+
             break
           }
         }
@@ -47,25 +59,37 @@
   })
 </script>
 
-<div class="flex flex-col text-xs overflow-auto h-full scrollbar-hide pb-12">
-  <div class="font-bold mb-2 pl-3 ml-[4px] text-sm">On this page</div>
-  <ul>
-    {#each filteredHeadings as heading}
-      <li
-        class={c(
-          'py-0.5 group border-l-4 border-white/20 pl-3 hover:border-white/60 text-faded hover:text-white',
-          !!currentHeadingId &&
-            heading.slug === currentHeadingId &&
-            'bg-orange-500 !text-white !border-white/60'
-        )}
-      >
-        <a
-          data-depth={heading.depth}
-          class={c('group-hover:underline pr-4')}
-          style="margin-left: {(heading.depth - lowestHeadingDepth) * 10}px;"
-          href={`#${heading.slug}`}>{heading.text}</a
+<div
+  class="flex flex-row text-xs overflow-visible h-full scrollbar-hide pb-12 relative pl-12 gap-6 items-center"
+>
+  <button
+    class="group btn items-center overflow-hidden cursor-pointer lg:cursor-default justify-between border border-white/70 rounded-md lg:border-none"
+  >
+    <span class="font-bold mb-2 px-4 lg:px-0 py-2 lg:py-0 lg:pl-3 lg:ml-[4px] text-sm"
+      >On this page</span
+    >
+
+    <ul
+      class="hidden absolute top-1/2 w-1/2 lg:w-full text-left group-active:block group-focus:block lg:block bg-[#0c1421]"
+    >
+      {#each filteredHeadings as heading}
+        <li
+          class={c(
+            'py-0.5 group border-l-4 border-white/20 pl-3 hover:border-white/60 text-faded hover:text-white',
+            !!currentHeadingId &&
+              heading.slug === currentHeadingId &&
+              'bg-orange-500 !text-white !border-white/60'
+          )}
         >
-      </li>
-    {/each}
-  </ul>
+          <a
+            data-depth={heading.depth}
+            class={c('group-hover:underline pr-4 block no-underline')}
+            style="margin-left: {(heading.depth - lowestHeadingDepth) * 10}px;"
+            href={`#${heading.slug}`}>{heading.text}</a
+          >
+        </li>
+      {/each}
+    </ul>
+  </button>
+  <span class="text-base font-bold lg:hidden">{currentHeading?.text}</span>
 </div>
