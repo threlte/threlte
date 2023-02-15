b7fb883c968a9fdce17b42acf8bc0b2d8386561e
jerzakm <jerzakm@gmail.com>
2023-02-15 16:00:18 +0100

    remove temp files

--- apps/docs-next/src/components/TempPrettierTest.svelte
+++
@@ -1,6 +0,0 @@
-<!-- Before -->
-<!-- <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button> -->
-<button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
-
-<!-- After -->
-<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
