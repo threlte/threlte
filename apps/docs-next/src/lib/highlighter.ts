077944be20757d8388db19bb2d98f6ba00341156
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-03-12 18:43:45 +0100

    Centralized Shiki highlighter

---
+++ apps/docs-next/src/lib/highlighter.ts
@@ -0,0 +1,25 @@
+import { getHighlighter, setCDN, type Highlighter } from 'shiki'
+
+setCDN('https://unpkg.com/shiki/')
+
+let highlighter: Promise<Highlighter>
+
+export const getShiki = () => {
+  if (highlighter) return highlighter
+  highlighter = getHighlighter({
+    theme: 'dracula-soft',
+    langs: [
+      'astro',
+      'typescript',
+      'javascript',
+      'svelte',
+      'astro',
+      'markdown',
+      'mdx',
+      'md',
+      'json',
+      'html'
+    ]
+  })
+  return highlighter
+}
