ada44ce90fe2f772e525fa9f8bc7f0a5400c78df
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2023-03-17 11:37:14 +1300

    added small template and adjusted for example markdown and adjusted terrain example.

--- apps/docs-next/src/examples/geometry/terrain/App.svelte
+++ apps/docs-next/src/examples/geometry/terrain/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { Canvas } from '@threlte/core'
-	import Scene from './Scene.svelte'
+  import { Canvas } from '@threlte/core'
+  import { World } from '@threlte/rapier'
+  import Scene from './Scene.svelte'
 </script>
 
 <Canvas>
-	<Scene />
+  <World>
+    <Scene />
+  </World>
 </Canvas>
