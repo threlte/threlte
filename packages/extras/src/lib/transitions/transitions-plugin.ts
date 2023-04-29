e48972558f1365f9b7e18f3f7384abd7b363db5c
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-04-29 23:48:03 +0200

    wording

--- packages/extras/src/lib/transitions/transitions-plugin.ts
+++ packages/extras/src/lib/transitions/transitions-plugin.ts
@@ -94,7 +94,7 @@ export const transitions = () => {
 
     let currentRef = ref
 
-    // fake element
+    // fake HTML Element
     const el = {
       style: {},
       // eslint-disable-next-line @typescript-eslint/no-empty-function
