7751a77e8c5193c52dce15d6a270087126fe5d55
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-05-03 11:37:29 +0200

    Sanity chck

--- packages/extras/src/lib/interactivity/hook.ts
+++ packages/extras/src/lib/interactivity/hook.ts
@@ -14,6 +14,10 @@ export const useInteractivity = () => {
       return
     }
     object.userData._threlte_interactivity_dispatcher = eventDispatcher
+
+    // check if the object is already in the list
+    if (state.interactiveObjects.find((obj) => obj.uuid === object.uuid)) return
+
     state.interactiveObjects.push(object)
   }
 
