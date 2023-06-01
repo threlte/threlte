d9558489ef56e4b3a6c30dc533f25ddc336db21e
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-06-02 00:30:24 +0200

    Added Studio to sheetObject example

--- apps/docs-next/src/examples/theatre/sheet-object-action/App.svelte
+++ apps/docs-next/src/examples/theatre/sheet-object-action/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import { Project, Sequence, Sheet } from '@threlte/theatre'
+  import { Project, Sequence, Sheet, Studio } from '@threlte/theatre'
 
   import Scene from './Scene.svelte'
   import state from './state.json'
 </script>
+
+<Studio />
 
 <Project config={{ state }}>
   <Sheet>
