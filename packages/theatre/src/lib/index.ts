652c87268400a94ca49b7ce2471cb00c141cf9ba
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-06-02 00:10:45 +0200

    I should have saved after refactoring

--- packages/theatre/src/lib/index.ts
+++ packages/theatre/src/lib/index.ts
@@ -4,7 +4,7 @@ export { default as Project } from './project/Project.svelte'
 export { default as Sequence } from './sequence/Sequence.svelte'
 export { default as Sheet } from './sheet/Sheet.svelte'
 export { default as Editable } from './editable/Editable.svelte'
-export { default as SheetObject } from './editable/SheetObject.svelte'
+export { default as SheetObject } from './sheetObject/SheetObject.svelte'
 export { default as Theatre } from './theatre/Theatre.svelte'
 
 // hooks
