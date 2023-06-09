68167b5440147d37d2215b5fa1372555fa57e623
Michael Hutchings <mdhutchings94@gmail.com>
2023-06-09 13:32:44 +0100

    feat: add outline postproc example to docs
    todo:
    - add animation
    - make maze cleaner

---
+++ apps/docs-next/src/examples/postprocessing/outline/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import { useTweakpane } from '../../utils/useTweakpane'
+  import { animationPlaying } from './state'
+
+  // add tweakpane to show or hide the terrain collision mesh
+  const { action, addButton } = useTweakpane()
+
+  addButton({
+    title: 'toggle',
+    label: 'Animation',
+    onClick: () => {
+      $animationPlaying = !$animationPlaying
+    }
+  })
+</script>
+
+<div use:action />
+
+<Canvas>
+  <Scene />
+</Canvas>
---
+++ apps/docs-next/src/examples/postprocessing/outline/Scene.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { PlaneGeometry } from 'three'
+  import { T } from '@threlte/core'
+  import { OrbitControls, Grid, ContactShadows } from '@threlte/extras'
+  import { animationPlaying } from './state'
+  import Maze from './Maze.svelte'
+  import CustomRenderer from './CustomRenderer.svelte'
+
+	let outlinedCube;
+</script>
+
+<Maze />
+
+<T.Mesh position={[1.8,1,1]} bind:ref={outlinedCube}>
+  <T.MeshToonMaterial color="gold" />
+  <T.BoxGeometry />
+</T.Mesh>
+
+<CustomRenderer selectedMesh={outlinedCube} />
+
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[0, 6, -10]}
+  fov={15}
+  zoom={0.2}
+>
+  <OrbitControls enableZoom={true} enableDamping target.y={0} />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight
+  intensity={0.8}
+  position.x={5}
+  position.y={10}
+/>
+<T.AmbientLight intensity={0.2} />
+
+<Grid
+  position.y={-0.001}
+  cellColor="#ffffff"
+  sectionColor="#ffffff"
+  sectionThickness={0}
+  fadeDistance={75}
+  cellSize={2}
+/>
+
+<ContactShadows scale={12} blur={2} far={5} opacity={0.5} />
+
