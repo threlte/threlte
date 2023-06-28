c9509ce580c11c1a11556d03f7c81c4b6c6819c4
Michael Hutchings <mdhutchings94@gmail.com>
2023-06-28 11:26:19 +0100

    fixup terrain rapier physics example

--- apps/docs-next/src/examples/geometry/physics-terrain/App.svelte
+++ apps/docs-next/src/examples/geometry/physics-terrain/App.svelte
@@ -2,10 +2,31 @@
 	import { Canvas } from '@threlte/core'
 	import { World } from '@threlte/rapier'
 	import Scene from './Scene.svelte'
+	import { useTweakpane } from '../../utils/useTweakpane'
+
+	let reset: (() => void) | undefined
+	let toggleDebug: (() => void) | undefined
+
+	const { action, addButton } = useTweakpane()
+
+	addButton({
+		title: 'Reset',
+		onClick: () => {
+			reset?.()
+		}
+	})
+	addButton({
+		title: 'Toggle Debug',
+		onClick: () => {
+			toggleDebug?.()
+		}
+	})
 </script>
+
+<div use:action />
 
 <Canvas>
 	<World>
-		<Scene />
+		<Scene bind:reset bind:toggleDebug />
 	</World>
 </Canvas>
--- apps/docs-next/src/examples/geometry/physics-terrain/Scene.svelte
+++ apps/docs-next/src/examples/geometry/physics-terrain/Scene.svelte
@@ -6,12 +6,12 @@
   import { OrbitControls } from '@threlte/extras'
   import RAPIER from '@dimforge/rapier3d-compat'
   import { Collider, Debug, RigidBody } from '@threlte/rapier'
+  import FallingShapes from './FallingShapes.svelte'
 
   let nsubdivs = 10
-  let heights = generateHeightfield(nsubdivs)
+  let heights = []
 
   const geometry = new PlaneGeometry(10, 10, nsubdivs, nsubdivs)
-  const material = new MeshStandardMaterial()
 
   const noise = createNoise2D()
   const vertices = geometry.getAttribute('position').array
@@ -28,45 +28,20 @@
     }
   }
 
-  let flippedHeights = new Float32Array(heights.length)
-
-  for (let x = 0; x <= nsubdivs; x++) {
-    for (let y = 0; y <= nsubdivs; y++) {
-      const indexOriginal = x + (nsubdivs + 1) * y
-      // const indexFlipped = x * nsubdivs + (11 - y)
-      // console.log(indexFlipped)
-      flippedHeights[indexOriginal] = heights[indexOriginal]
-    }
-  }
-
   // needed for lighting
   geometry.computeVertexNormals()
 
   const scale = new RAPIER.Vector3(10.0, 1, 10)
 
-  function generateHeightfield(nsubdivs: number) {
-    let heights = []
-
-    const rng = Math.random() * 3
-
-    let i, j
-    for (i = 0; i <= nsubdivs; ++i) {
-      for (j = 0; j <= nsubdivs; ++j) {
-        heights.push(rng)
-      }
-    }
-
-    return new Float32Array(heights)
+	let resetCounter = 0
+  export const reset = () => {
+    resetCounter += 1
   }
 
-  // TODO - cleanup
-  // unnecessary, without this PR the only way to make heightfields is vanilla rapier?
-  // const { world } = useRapier()
-
-  // const bodyDesc = RAPIER.RigidBodyDesc.fixed()
-  // const body = world.createRigidBody(bodyDesc)
-  // const colliderDesc = RAPIER.ColliderDesc.heightfield(nsubdivs, nsubdivs, heights, scale)
-  // world.createCollider(colliderDesc, body)
+	let debugEnabled = false
+	export const toggleDebug = () => {
+		debugEnabled = !debugEnabled
+	}
 </script>
 
 <T.PerspectiveCamera
@@ -81,19 +56,25 @@
 <T.DirectionalLight position={[3, 10, 10]} />
 <T.HemisphereLight intensity={0.2} />
 
-<T.Mesh
-  receiveShadow
-  {material}
-  {geometry}
-  rotation.x={DEG2RAD * -90}
-  rotation.z={DEG2RAD * 0}
-/>
+{#key resetCounter}
+	<FallingShapes />
+{/key}
 
+<T.Mesh
+	receiveShadow
+	{geometry}
+	rotation.x={DEG2RAD * -90}
+	rotation.z={DEG2RAD * 0}
+>
+	<T.MeshStandardMaterial color="teal" opacity="0.8" transparent/>
+</T.Mesh>
 <RigidBody type={'fixed'}>
   <Collider
-    shape={'heightfield'}
-    args={[nsubdivs, nsubdivs, heights, scale]}
+	shape={'heightfield'}
+	args={[nsubdivs, nsubdivs, heights, scale]}
   />
 </RigidBody>
 
-<Debug />
+{#if debugEnabled===true}
+	<Debug />
+{/if}
