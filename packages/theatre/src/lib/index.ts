8201a9dda460d2b1b113e2d6bd695bbf5d8537a6
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-07-03 23:10:30 +0200

    Inverted <Transform> grouping

--- packages/theatre/src/lib/index.ts
+++ packages/theatre/src/lib/index.ts
@@ -4,8 +4,12 @@ export { default as Project } from './project/Project.svelte'
 export { default as Sequence } from './sequence/Sequence.svelte'
 export { default as Sheet } from './sheet/Sheet.svelte'
 export { default as Editable } from './editable/Editable.svelte'
-export { default as SheetObject } from './sheetObject/SheetObject.svelte'
 export { default as Theatre } from './theatre/Theatre.svelte'
+
+// SheetObject related things
+export { default as SheetObject } from './sheetObject/SheetObject.svelte'
+export { createTransformer } from './sheetObject/transfomers/createTransformer'
+export type { Transformer } from './sheetObject/transfomers/types'
 
 // hooks
 export { useStudio } from './studio/useStudio'
--- packages/theatre/src/lib/sheetObject/transform/Transform.svelte
+++ packages/theatre/src/lib/sheetObject/transform/Transform.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import { Group } from 'three'
   import { types, type ISheetObject, type UnknownShorthandCompoundProps } from '@theatre/core'
   import type { IScrub } from '@theatre/studio'
-  import { useParent, watch, type CurrentWritable } from '@threlte/core'
+  import { watch, type CurrentWritable, T } from '@threlte/core'
   import { TransformControls } from '@threlte/extras'
   import { onMount, type ComponentProps } from 'svelte'
   import { RAD2DEG } from 'three/src/math/MathUtils'
@@ -14,30 +15,22 @@
 
   export let label: Label = undefined as Label
   export let key: Props['key'] = undefined as Props['key']
-  export let mode: Props['mode'] = undefined as Props['mode']
+  export let mode: Props['mode'] = 'translate' as Props['mode']
 
   export let sheetObject: CurrentWritable<ISheetObject>
   export let addProps: (props: UnknownShorthandCompoundProps) => void
   export let removeProps: (propNames: string[]) => void
 
-  const parent = useParent()
+  const group = new Group()
 
-  if (!$parent) {
-    throw new Error(
-      '<Transform> must be used as a child to a component instantiating a THREE.Object3D'
-    )
-  }
-
-  const positionTransformer = getDefaultTransformer($parent, 'position', 'position')
-  const rotationTransformer = getDefaultTransformer($parent, 'rotation', 'rotation')
-  const scaleTransformer = getDefaultTransformer($parent, 'scale', 'scale')
+  const positionTransformer = getDefaultTransformer(group, 'position', 'position')
+  const rotationTransformer = getDefaultTransformer(group, 'rotation', 'rotation')
+  const scaleTransformer = getDefaultTransformer(group, 'scale', 'scale')
 
   const initTransform = () => {
-    if (!$parent) return
-
-    const positionProp = positionTransformer.transform($parent.position)
-    const rotationProp = rotationTransformer.transform($parent.rotation)
-    const scaleProp = scaleTransformer.transform($parent.scale)
+    const positionProp = positionTransformer.transform(group.position)
+    const rotationProp = rotationTransformer.transform(group.rotation)
+    const scaleProp = scaleTransformer.transform(group.scale)
 
     if (label && key) {
       addProps({
@@ -61,19 +54,17 @@
     }
   }
 
-  watch([parent, sheetObject], ([parent, sheetObject]) => {
-    if (!parent) return
-
+  watch([sheetObject], ([sheetObject]) => {
     return sheetObject?.onValuesChange((values) => {
-      if (!values.position || !values.rotation || !values.scale || !parent) return
+      if (!values.position || !values.rotation || !values.scale) return
 
       const object = key ? values[key] : values
 
       if (!object) return
 
-      positionTransformer.apply(parent, 'position', object.position)
-      rotationTransformer.apply(parent, 'rotation', object.rotation)
-      scaleTransformer.apply(parent, 'scale', object.scale)
+      positionTransformer.apply(group, 'position', object.position)
+      rotationTransformer.apply(group, 'rotation', object.rotation)
+      scaleTransformer.apply(group, 'scale', object.scale)
     })
   })
 
@@ -104,23 +95,22 @@
   const onChange = () => {
     if (!scrub) return
     scrub.capture((api) => {
-      if (!$parent) return
       if (!$sheetObject) return
 
       const baseTarget = key ? $sheetObject.props[key] : $sheetObject.props
 
       api.set(baseTarget.position as any, {
-        ...$parent.position
+        ...group.position
       })
 
       api.set(baseTarget.rotation as any, {
-        x: $parent.rotation.x * RAD2DEG,
-        y: $parent.rotation.y * RAD2DEG,
-        z: $parent.rotation.z * RAD2DEG
+        x: group.rotation.x * RAD2DEG,
+        y: group.rotation.y * RAD2DEG,
+        z: group.rotation.z * RAD2DEG
       })
 
       api.set(baseTarget.scale as any, {
-        ...$parent.scale
+        ...group.scale
       })
     })
   }
@@ -130,14 +120,23 @@
     scrub.commit()
     scrub = undefined
   }
+
+  const groupRef = group as any
 </script>
 
-{#if isSelected}
-  <TransformControls
-    object={$parent}
-    {mode}
-    on:mouseDown={onMouseDown}
-    on:objectChange={onChange}
-    on:mouseUp={onMouseUp}
-  />
-{/if}
+<T
+  is={groupRef}
+  let:ref
+>
+  {#if isSelected}
+    <TransformControls
+      object={ref}
+      {mode}
+      on:mouseDown={onMouseDown}
+      on:objectChange={onChange}
+      on:mouseUp={onMouseUp}
+    />
+  {/if}
+
+  <slot />
+</T>
