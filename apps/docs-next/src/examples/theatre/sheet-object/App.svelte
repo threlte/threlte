7e49b5b0841aded2acd57e0bc463db352f9bea02
Matan Bendix Shenhav <matan@shenhav.fyi>
2023-07-16 09:42:36 +0200

    Add sheet object example (broken)

---
+++ apps/docs-next/src/examples/theatre/sheet-object/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Project, Sheet, Studio } from '@threlte/theatre'
+
+  import Scene from './Scene.svelte'
+  //import state from './state.json'
+</script>
+
+<Studio />
+
+<Project>
+  <Sheet>
+    <Scene />
+  </Sheet>
+</Project>
---
+++ apps/docs-next/src/examples/theatre/sheet-object/Scene.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { SheetObject } from '@threlte/theatre'
+</script>
+
+<SheetObject
+  key="box"
+  let:AutoProps
+  let:ManualProps
+  let:Transform
+>
+  <T.Mesh>
+    <Transform key="positioning" />
+    <T.BoxGeometry />
+    <T.MeshPhysicalMaterial
+      let:ref
+      color="red"
+    >
+      <AutoProps
+        {ref}
+        transparent
+        opacity
+        metalness
+        roughness
+        side
+        color
+      />
+    </T.MeshPhysicalMaterial>
+  </T.Mesh>
+</SheetObject>
