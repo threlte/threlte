e343439ebcf755b0c4433537581bc084eb304d1d
nemzyx <ferryisworking@gmail.com>
2023-07-23 15:28:52 +0200

    Add LOD documentation @next

---
+++ apps/docs-next/src/examples/geometry/LOD/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import Scene from './Scene.svelte'
+	import { useTweakpane } from '../../utils/useTweakpane'
+
+	let reset: (() => void) | undefined
+
+	const { action, addButton } = useTweakpane()
+
+	addButton({
+		title: 'Reset Camera',
+		onClick: () => {
+			reset?.()
+		}
+	})
+</script>
+
+<div use:action />
+
+<Canvas>
+	<Scene bind:reset />
+</Canvas>
