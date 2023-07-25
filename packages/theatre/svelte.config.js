2fd09fb7f0b7abfc6e169012d0518eb976ffc576
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-07-25 15:15:29 +0200

    Updated deps for core

--- packages/theatre/svelte.config.js
+++ packages/theatre/svelte.config.js
@@ -6,18 +6,7 @@ const config = {
   preprocess: [preprocess()],
 
   kit: {
-    // prerender: {
-    //   default: true
-    // },
-
     adapter: adapter()
-
-    // package: {
-    //   dir: 'dist',
-    //   emitTypes: true,
-    //   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    //   exports: (file) => file === 'index.ts'
-    // }
   }
 }
 
