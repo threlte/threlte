920d2ce43e98d0e7ab31fdd9b237639b2b61453a
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-07-28 16:23:47 +0200

    added animated carets

--- apps/docs-next/src/components/Hero/Intro/Intro.svelte
+++ apps/docs-next/src/components/Hero/Intro/Intro.svelte
@@ -32,6 +32,7 @@
 >
   <div
     class="fixed top-0 left-0 mt-[40vh] flex w-screen flex-col items-center justify-center"
+    class="fixed top-0 left-0 mt-[30vh] flex w-screen flex-col items-center justify-center"
     style="transform: translateY({$springScrollPos * -50}px)"
   >
     <TheatreTextBox key="mission">
@@ -80,7 +81,7 @@
   </div>
 </FadeOut>
 
-<div class="fixed bottom-0 left-0 flex w-screen justify-center pb-[20vh]">
+<div class="fixed bottom-0 left-0 flex w-screen flex-col items-center pb-[20vh]">
   <FadeOut
     progress={$springScrollPos}
     from={0}
@@ -106,14 +107,59 @@
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="40"
-        height="40"
+        width="32"
+        height="32"
+        class="pulse-1 -mb-[20px]"
         fill="#fff"
         viewBox="0 0 256 256"
-        ><path
-          d="M200.12,55.87A102,102,0,0,0,55.87,200.12,102,102,0,1,0,200.12,55.87Zm-8.48,135.77a90,90,0,1,1,0-127.28A90.1,90.1,0,0,1,191.64,191.64ZM164.24,83.76a6,6,0,0,1,0,8.48l-32,32a6,6,0,0,1-8.48,0l-32-32a6,6,0,0,1,8.48-8.48L128,111.51l27.76-27.75A6,6,0,0,1,164.24,83.76Zm0,56a6,6,0,0,1,0,8.48l-32,32a6,6,0,0,1-8.48,0l-32-32a6,6,0,0,1,8.48-8.48L128,167.51l27.76-27.75A6,6,0,0,1,164.24,139.76Z"
-        /></svg
       >
+        <path
+          d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        class="pulse-2 -mb-[20px]"
+        fill="#fff"
+        viewBox="0 0 256 256"
+      >
+        <path
+          d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        class="pulse-3 -mb-[20px]"
+        fill="#fff"
+        viewBox="0 0 256 256"
+      >
+        <path
+          d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+        />
+      </svg>
     </div>
   </FadeOut>
 </div>
+
+<style>
+  @keyframes custom-pulse {
+    50% {
+      opacity: 0;
+    }
+  }
+  .pulse-1 {
+    animation: custom-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  }
+  .pulse-2 {
+    animation: custom-pulse 2s 0.3s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  }
+  .pulse-3 {
+    animation: custom-pulse 2s 0.6s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  }
+</style>
