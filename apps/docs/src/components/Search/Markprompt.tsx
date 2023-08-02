2697d2b8af815f6c1fde81a7243f3e58b5dc60ed
Martin J <jerzakm@gmail.com>
2023-08-02 20:05:43 +0200

    code highlighting

--- apps/docs/src/components/Search/Markprompt.tsx
+++ apps/docs/src/components/Search/Markprompt.tsx
@@ -1,9 +1,99 @@
 import { searchActive } from './searchStore'
 import { Markprompt } from '@markprompt/react'
 import { h, Component } from 'preact'
-import { useEffect, useState } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import hljs from 'highlight.js'
+//@ts-ignore
+import hljs_svelte from 'highlightjs-svelte'
+
+function Search({ visible }: { visible: boolean }) {
+  // Code highlighting
+
+  const container = useRef(null)
+
+  const callback = (mutationList: any) => {
+    for (const mutation of mutationList) {
+      if (
+        mutation.type === 'attributes' &&
+        mutation.attributeName === 'data-loading-state' &&
+        mutation.target.className === 'MarkpromptContentPlain'
+      ) {
+        const value = mutation.target.attributes.getNamedItem('data-loading-state')
+
+        if (value.nodeValue === 'done') {
+          mutation.target.querySelectorAll('pre').forEach((el: HTMLElement) => {
+            if (!el.classList.contains('hljs') && el.innerHTML.length > 20) {
+              hljs.highlightElement(el)
+            }
+          })
+        }
+      }
+    }
+  }
+
+  const config = { attributes: true, childList: true, subtree: true }
+
+  useEffect(() => {
+    const observer = new MutationObserver(callback)
+
+    if (container.current) {
+      observer.observe(container.current, config)
+    }
+
+    return () => observer.disconnect()
+  }, [container.current])
+
+  return (
+    visible && (
+      <>
+        <div
+          className="h-[75vh]"
+          ref={container}
+        >
+          <Markprompt
+            projectKey="sk_test_jRhXPf4l0JEZlzdGzRwSsTFKSYT6ZzeA"
+            close={{
+              visible: false
+            }}
+            search={{
+              enabled: true,
+              provider: {
+                name: 'algolia',
+                apiKey: 'b14257a2e455fc213a51ce63942dd721',
+                appId: 'ZEJ3L4CHC0',
+                indexName: 'threlte'
+              }
+            }}
+            references={{
+              heading: 'Visit sources this answer was generated from:',
+              getHref: (ref) => {
+                const baseUrl = `/docs${ref.file.path.replace('.mdx', '').replace('.md', '')}`
+                if (ref.meta?.leadHeading?.slug) {
+                  return `${baseUrl}#${ref.meta.leadHeading.slug}`
+                }
+                return baseUrl
+              },
+              getLabel: (ref) => {
+                if (JSON.stringify(ref.file.meta.title) === '{}') {
+                  return ref.file.meta.name
+                }
+
+                if (ref.meta?.leadHeading?.value) {
+                  return `${ref.file.meta.category} - ${ref.meta.leadHeading.value}`
+                }
+                return `${ref.file.meta.category} - ${ref.file.meta.title}`
+              }
+            }}
+            display="plain"
+          />
+        </div>
+      </>
+    )
+  )
+}
 
 export default function NewMarkprompt() {
+  // Visibility
   const [visible, setVisible] = useState(false)
 
   const unsubscribe = searchActive.subscribe((v) => {
@@ -24,50 +114,12 @@ export default function NewMarkprompt() {
             onClick={close}
           />
           <dialog
-            open={true}
+            open={visible}
             className={
               'pointer-events-auto mt-[10vh] flex max-h-[75vh] w-full max-w-[800px] flex-col gap-4 rounded-lg border border-white/20 bg-blue-900 px-0 py-2 text-white'
             }
           >
-            <div className="h-[75vh]">
-              <Markprompt
-                projectKey="sk_test_jRhXPf4l0JEZlzdGzRwSsTFKSYT6ZzeA"
-                debug={true}
-                close={{
-                  visible: false
-                }}
-                search={{
-                  enabled: true,
-                  provider: {
-                    name: 'algolia',
-                    apiKey: 'b14257a2e455fc213a51ce63942dd721',
-                    appId: 'ZEJ3L4CHC0',
-                    indexName: 'threlte'
-                  }
-                }}
-                references={{
-                  heading: 'Visit sources this answer was generated from:',
-                  getHref: (ref) => {
-                    const baseUrl = `/docs${ref.file.path.replace('.mdx', '').replace('.md', '')}`
-                    if (ref.meta?.leadHeading?.slug) {
-                      return `${baseUrl}#${ref.meta.leadHeading.slug}`
-                    }
-                    return baseUrl
-                  },
-                  getLabel: (ref) => {
-                    if (JSON.stringify(ref.file.meta.title) === '{}') {
-                      return ref.file.meta.name
-                    }
-
-                    if (ref.meta?.leadHeading?.value) {
-                      return `${ref.file.meta.category} - ${ref.meta.leadHeading.value}`
-                    }
-                    return `${ref.file.meta.category} - ${ref.file.meta.title}`
-                  }
-                }}
-                display="plain"
-              />
-            </div>
+            <Search visible={visible} />
           </dialog>
         </div>
       </>
