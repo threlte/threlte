a602134048f0ac0788a003e643cf844ade9f50a5
Martin J <jerzakm@gmail.com>
2023-08-30 23:51:55 +0200

    reorganize stackblitz template - Instead of template strings in one file use actual files and ?raw vite import

---
+++ apps/docs/src/components/Example/stackblitz-files/src/Layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import App from './example/App.svelte'
+</script>
+
+<div>
+  <App />
+</div>
+
+<style>
+  :global(body, html) {
+    margin: 0;
+  }
+
+  div {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+</style>
