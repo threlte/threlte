94840361adea318c53cff97416ba0d92655f89a2
Micheal Parks <michealparks1989@gmail.com>
2023-08-30 19:17:08 -0400

    don't extend catalog

--- packages/xr/src/lib/components/ray.svelte
+++ packages/xr/src/lib/components/ray.svelte
@@ -1,15 +1,9 @@
-<script lang='ts' context='module'>
+<script lang='ts'>
 
-import { T, extend } from '@threlte/core'
+import { T } from '@threlte/core'
 import { Line2 } from 'three/examples/jsm/lines/Line2'
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry'
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial'
-
-extend({ Line2, LineGeometry, LineMaterial })
-
-</script>
-
-<script lang='ts'>
 
 export let positions: Float32Array | undefined
 export let color = 'white'
@@ -22,13 +16,13 @@ $: if (positions) {
 
 </script>
 
-<T.Line2
+<T is={Line2}
   {...$$restProps}
   position.z={-0.01}
 >
   <T is={lineGeometry} />
-  <T.LineMaterial
+  <T is={LineMaterial}
     {color}
     linewidth={0.004}
   />
-</T.Line2>
+</T>
