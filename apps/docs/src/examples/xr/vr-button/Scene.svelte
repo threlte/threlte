5438d29c76c16eea46a74a87f0abf41335fced91
Micheal Parks <michealparks1989@gmail.com>
2023-08-29 21:58:33 -0400

    complete vr-button example

--- apps/docs/src/examples/xr/vr-button/Scene.svelte
+++ apps/docs/src/examples/xr/vr-button/Scene.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { T, useFrame } from '@threlte/core'
+	import { RoundedBoxGeometry } from '@threlte/extras';
 	import { XR } from '@threlte/xr'
 
 	let rotation = 0
@@ -10,12 +11,25 @@
 
 <XR />
 
+<T.AmbientLight />
 <T.DirectionalLight position.y={10} position.z={10} />
 
+<T.PerspectiveCamera
+	makeDefault
+	position.y={1.8}
+	position.z={1}
+/>
+
 <T.Mesh
+	position.y={1.8}
 	rotation.x={rotation}
 	rotation.y={rotation}
 >
-	<T.BoxGeometry args={[2, 2, 2]} />
-	<T.MeshStandardMaterial />
+	<RoundedBoxGeometry args={[0.2, 0.2, 0.2]} />
+	<T.MeshStandardMaterial color='orange' />
+</T.Mesh>
+
+<T.Mesh>
+	<T.CylinderGeometry args={[3, 0.01]} />
+	<T.MeshStandardMaterial color='turquoise' />
 </T.Mesh>
