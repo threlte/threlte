8d5acca857e9030b377b4e93e69bf46f00942921
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-09-02 11:05:24 +0200

    Poll for joint radius on every frame until ready.

--- packages/xr/src/lib/hooks/useHandJoint.ts
+++ packages/xr/src/lib/hooks/useHandJoint.ts
@@ -1,41 +1,28 @@
-import { onDestroy } from 'svelte'
-import { currentWritable } from '@threlte/core'
-import type { HandJoints } from '../lib/handJoints'
+import { currentWritable, useFrame } from '@threlte/core'
 import type * as THREE from 'three'
+import type { HandJoints } from '../lib/handJoints'
 import { useHand } from './useHand'
-
-const onHandReady = (
-  handedness: 'left' | 'right',
-  joint: HandJoints,
-  callback: (jointSpace: THREE.XRJointSpace) => void
-) => {
-  const xrhand = useHand(handedness)
-
-  let id: number
-
-  const tick = () => {
-    const jointSpace = xrhand.current?.hand.joints[joint]
-
-    if (jointSpace?.jointRadius === undefined) {
-      id = window.setTimeout(tick, 100)
-      return
-    }
-
-    callback(jointSpace)
-  }
-
-  tick()
-
-  return () => clearTimeout(id)
-}
 
 /**
  * Provides a reference to a requested hand joint, once available.
  */
 export const useHandJoint = (handedness: 'left' | 'right', joint: HandJoints) => {
-  const jointSpace = currentWritable<THREE.XRJointSpace | undefined>(undefined)
+  const jointSpaceStore = currentWritable<THREE.XRJointSpace | undefined>(undefined)
 
-  onDestroy(onHandReady(handedness, joint, (joint) => jointSpace.set(joint)))
+  const xrhand = useHand(handedness)
 
-  return jointSpace
+  const { stop } = useFrame(
+    ({ invalidate }) => {
+      const jointSpace = xrhand.current?.hand.joints[joint]
+      // The joint radius is a good indicator that the joint is ready
+      if (jointSpace?.jointRadius !== undefined) {
+        jointSpaceStore.set(jointSpace)
+        invalidate()
+        stop()
+      }
+    },
+    { autostart: false, invalidate: false }
+  )
+
+  return jointSpaceStore
 }
