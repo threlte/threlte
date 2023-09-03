d4c87ebf0fb4ca607ebd31a19d03b2310e9a62dd
Ed Sunman <ed.sunman@freshlabel.co.uk>
2023-09-03 22:43:53 +0100

    spacing

--- packages/extras/src/lib/components/MeshLine/fragment.ts
+++ packages/extras/src/lib/components/MeshLine/fragment.ts
@@ -7,21 +7,21 @@ export const fragmentShader = `
     uniform float dashArray;
     uniform float dashOffset;
     uniform float dashRatio;
-		uniform sampler2D alphaMap;
-		uniform float useAlphaMap;
+    uniform sampler2D alphaMap;
+    uniform float useAlphaMap;
 
-		varying vec2 vUV;
+    varying vec2 vUV;
     varying vec4 vColor;
     varying float vCounters;
 
     void main()	{
 
         ${ShaderChunk.logdepthbuf_fragment}
-				${ShaderChunk.encodings_fragment}
+        ${ShaderChunk.encodings_fragment}
 
         vec4 c = vColor;
 
-				if( useAlphaMap == 1. ) c.a *= texture2D( alphaMap, vUV ).r;
+        if( useAlphaMap == 1. ) c.a *= texture2D( alphaMap, vUV ).r;
 
         if( useDash == 1. ){
             c.a *= ceil(mod(vCounters + dashOffset, dashArray) - (dashArray * dashRatio));
