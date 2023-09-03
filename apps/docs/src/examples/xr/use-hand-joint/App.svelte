f9f3e9f5ef57f9074bb746c09743a431a10ef246
Micheal Parks <michealparks1989@gmail.com>
2023-09-03 00:26:04 -0400

    remove debug

--- apps/docs/src/examples/xr/use-hand-joint/App.svelte
+++ apps/docs/src/examples/xr/use-hand-joint/App.svelte
@@ -1,13 +1,12 @@
 <script lang='ts'>
   import { Canvas } from '@threlte/core'
-  import { World, Debug } from '@threlte/rapier'
+  import { World } from '@threlte/rapier'
   import { VRButton } from '@threlte/xr'
   import Scene from './Scene.svelte'
 </script>
 
 <Canvas>
   <World gravity={[0, 0, 0]}>
-    <Debug />
     <Scene />
   </World>
 </Canvas>
