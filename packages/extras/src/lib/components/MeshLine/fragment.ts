82e4c1b1076d367e5b3f5579e2224470b6db73d2
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-09-07 12:22:52 +0200

    Added revision based includes in shaders

--- packages/extras/src/lib/components/MeshLine/fragment.ts
+++ packages/extras/src/lib/components/MeshLine/fragment.ts
@@ -1,32 +1,32 @@
-import { ShaderChunk } from 'three'
+import { revision } from '../../lib/revision'
+
 export const fragmentShader = `
-    ${ShaderChunk.fog_pars_fragment}
-    ${ShaderChunk.logdepthbuf_pars_fragment}
+#include <fog_pars_fragment>
+#include <logdepthbuf_pars_fragment>
 
-    uniform float useDash;
-    uniform float dashArray;
-    uniform float dashOffset;
-    uniform float dashRatio;
-    uniform sampler2D alphaMap;
-    uniform float useAlphaMap;
+uniform float useDash;
+uniform float dashArray;
+uniform float dashOffset;
+uniform float dashRatio;
+uniform sampler2D alphaMap;
+uniform float useAlphaMap;
 
-    varying vec2 vUV;
-    varying vec4 vColor;
-    varying float vCounters;
+varying vec2 vUV;
+varying vec4 vColor;
+varying float vCounters;
 
-    void main()	{
+void main()	{
+	#include <logdepthbuf_fragment>
+	#include <${revision < 154 ? 'encodings_fragment' : 'colorspace_fragment'}>
 
-        ${ShaderChunk.logdepthbuf_fragment}
-        ${ShaderChunk.encodings_fragment}
+	vec4 c = vColor;
 
-        vec4 c = vColor;
+	if( useAlphaMap == 1. ) c.a *= texture2D( alphaMap, vUV ).r;
 
-        if( useAlphaMap == 1. ) c.a *= texture2D( alphaMap, vUV ).r;
+	if( useDash == 1. ){
+			c.a *= ceil(mod(vCounters + dashOffset, dashArray) - (dashArray * dashRatio));
+	}
 
-        if( useDash == 1. ){
-            c.a *= ceil(mod(vCounters + dashOffset, dashArray) - (dashArray * dashRatio));
-        }
-
-        gl_FragColor = LinearTosRGB(c);
-    }
+	gl_FragColor = LinearTosRGB(c);
+}
 `
