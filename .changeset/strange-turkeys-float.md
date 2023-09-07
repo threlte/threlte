e71b640830db31328793857e01d63adc5ea4c0ab
Micheal Parks <michealparks1989@gmail.com>
2023-09-07 00:42:15 -0400

    add changeset

---
+++ .changeset/strange-turkeys-float.md
@@ -0,0 +1,5 @@
+---
+'@threlte/xr': patch
+---
+
+Add headset component
---
+++ apps/docs/src/examples/xr/headset/Microphone.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+</script>
+
+<T.Mesh position={[0, 0, -1]}>
+  {@const size = 0.01}
+  <T.CylinderGeometry args={[size, size, size * 10]} />
+  <T.MeshStandardMaterial />
+  <T.Mesh position={[size * 3, -size * 8, 0]} rotation.z={Math.PI / 4}>
+    <T.CylinderGeometry args={[size, size, size * 10]} />
+    <T.MeshStandardMaterial />
+    <T.Mesh position={[0, -size * 8, 0]} rotation.z={Math.PI / 4}>
+      <T.IcosahedronGeometry args={[size * 3, 2]} />
+      <T.MeshStandardMaterial color='gray' />
+    </T.Mesh>
+  </T.Mesh>
+</T.Mesh>
