f15eeec4e2d39e643529ff9c5aefb4671e5041c7
Laszlo Korte <laszlokorte@users.noreply.github.com>
2023-09-14 13:41:38 +0200

    Add back line scaling in MeshLine vertex shader

--- packages/extras/src/lib/components/MeshLine/vertex.ts
+++ packages/extras/src/lib/components/MeshLine/vertex.ts
@@ -67,6 +67,7 @@ export const vertexShader = `
 
         if(sizeAttenuation != 0.0) {
             normal /= currentClip.w;
+						normal *= min(resolution.x, resolution.y) / 2.0;
         }
 
         if (scaleDown > 0.0) {
