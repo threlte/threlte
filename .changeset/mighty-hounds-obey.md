38f95de5ba5b6cffa361dbaf4ab8343161d07af6
Micheal Parks <michealparks1989@gmail.com>
2023-09-25 13:06:25 -0400

    fix headset position when teleporting

---
+++ .changeset/mighty-hounds-obey.md
@@ -0,0 +1,6 @@
+---
+'@threlte/xr': patch
+---
+
+* Fix `<Headset>`` position when teleporting
+* Add `<Headset syncCamera>` property
--- packages/xr/src/routes/+page.svelte
+++ packages/xr/src/routes/+page.svelte
@@ -1,69 +1,12 @@
 <script lang="ts">
-  import { T, Canvas } from '@threlte/core'
-  import { XR, VRButton, Controller, Hand, TeleportControls } from '$lib'
+  import { Canvas } from '@threlte/core'
+  import { VRButton } from '$lib'
+  import Scene from './scene.svelte'
 </script>
 
 <main>
   <Canvas>
-    <XR>
-      <Controller left>
-        <T.Mesh>
-          {@const radius = 0.01}
-          {@const length = 0.08}
-          <T.CylinderGeometry args={[radius, radius, length]} />
-          <T.MeshStandardMaterial color='orange' />
-        </T.Mesh>
-        <T.Mesh slot='target-ray'>
-          {@const size = 0.05}
-          <T.BoxGeometry args={[size, size, size]} />
-          <T.MeshStandardMaterial color='turquoise' />
-        </T.Mesh>
-        <T.Mesh slot='grip'>
-          {@const radius = 0.02}
-          <T.IcosahedronGeometry args={[radius]} />
-          <T.MeshStandardMaterial color='skyblue' />
-        </T.Mesh>
-      </Controller>
-
-      <Controller right />
-
-      <Hand left>
-        <T.Mesh slot='wrist'>
-          {@const size = 0.05}
-          <T.BoxGeometry args={[size, size, size]} />
-          <T.MeshStandardMaterial color='turquoise' />
-        </T.Mesh>
-      </Hand>
-
-      <Hand right>
-        <T.Mesh>
-          {@const size = 0.05}
-          <T.BoxGeometry args={[size, size, size]} />
-          <T.MeshStandardMaterial color='skyblue' />
-        </T.Mesh>
-      </Hand>
-    </XR>
-
-    <TeleportControls>
-      <T.Mesh receiveShadow teleportSurface position.y={-0.05}>
-        <T.CylinderGeometry args={[2, 2, 0.1]} />
-        <T.MeshStandardMaterial />
-      </T.Mesh>
-    </TeleportControls>
-
-    <T.PerspectiveCamera
-      makeDefault
-      position={[3, 3, 3]}
-      on:create={({ ref }) => ref.lookAt(0, 0, 0)}
-    />
-
-    <T.Mesh position.y={0.5} castShadow receiveShadow>
-      <T.MeshStandardMaterial color="hotpink" />
-      <T.BoxGeometry />
-    </T.Mesh>
-
-    <T.DirectionalLight castShadow />
-    <T.AmbientLight />
+    <Scene />
   </Canvas>
 
   <VRButton />
