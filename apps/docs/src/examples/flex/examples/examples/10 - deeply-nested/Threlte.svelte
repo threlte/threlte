57bc4ec006c2ce8f72a6923ca99f693a17159577
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-09-28 15:54:43 +0200

    Added examples route to getting started

---
+++ apps/docs/src/examples/flex/examples/examples/10 - deeply-nested/Threlte.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+  import { Flex, Box } from '@threlte/flex'
+  import Plane from '../../Plane.svelte'
+</script>
+
+<Plane
+  width={500}
+  height={500}
+  color="red"
+/>
+
+<Flex
+  width={500}
+  height={500}
+  justifyContent="Center"
+  alignItems="Stretch"
+  gap={20}
+  padding={20}
+>
+  <Box
+    width="auto"
+    height="auto"
+    flex={1}
+    gap={20}
+    padding={20}
+    flexDirection="Column"
+    let:width
+    let:height
+  >
+    <Plane
+      color="yellow"
+      {width}
+      {height}
+      depth={1}
+    />
+
+    <Box
+      flex={1}
+      width="auto"
+      height="auto"
+      let:width
+      let:height
+    >
+      <Plane
+        color="fuchsia"
+        {width}
+        {height}
+        depth={2}
+      />
+    </Box>
+
+    <Box
+      flex={1}
+      width="auto"
+      height="auto"
+      justifyContent="Center"
+      alignItems="Center"
+      let:width
+      let:height
+    >
+      <Plane
+        color="orange"
+        {width}
+        {height}
+        depth={2}
+      />
+
+      <Box
+        width={100}
+        height={100}
+        let:width
+        let:height
+      >
+        <Plane
+          color="red"
+          {width}
+          {height}
+          depth={3}
+        />
+      </Box>
+    </Box>
+  </Box>
+
+  <Box
+    width="auto"
+    height="auto"
+    flex={0.5}
+    alignItems="Stretch"
+    padding={20}
+    let:width
+    let:height
+  >
+    <Plane
+      color="blue"
+      {width}
+      {height}
+      depth={1}
+    />
+
+    <Box
+      flex={1}
+      width="auto"
+      height={100}
+      let:width
+      let:height
+    >
+      <Plane
+        color="pink"
+        {width}
+        {height}
+        depth={2}
+      />
+    </Box>
+
+    <Box
+      flex={1}
+      width="auto"
+      height={100}
+      alignSelf="FlexEnd"
+      let:width
+      let:height
+    >
+      <Plane
+        color="hotpink"
+        {width}
+        {height}
+        depth={2}
+      />
+    </Box>
+  </Box>
+</Flex>
---
+++ apps/docs/src/examples/flex/examples/examples/Common.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
+</script>
+
+<T.OrthographicCamera
+  makeDefault
+  near={100}
+  far={10000}
+  position={[0, 0, 5000]}
+  on:create={({ ref }) => ref.lookAt(0, 0, 0)}
+>
+  <OrbitControls zoomToCursor />
+</T.OrthographicCamera>
+
+<T.DirectionalLight />
