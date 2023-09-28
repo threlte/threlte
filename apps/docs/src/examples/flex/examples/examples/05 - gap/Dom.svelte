c401ab26dd3175898981364ee2712cd65b113129
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-09-28 15:54:43 +0200

    Added examples route to getting started

---
+++ apps/docs/src/examples/flex/examples/examples/05 - gap/Dom.svelte
@@ -0,0 +1,18 @@
+<div
+  class="red"
+  style="width: 500px; height: 500px; display: flex; justify-content: center; align-items: center; gap: 20px"
+>
+  <div
+    class="yellow"
+    style="width: 100px; height: 100px;"
+  />
+
+  <div
+    class="blue"
+    style="width: 100px; height: 100px;"
+  />
+</div>
+
+<style lang="postcss">
+  @import '../../colors.postcss';
+</style>
---
+++ apps/docs/src/examples/flex/examples/examples/08 - tailwind/Threlte.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Flex, Box, tailwindParser } from '@threlte/flex'
+  import Plane from '../../Plane.svelte'
+</script>
+
+<Plane
+  width={500}
+  height={500}
+  color="red"
+/>
+
+<Flex
+  width={500}
+  height={500}
+  classParser={tailwindParser}
+  class="gap-10 p-10"
+>
+  <Box
+    class="w-100 h-100"
+    let:width
+    let:height
+  >
+    <Plane
+      {width}
+      {height}
+      color="yellow"
+      depth={1}
+    />
+  </Box>
+
+  <Box
+    class="flex-1"
+    let:width
+  >
+    <Plane
+      color="blue"
+      {width}
+      height={100}
+      depth={1}
+    />
+  </Box>
+</Flex>
