16285756cc9958a731585a04f7df7b4ee73cddfe
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-09-30 14:01:02 +0200

    Adding basics

---
+++ apps/docs/src/examples/flex/intro/Circle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+
+  export let color: string = 'white'
+  export let radius = 5
+  export let z = 0
+</script>
+
+<T.Mesh position.z={z}>
+  <T.CircleGeometry args={[radius]} />
+  <T.MeshBasicMaterial {color} />
+</T.Mesh>
