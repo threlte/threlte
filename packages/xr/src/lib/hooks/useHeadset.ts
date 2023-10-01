56c8b91bb642f9053fce9464f52308ca2dbe7416
Micheal Parks <michealparks1989@gmail.com>
2023-10-01 19:55:00 -0400

    tie headset updates to XR component

--- packages/xr/src/lib/hooks/useHeadset.ts
+++ packages/xr/src/lib/hooks/useHeadset.ts
@@ -1,52 +1,5 @@
-import { useFrame, useThrelte } from '@threlte/core'
-import { Group } from 'three'
-import { isPresenting } from '../internal/stores'
-
-const group = new Group()
-
-let initialized = false
-
-const initialize = () => {
-  const { renderer, camera } = useThrelte()
-  const { xr } = renderer
-
-  const immersiveFrame = useFrame(() => {
-    const space = xr.getReferenceSpace()
-
-    if (space === null) return
-
-    const pose = xr.getFrame().getViewerPose(space)
-
-    if (pose === undefined) return
-
-    const { position, orientation } = pose.transform
-
-    group.position.set(position.x, position.y, position.z)
-    group.quaternion.set(orientation.x, orientation.y, orientation.z, orientation.w)
-  }, { autostart: false })
-
-  const nonImmersiveFrame = useFrame(() => {
-    group.position.copy(camera.current.position)
-    group.quaternion.copy(camera.current.quaternion)
-  }, { autostart: false })
-
-  isPresenting.subscribe((value) => {
-    if (value) {
-      immersiveFrame.start()
-      nonImmersiveFrame.stop()
-    } else {
-      immersiveFrame.stop()
-      nonImmersiveFrame.start()
-    }
-  })
-}
+import { headset } from '../internal/headset'
 
 export const useHeadset = (): Readonly<THREE.Group> => {
-  // We don't want to perform headset syncing more than once per frame
-  if (!initialized) {
-    initialize()
-    initialized = true
-  }
-
-  return group
+  return headset
 }
---
+++ packages/xr/src/lib/internal/headset.ts
@@ -0,0 +1,40 @@
+import { Group } from 'three'
+import { useThrelte, useFrame } from '@threlte/core'
+import { isPresenting } from '../internal/stores'
+
+export const headset = new Group()
+
+export const useUpdateHeadset = () => {
+  const { renderer, camera } = useThrelte()
+  const { xr } = renderer
+
+  const immersiveFrame = useFrame(() => {
+    const space = xr.getReferenceSpace()
+
+    if (space === null) return
+
+    const pose = xr.getFrame().getViewerPose(space)
+
+    if (pose === undefined) return
+
+    const { position, orientation } = pose.transform
+
+    headset.position.set(position.x, position.y, position.z)
+    headset.quaternion.set(orientation.x, orientation.y, orientation.z, orientation.w)
+  }, { autostart: false })
+
+  const nonImmersiveFrame = useFrame(() => {
+    headset.position.copy(camera.current.position)
+    headset.quaternion.copy(camera.current.quaternion)
+  }, { autostart: false })
+
+  isPresenting.subscribe((value) => {
+    if (value) {
+      immersiveFrame.start()
+      nonImmersiveFrame.stop()
+    } else {
+      immersiveFrame.stop()
+      nonImmersiveFrame.start()
+    }
+  })
+}
