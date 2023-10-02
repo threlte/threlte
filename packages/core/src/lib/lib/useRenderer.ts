2088939bdf27d18b8aab554506b340336a934f24
Micheal Parks <michealparks1989@gmail.com>
2023-10-02 12:10:46 -0400

    optional chain xr prop

--- packages/core/src/lib/lib/useRenderer.ts
+++ packages/core/src/lib/lib/useRenderer.ts
@@ -93,7 +93,7 @@ export const useRenderer = (ctx: ThrelteContext) => {
   })
 
   watch([renderer, ctx.size], ([renderer, size]) => {
-    if (renderer?.xr.isPresenting) return
+    if (renderer?.xr?.isPresenting) return
     renderer?.setSize(size.width, size.height)
   })
 
