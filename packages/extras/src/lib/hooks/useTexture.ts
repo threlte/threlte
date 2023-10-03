ce85e19c9fe763198650694683352e1d982c4e30
Marcin Jerzak <jerzakm@gmail.com>
2023-10-03 10:07:19 +0200

    updated useTexture

--- packages/extras/src/lib/hooks/useTexture.ts
+++ packages/extras/src/lib/hooks/useTexture.ts
@@ -1,15 +1,55 @@
 import {
   useLoader,
+  useThrelte,
   type UseLoaderLoadInput,
   type UseLoaderLoadResult,
-  type UseLoaderOptions
+  type UseLoaderOptions,
+  type Props
 } from '@threlte/core'
-import { TextureLoader } from 'three'
+import { Texture, TextureLoader } from 'three'
 
 export const useTexture = <Input extends UseLoaderLoadInput>(
   input: Input,
-  options?: UseLoaderOptions<TextureLoader>
+  options?: UseLoaderOptions<TextureLoader>,
+  textureProps: Props<Texture> = {}
 ): UseLoaderLoadResult<TextureLoader, Input> => {
   const loader = useLoader(TextureLoader, options)
-  return loader.load(input)
+  const texture = loader.load(input)
+
+  const { renderer } = useThrelte()
+  const rendererColorSpace = renderer.outputColorSpace
+
+  const assignOptions = (texture: Texture) => {
+    console.log(texture)
+    texture.colorSpace = rendererColorSpace
+
+    for (const prop in textureProps) {
+      //@ts-ignore todo type this
+      texture[prop] = textureProps[prop]
+    }
+
+    texture.needsUpdate = true
+  }
+
+  texture.then((textureResult) => {
+    console.log({ textureResult })
+    if (textureResult?.constructor?.name == '_Texture') {
+      console.log('Texture')
+      assignOptions(textureResult as Texture)
+      return
+    }
+    if (Array.isArray(textureResult)) {
+      console.log('TextureArr')
+      textureResult.map((texture) => assignOptions(texture))
+      return
+    }
+    if (typeof textureResult === 'object') {
+      console.log('TextureMap')
+      Object.values(textureResult).map((texture) => {
+        assignOptions(texture)
+      })
+      return
+    }
+  })
+  return texture
 }
