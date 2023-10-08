6654c2bc3f69adfc26e44987483834a603d5eb2d
Marcin Jerzak <jerzakm@gmail.com>
2023-10-08 18:28:48 +0200

    csm example

---
+++ apps/docs/src/examples/extras/csm/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+</script>
+
+<div class="relative h-full w-full ">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
