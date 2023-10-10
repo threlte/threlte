065b750b6fe7fa2f18c5eed14ebadaa90f973e07
Micheal Parks <michealparks1989@gmail.com>
2023-10-09 23:31:00 -0400

    csm refactor

--- packages/extras/src/lib/components/CSM/CSM.svelte
+++ packages/extras/src/lib/components/CSM/CSM.svelte
@@ -2,94 +2,69 @@
   import { useFrame, useThrelte, watch } from '@threlte/core'
   import { onDestroy } from 'svelte'
   import { writable } from 'svelte/store'
-  import type { Camera, MeshPhongMaterial, MeshStandardMaterial } from 'three'
+  import type { Camera } from 'three'
   import { CSM } from 'three/examples/jsm/csm/CSM'
   import { useMaterials } from './useMaterials'
 
   export let enabled = true
   export let params: Partial<ConstructorParameters<typeof CSM>[0]> = {}
   export let camera: Camera | undefined = undefined
-  export let transform: ((csm: CSM) => CSM) | undefined = undefined
+  export let transform: ((csm: CSM) => CSM) = (csm: CSM) => csm
+  export let fade: boolean | undefined
+  export let maxFar: number | undefined
 
   const enabledStore = writable(enabled)
   $: enabledStore.set(enabled)
 
-  const { camera: defaultCamera, scene } = useThrelte()
+  const { camera: defaultCamera, scene, size } = useThrelte()
 
   let csm: CSM | undefined
 
-  let nextframe = true
-  let thisframe = false
+  const { start, stop } = useFrame(() => csm!.update(), {
+    invalidate: false,
+    autostart: false
+  })
 
-  useFrame(
-    () => {
-      if (!csm) return
-      csm.update()
-      if (thisframe) {
-        thisframe = false
-        csm.updateFrustums()
-      }
-      if (nextframe) {
-        nextframe = false
-        thisframe = true
-      }
-    },
-    { invalidate: false }
-  )
+  const { onNewMaterial, allMaterials } = useMaterials()
 
-  const { materialsQueue, allMaterials } = useMaterials()
-
-  const setupMaterial = (material: MeshPhongMaterial | MeshStandardMaterial) => {
-    // material might already be set up
-    if (!csm || material.defines?.USE_CSM === 1) return
-    csm.setupMaterial(material)
+  const disposeCSM = () => {
+    csm?.remove()
+    csm?.dispose()
+    csm = undefined
   }
 
-  useFrame(
-    ({ invalidate }) => {
-      // we're only dealing with setting up materials in here
-      if (!materialsQueue.size || !csm) return
-      for (const material of materialsQueue) {
-        setupMaterial(material)
-      }
-      materialsQueue.clear()
-      invalidate()
-    },
-    { invalidate: false }
-  )
+  $: $size, csm?.updateFrustums()
 
   // set the default camera
-  $: if (csm) csm.camera = camera ?? $defaultCamera
+  $: if (csm) {
+    csm.camera = camera ?? $defaultCamera
 
-  watch([enabledStore], ([enabled]) => {
-    if (!enabled) {
-      if (csm) {
-        csm.remove()
-        csm.dispose()
-        csm = undefined
-      }
-    } else {
-      csm = new CSM({
+    if (fade !== undefined) csm.fade = fade
+    if (maxFar !== undefined) csm.maxFar = maxFar
+
+    csm.updateFrustums()
+  }
+
+  watch(enabledStore, (enabled) => {
+    if (enabled) { 
+      csm = transform(new CSM({
         camera: camera ?? $defaultCamera,
         parent: scene,
         ...params
-      })
-      csm = transform?.(csm) ?? csm
+      }))
       for (const material of allMaterials) {
-        setupMaterial(material)
-        // if a material is set up here, we can safely remove it from the queue
-        materialsQueue.delete(material)
+        csm.setupMaterial(material)
       }
+      start()
+    } else {
+      stop()
+      disposeCSM()
     }
   })
 
-  onDestroy(() => {
-    if (csm) {
-      csm.remove()
-      csm.dispose()
-      csm = undefined
-    }
-  })
+  onNewMaterial((material) => csm?.setupMaterial(material))
+
+  onDestroy(disposeCSM)
 </script>
 
 <slot />
