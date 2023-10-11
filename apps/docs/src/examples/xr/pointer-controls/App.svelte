045975dbf0adfe9f76d5cb057507fb2b3e321700
Micheal Parks <michealparks1989@gmail.com>
2023-10-10 22:43:40 -0400

    call set_raf, add teleport controls context and hook

--- apps/docs/src/examples/xr/pointer-controls/App.svelte
+++ apps/docs/src/examples/xr/pointer-controls/App.svelte
@@ -3,28 +3,29 @@
   import { BufferGeometry, Vector3 } from 'three'
   import { XR, Controller, VRButton } from '@threlte/xr'
   import Scene from './Scene.svelte'
+
+  const points = [new Vector3(0, 0, 0), new Vector3(0, 0, -1000)]
   
+  let debug = false
 </script>
+
+<svelte:window
+  on:keyup={(e) => e.key === 'd' && (debug = !debug)}
+/>
 
 <Canvas>
   <Scene />
 
   <XR>
     <Controller left>
-      <T.Line slot='pointer-ray'>
-        <T is={new BufferGeometry().setFromPoints([
-          new Vector3(0, 0, 0),
-          new Vector3(0, 0, -1000)
-        ])} />
-      </T.Line>
+        <T.Line slot='target-ray' visible={debug}>
+          <T is={new BufferGeometry().setFromPoints(points)} />
+        </T.Line>
     </Controller>
 
     <Controller right>
-      <T.Line slot='pointer-ray'>
-        <T is={new BufferGeometry().setFromPoints([
-          new Vector3(0, 0, 0),
-          new Vector3(0, 0, -1000)
-        ])} />
+      <T.Line slot='target-ray' visible={debug}>
+        <T is={new BufferGeometry().setFromPoints(points)} />
       </T.Line>
     </Controller>
 
