fa8c16777caa528a890a65e87ea594ee70e442cf
Grischa Erbe <grischa.erbe@googlemail.com>
2023-12-01 23:55:58 +0100

    Added support for reverse animation direction in `<AnimatedSpriteMaterial>`

--- apps/docs/src/examples/extras/animated-sprite-material/App.svelte
+++ apps/docs/src/examples/extras/animated-sprite-material/App.svelte
@@ -1,63 +1,19 @@
 <script lang="ts">
-  import { Canvas, T } from '@threlte/core'
-  import { AnimatedSpriteMaterial } from '@threlte/extras'
+  import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
-  import { useTweakpane } from '$lib/useTweakpane'
-
-  const { addInput, addButton, action } = useTweakpane()
-
-  const fps = addInput({
-    label: 'fps',
-    value: 10,
-    params: {
-      min: 1,
-      step: 1,
-      max: 60
-    }
-  })
-
-  const loop = addInput({
-    label: 'loop',
-    value: true
-  })
-
-  let play: () => void
-  let pause: () => void
-
-  addButton({
-    title: 'play',
-    onClick: () => play()
-  })
-
-  addButton({
-    title: 'pause',
-    onClick: () => pause()
-  })
 </script>
 
-<Canvas>
-  <Scene />
+<div class="wrapper">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
 
-  <T.Sprite position.y={-2.3}>
-    <AnimatedSpriteMaterial
-      bind:play
-      bind:pause
-      textureUrl='/textures/sprites/fire.png'
-      totalFrames={8}
-      fps={$fps}
-      loop={$loop}
-    />
-    <T.PointLight
-      intensity={12}
-      decay={0.5}
-      position.y={-0.2}
-      position.z={0.02}
-    />
-  </T.Sprite>
-  
-
-  <T.AmbientLight />
-  <T.DirectionalLight />
-</Canvas>
-
-<div use:action />
+<style>
+  div.wrapper {
+    background-color: black;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- apps/docs/src/examples/extras/animated-sprite-material/Scene.svelte
+++ apps/docs/src/examples/extras/animated-sprite-material/Scene.svelte
@@ -1,22 +1,44 @@
-<script lang='ts'>
+<script lang="ts">
   import { T } from '@threlte/core'
-  import { AnimatedSpriteMaterial, useTexture } from '@threlte/extras'
+  import { AnimatedSpriteMaterial, Suspense, useTexture } from '@threlte/extras'
+  import Fire from './Fire.svelte'
   import Player from './Player.svelte'
+  import ThrelteSeven from './ThrelteSeven.svelte'
+  import { tweened } from 'svelte/motion'
+  import { cubicOut } from 'svelte/easing'
 
   const texture = useTexture('/textures/sprites/bg.png')
+
+  let playerPosition: [number, number, number]
+  $: playerAtFire = playerPosition && Math.abs(playerPosition[0]) < 0.7
+
+  const fov = tweened(50, {
+    easing: cubicOut,
+    duration: 900
+  })
+  $: fov.set(playerAtFire ? 45 : 50)
+  const cameraPosY = tweened(-0.2, {
+    easing: cubicOut,
+    duration: 900
+  })
+  $: cameraPosY.set(playerAtFire ? -0.9 : -0.2)
 </script>
+
+<Fire />
+
+<T.AmbientLight
+  color="#6697C7"
+  intensity={0.3}
+/>
 
 {#each { length: 9 } as _, i}
   <T.Sprite
     scale={0.5}
     position.y={-1.99}
-    position.x={i < 5
-      ? (i / 2.4) + (Math.random() * 0.4) - 2.8
-      : (i / 2.4) + (Math.random() * 0.4) - 1
-    }
+    position.x={i < 5 ? i / 2.4 + Math.random() * 0.4 - 2.8 : i / 2.4 + Math.random() * 0.4 - 1}
   >
     <AnimatedSpriteMaterial
-      textureUrl='/textures/sprites/grass.png'
+      textureUrl="/textures/sprites/grass.png"
       totalFrames={6}
       fps={5}
       delay={i * 40}
@@ -30,17 +52,19 @@
   position.y={0.4}
 >
   {#if $texture}
-    <T.MeshBasicMaterial
-      map={$texture}
-      totalFrames={8}
-    />
+    <T.MeshBasicMaterial map={$texture} />
   {/if}
 </T.Sprite>
 
-<Player />
+<Suspense>
+  <ThrelteSeven show={playerAtFire} />
+</Suspense>
+
+<Player bind:playerPosition />
 
 <T.PerspectiveCamera
   makeDefault
   position.z={7}
+  position.y={$cameraPosY}
+  fov={$fov}
 />
-
