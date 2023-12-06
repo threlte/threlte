446b18ff23b208fd4257f1bf8cd18d7a4552834b
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2023-12-06 13:00:03 +1300

    copy paste from @grischaerbe 's work

---
+++ apps/docs/src/examples/camera/camera-controls/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import { useTweakpane } from '$lib/useTweakpane'
+  const { addButton, action } = useTweakpane()
+
+  addButton({
+    title: 'test',
+    onClick: () => {
+      console.log('clicked')
+    }
+  })
+</script>
+
+<div use:action />
+
+<div>
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+
+  div {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ apps/docs/src/examples/camera/camera-controls/Scene.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { T, useTask } from '@threlte/core'
+  import { Grid } from '@threlte/extras'
+  // import { spring } from 'svelte/motion'
+  import CameraControls from './CameraControls.svelte'
+
+  // interactivity()
+  // const scale = spring(1)
+  // let rotation = 0
+  // useTask((delta) => {
+  //   rotation += delta
+  // })
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
+>
+  <CameraControls />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight position={[3, 10, 7]} />
+
+<T.Mesh position.y={1}>
+  <T.BoxGeometry args={[1, 1, 1]} />
+  <T.MeshBasicMaterial
+    color="red"
+    wireframe={true}
+  />
+</T.Mesh>
+
+<Grid
+  sectionColor={'#ff3e00'}
+  sectionThickness={1}
+  cellColor={'#cccccc'}
+  gridSize={40}
+/>
