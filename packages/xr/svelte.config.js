f4040fb0791603ca8f6feffd2c8a1941d180fc05
Micheal Parks <michealparks1989@gmail.com>
2024-01-28 15:00:12 -0500

    add changeset

--- packages/xr/svelte.config.js
+++ packages/xr/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
