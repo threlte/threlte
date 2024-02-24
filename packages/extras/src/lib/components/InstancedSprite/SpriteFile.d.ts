2b709fec3753aff780d4b00d2676f767ff43357b
Marcin Jerzak <jerzakm@gmail.com>
2024-02-24 16:15:53 +0100

    remove unused components

--- packages/extras/src/lib/components/InstancedSprite/SpriteFile.d.ts
+++
@@ -1,9 +0,0 @@
-import type { createSpritesheet } from '@threejs-kit/instanced-sprite-mesh'
-import { SvelteComponent } from 'svelte'
-
-export type SpriteFileProps = {
-  textureUrl: string
-  options: Parameters<ReturnType<typeof createSpritesheet>['add']>['1']
-}
-
-export default class SpriteFile extends SvelteComponent<SpriteFileProps> {}
