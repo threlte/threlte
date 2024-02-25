290e93e1ac3c9dd9a2b70cb95a07a82a4255b42c
Marcin Jerzak <jerzakm@gmail.com>
2024-02-25 22:39:25 +0100

    useFbo scene setup

---
+++ apps/docs/src/examples/shaders/useFboScope/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+</script>
+
+<div>
+  <span class="absolute left-0 top-0 z-20 whitespace-nowrap pl-4">Right click to enable zoom</span>
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    height: 100%;
+  }
+</style>
---
+++ apps/docs/src/examples/shaders/useFboScope/Scene.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { T, useThrelte } from '@threlte/core'
+
+  import { interactivity, Sky } from '@threlte/extras'
+
+  import DeathValley from './deathValley.svelte'
+  import { Vector3 } from 'three'
+
+  interactivity()
+
+  const { camera } = useThrelte()
+
+  $: {
+    $camera.lookAt(new Vector3(0, 0, 0))
+  }
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[1, 0.5, 1]}
+  fov={80}
+/>
+
+<Sky />
+<DeathValley />
