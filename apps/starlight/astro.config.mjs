0158e7047d6e6814b812553278132c9e9c45c9e4
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-03-01 22:11:57 +1300

    why css?! why you be like this

--- apps/starlight/astro.config.mjs
+++ apps/starlight/astro.config.mjs
@@ -1,32 +1,39 @@
-import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Threlte',
-      social: {
-        github: 'https://github.com/threlte/threlte',
-        'x.com': 'https://twitter.com/threlte',
-        discord: 'https://discord.gg/EqUBCfCaGm'
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' }
-          ]
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' }
-        }
-      ],
-      components: {
-        Header: './src/components/Header/Header.astro'
+  integrations: [starlight({
+    title: 'Threlte',
+    social: {
+      github: 'https://github.com/threlte/threlte',
+      'x.com': 'https://twitter.com/threlte',
+      discord: 'https://discord.gg/EqUBCfCaGm'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
       }
-    })
-  ]
-})
+    }],
+    logo: {
+      replacesTitle: true,
+      src: './src/assets/logo/threlte-logo.png'
+    },
+    components: {
+      Header: './src/components/Header/Header.astro'
+    },
+    plugins: [starlightLinksValidator()]
+  }), tailwind()]
+});
---
+++ apps/starlight/tailwind.config.mts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+export const config: Config = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
+
+export default config
