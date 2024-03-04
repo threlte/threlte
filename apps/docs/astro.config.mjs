4323acca81ae69bfab7ba1ecb659bb87cb273b2f
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-03-04 16:18:44 +1300

    remove packages from top nav. Add reference section to sidebar.

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,20 +1,38 @@
 import { defineConfig } from 'astro/config'
+import { resolve } from 'path'
 import starlight from '@astrojs/starlight'
 import starlightLinksValidator from 'starlight-links-validator'
 
+import AutoImport from 'astro-auto-import'
 import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    AutoImport({
+      imports: [
+        // '$components/Example/Example.astro',
+        '$components/Tip/Tip.astro'
+        // '$components/ManualInstallGuide/ManualInstallGuide.svelte',
+        // '$components/Card/Card.astro'
+      ]
+    }),
+    svelte(),
     starlight({
       title: 'Threlte',
       social: {
         github: 'https://github.com/threlte/threlte',
-        'x.com': 'https://twitter.com/threlte',
-        discord: 'https://discord.gg/EqUBCfCaGm'
+        discord: 'https://discord.gg/EqUBCfCaGm',
+        twitter: 'https://twitter.com/threlte'
       },
       sidebar: [
+        {
+          label: 'Start Here',
+          autogenerate: {
+            directory: 'getting-started'
+          }
+        },
         {
           label: 'Guides',
           items: [
@@ -27,9 +45,36 @@ export default defineConfig({
         },
         {
           label: 'Reference',
-          autogenerate: {
-            directory: 'reference'
-          }
+          items: [
+            {
+              label: 'Core',
+              link: '/reference/core/'
+            },
+            {
+              label: 'Extras',
+              link: '/reference/extras/'
+            },
+            {
+              label: 'Rapier',
+              link: '/reference/rapier/'
+            },
+            {
+              label: 'Theatre',
+              link: '/reference/theatre/'
+            },
+            {
+              label: 'XR',
+              link: '/reference/xr/'
+            },
+            {
+              label: 'Flex',
+              link: '/reference/flex/'
+            },
+            {
+              label: 'gltf',
+              link: '/reference/gltf/'
+            }
+          ]
         }
       ],
       logo: {
