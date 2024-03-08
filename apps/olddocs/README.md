b15638de5112e267283e287192997291c0968e90
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-03-08 13:16:59 +1300

    delete olddocs folder

--- apps/olddocs/README.md
+++
@@ -1,3 +0,0 @@
-## 👀 Our docs are built using Astro
-
-Feel free to ask questions in our [Discord server](https://discord.gg/EqUBCfCaGm) or check out [their documentation](https://docs.astro.build).
--- apps/olddocs/src/examples/extras/positional-audio/Turntable.svelte.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Events, Props, Slots } from '@threlte/core'
-import { SvelteComponent } from 'svelte'
-import type { Group } from 'three'
-
-export default class Turntable extends SvelteComponent<Props<Group>, Events<Group>, Slots<Group>> {}
