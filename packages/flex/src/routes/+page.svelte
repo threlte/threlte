761fc372c443c9e798112a5a9607c2239a735157
Ed Sunman <ed.sunman@freshlabel.co.uk>
2024-04-02 22:46:49 +0100

    removed font-family property

--- packages/flex/src/routes/+page.svelte
+++ packages/flex/src/routes/+page.svelte
@@ -107,18 +107,6 @@
   :global(body) {
     background: #333333;
     margin: 0;
-    font-family:
-      system-ui,
-      -apple-system,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      Oxygen,
-      Ubuntu,
-      Cantarell,
-      'Open Sans',
-      'Helvetica Neue',
-      sans-serif;
   }
 
   nav {
