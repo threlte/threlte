1cb6495e29e21431abdfa0d1887ec5643cfd50eb
Micheal Parks <michealparks1989@gmail.com>
2024-06-23 18:43:10 -0400

    fix lod example

--- apps/docs/src/examples/geometry/LOD/Scene.svelte
+++ apps/docs/src/examples/geometry/LOD/Scene.svelte
@@ -20,28 +20,30 @@
 <T.DirectionalLight position={[3, 10, 10]} />
 <T.HemisphereLight intensity={0.2} />
 
-<T.LOD let:ref={lod}>
-  {#each ['red', 'green', 'blue'] as color, i}
-    <T.Group
-      on:create={({ ref }) => {
-        lod.addLevel(ref, i * 75)
-      }}
-    >
-      <T.Mesh>
-        <T.IcosahedronGeometry args={[10, 3 - i]} />
-        <T.MeshStandardMaterial
-          {color}
-          wireframe
-        />
-      </T.Mesh>
-      <T.Mesh>
-        <T.IcosahedronGeometry args={[10, 3 - i]} />
-        <T.MeshStandardMaterial
-          {color}
-          transparent
-          opacity={0.3}
-        />
-      </T.Mesh>
-    </T.Group>
-  {/each}
+<T.LOD>
+  {#snippet children({ ref: lod })}
+    {#each ['red', 'green', 'blue'] as color, i}
+      <T.Group
+        oncreate={({ ref }) => {
+          lod.addLevel(ref, i * 75)
+        }}
+      >
+        <T.Mesh>
+          <T.IcosahedronGeometry args={[10, 3 - i]} />
+          <T.MeshStandardMaterial
+            {color}
+            wireframe
+          />
+        </T.Mesh>
+        <T.Mesh>
+          <T.IcosahedronGeometry args={[10, 3 - i]} />
+          <T.MeshStandardMaterial
+            {color}
+            transparent
+            opacity={0.3}
+          />
+        </T.Mesh>
+      </T.Group>
+    {/each}
+  {/snippet}
 </T.LOD>
