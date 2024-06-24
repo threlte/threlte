9b5c281ae43cdd443c4430cfba3cfdae5d82f95e
Grischa Erbe <grischa.erbe@googlemail.com>
2024-06-25 00:29:19 +0200

    Separate contexts by domain

--- packages/core/src/lib/hooks/useLoader.ts
+++ packages/core/src/lib/hooks/useLoader.ts
@@ -1,5 +1,5 @@
+import { useCache } from '../context/fragments/cache'
 import { asyncWritable, type AsyncWritable } from '../lib/asyncWritable'
-import { useCache } from '../lib/contexts/cache'
 
 type AsyncLoader = {
   loadAsync: (url: string, onProgress?: (event: ProgressEvent) => void) => Promise<any>
