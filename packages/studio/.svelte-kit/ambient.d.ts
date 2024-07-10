baa36a051fca6de4f4d7ba818fb022be9a576c10
Micheal Parks <michealparks1989@gmail.com>
2024-07-10 19:06:34 -0400

    mdx fixes

---
+++ packages/studio/.svelte-kit/ambient.d.ts
@@ -0,0 +1,327 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const MANPATH: string;
+	export const npm_package_devDependencies_prettier: string;
+	export const npm_package_devDependencies_svelte2tsx: string;
+	export const TERM_PROGRAM: string;
+	export const npm_package_devDependencies_eslint_plugin_svelte: string;
+	export const NODE: string;
+	export const npm_package_devDependencies_prettier_plugin_svelte: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const npm_package_publishOverrides_exports___types: string;
+	export const INIT_CWD: string;
+	export const TERM: string;
+	export const SHELL: string;
+	export const npm_package_scripts_cleanup: string;
+	export const npm_package_devDependencies_vite: string;
+	export const npm_package_devDependencies_rimraf: string;
+	export const HOMEBREW_REPOSITORY: string;
+	export const TMPDIR: string;
+	export const npm_package_scripts_lint: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const npm_package_publishOverrides_exports___extend_import: string;
+	export const npm_package_dependencies__tweakpane_plugin_camerakit: string;
+	export const npm_package_scripts_dev: string;
+	export const ZDOTDIR: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const MallocNanoZone: string;
+	export const npm_package_devDependencies__threlte_extras: string;
+	export const npm_package_devDependencies__sveltejs_kit: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const npm_package_exports___svelte: string;
+	export const npm_config_registry: string;
+	export const PNPM_HOME: string;
+	export const npm_package_publishOverrides_files_0: string;
+	export const USER: string;
+	export const npm_package_license: string;
+	export const npm_package_scripts_check_watch: string;
+	export const npm_package_devDependencies_index_to_position: string;
+	export const COMMAND_MODE: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_package_dependencies_camera_controls: string;
+	export const SSH_AUTH_SOCK: string;
+	export const npm_package_dependencies__mdi_js: string;
+	export const __CF_USER_TEXT_ENCODING: string;
+	export const npm_package_devDependencies_eslint: string;
+	export const npm_package_dependencies_flexible_tree: string;
+	export const npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+	export const npm_package_devDependencies_tslib: string;
+	export const npm_execpath: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_dependencies_tweakpane: string;
+	export const npm_package_publishOverrides_types: string;
+	export const npm_config_frozen_lockfile: string;
+	export const npm_package_devDependencies__typescript_eslint_parser: string;
+	export const PATH: string;
+	export const npm_package_publishOverrides_svelte: string;
+	export const npm_package_exports___extensions_types: string;
+	export const npm_config_engine_strict: string;
+	export const USER_ZDOTDIR: string;
+	export const __CFBundleIdentifier: string;
+	export const npm_package_author: string;
+	export const npm_config_auto_install_peers: string;
+	export const PWD: string;
+	export const npm_command: string;
+	export const npm_package_devDependencies__sveltejs_package: string;
+	export const npm_package_devDependencies_publint: string;
+	export const npm_lifecycle_event: string;
+	export const LANG: string;
+	export const npm_package_name: string;
+	export const npm_package_devDependencies__sveltejs_vite_plugin_svelte: string;
+	export const npm_package_types: string;
+	export const npm_package_svelte: string;
+	export const npm_package_publishOverrides_exports___extend_types: string;
+	export const npm_package_publishOverrides_exports___extend_svelte: string;
+	export const npm_package_devDependencies_estree_walker: string;
+	export const NODE_PATH: string;
+	export const npm_package_exports___types: string;
+	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+	export const XPC_FLAGS: string;
+	export const npm_package_publishOverrides_exports___svelte: string;
+	export const npm_package_dependencies_svelte_tweakpane_ui: string;
+	export const npm_package_exports___extend_types: string;
+	export const npm_package_publishOverrides_exports___import: string;
+	export const npm_package_devDependencies_eslint_config_prettier: string;
+	export const npm_package_publishOverrides_exports___extensions_types: string;
+	export const npm_config_node_gyp: string;
+	export const XPC_SERVICE_NAME: string;
+	export const npm_package_version: string;
+	export const npm_package_devDependencies__sveltejs_adapter_auto: string;
+	export const npm_package_devDependencies_magic_string: string;
+	export const VSCODE_INJECTION: string;
+	export const npm_package_devDependencies_svelte_check: string;
+	export const npm_package_devDependencies_three: string;
+	export const SHLVL: string;
+	export const HOME: string;
+	export const npm_package_devDependencies__threlte_core: string;
+	export const npm_package_type: string;
+	export const VSCODE_GIT_ASKPASS_MAIN: string;
+	export const npm_package_peerDependencies_three: string;
+	export const HOMEBREW_PREFIX: string;
+	export const npm_package_exports___extensions_svelte: string;
+	export const npm_package_devDependencies_vite_dev_rpc: string;
+	export const npm_package_publishOverrides_exports___extensions_svelte: string;
+	export const LOGNAME: string;
+	export const npm_package_scripts_format: string;
+	export const npm_package_devDependencies__types_three: string;
+	export const npm_package_peerDependencies_svelte: string;
+	export const npm_lifecycle_script: string;
+	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const npm_package_scripts_package: string;
+	export const npm_package_publishOverrides_exports___extensions_import: string;
+	export const npm_config_user_agent: string;
+	export const VSCODE_GIT_ASKPASS_NODE: string;
+	export const GIT_ASKPASS: string;
+	export const INFOPATH: string;
+	export const HOMEBREW_CELLAR: string;
+	export const npm_package_devDependencies__types_node: string;
+	export const npm_package_dependencies__floating_ui_dom: string;
+	export const npm_package_scripts_prepare: string;
+	export const npm_package_exports___extend_svelte: string;
+	export const npm_config_link_workspace_packages: string;
+	export const npm_package_devDependencies__yushijinhun_three_minifier_rollup: string;
+	export const npm_package_scripts_check: string;
+	export const COLORTERM: string;
+	export const npm_node_execpath: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		MANPATH: string;
+		npm_package_devDependencies_prettier: string;
+		npm_package_devDependencies_svelte2tsx: string;
+		TERM_PROGRAM: string;
+		npm_package_devDependencies_eslint_plugin_svelte: string;
+		NODE: string;
+		npm_package_devDependencies_prettier_plugin_svelte: string;
+		npm_package_devDependencies_typescript: string;
+		npm_package_publishOverrides_exports___types: string;
+		INIT_CWD: string;
+		TERM: string;
+		SHELL: string;
+		npm_package_scripts_cleanup: string;
+		npm_package_devDependencies_vite: string;
+		npm_package_devDependencies_rimraf: string;
+		HOMEBREW_REPOSITORY: string;
+		TMPDIR: string;
+		npm_package_scripts_lint: string;
+		TERM_PROGRAM_VERSION: string;
+		npm_package_publishOverrides_exports___extend_import: string;
+		npm_package_dependencies__tweakpane_plugin_camerakit: string;
+		npm_package_scripts_dev: string;
+		ZDOTDIR: string;
+		ORIGINAL_XDG_CURRENT_DESKTOP: string;
+		MallocNanoZone: string;
+		npm_package_devDependencies__threlte_extras: string;
+		npm_package_devDependencies__sveltejs_kit: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		npm_package_exports___svelte: string;
+		npm_config_registry: string;
+		PNPM_HOME: string;
+		npm_package_publishOverrides_files_0: string;
+		USER: string;
+		npm_package_license: string;
+		npm_package_scripts_check_watch: string;
+		npm_package_devDependencies_index_to_position: string;
+		COMMAND_MODE: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_package_dependencies_camera_controls: string;
+		SSH_AUTH_SOCK: string;
+		npm_package_dependencies__mdi_js: string;
+		__CF_USER_TEXT_ENCODING: string;
+		npm_package_devDependencies_eslint: string;
+		npm_package_dependencies_flexible_tree: string;
+		npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+		npm_package_devDependencies_tslib: string;
+		npm_execpath: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_dependencies_tweakpane: string;
+		npm_package_publishOverrides_types: string;
+		npm_config_frozen_lockfile: string;
+		npm_package_devDependencies__typescript_eslint_parser: string;
+		PATH: string;
+		npm_package_publishOverrides_svelte: string;
+		npm_package_exports___extensions_types: string;
+		npm_config_engine_strict: string;
+		USER_ZDOTDIR: string;
+		__CFBundleIdentifier: string;
+		npm_package_author: string;
+		npm_config_auto_install_peers: string;
+		PWD: string;
+		npm_command: string;
+		npm_package_devDependencies__sveltejs_package: string;
+		npm_package_devDependencies_publint: string;
+		npm_lifecycle_event: string;
+		LANG: string;
+		npm_package_name: string;
+		npm_package_devDependencies__sveltejs_vite_plugin_svelte: string;
+		npm_package_types: string;
+		npm_package_svelte: string;
+		npm_package_publishOverrides_exports___extend_types: string;
+		npm_package_publishOverrides_exports___extend_svelte: string;
+		npm_package_devDependencies_estree_walker: string;
+		NODE_PATH: string;
+		npm_package_exports___types: string;
+		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+		XPC_FLAGS: string;
+		npm_package_publishOverrides_exports___svelte: string;
+		npm_package_dependencies_svelte_tweakpane_ui: string;
+		npm_package_exports___extend_types: string;
+		npm_package_publishOverrides_exports___import: string;
+		npm_package_devDependencies_eslint_config_prettier: string;
+		npm_package_publishOverrides_exports___extensions_types: string;
+		npm_config_node_gyp: string;
+		XPC_SERVICE_NAME: string;
+		npm_package_version: string;
+		npm_package_devDependencies__sveltejs_adapter_auto: string;
+		npm_package_devDependencies_magic_string: string;
+		VSCODE_INJECTION: string;
+		npm_package_devDependencies_svelte_check: string;
+		npm_package_devDependencies_three: string;
+		SHLVL: string;
+		HOME: string;
+		npm_package_devDependencies__threlte_core: string;
+		npm_package_type: string;
+		VSCODE_GIT_ASKPASS_MAIN: string;
+		npm_package_peerDependencies_three: string;
+		HOMEBREW_PREFIX: string;
+		npm_package_exports___extensions_svelte: string;
+		npm_package_devDependencies_vite_dev_rpc: string;
+		npm_package_publishOverrides_exports___extensions_svelte: string;
+		LOGNAME: string;
+		npm_package_scripts_format: string;
+		npm_package_devDependencies__types_three: string;
+		npm_package_peerDependencies_svelte: string;
+		npm_lifecycle_script: string;
+		VSCODE_GIT_IPC_HANDLE: string;
+		npm_package_scripts_package: string;
+		npm_package_publishOverrides_exports___extensions_import: string;
+		npm_config_user_agent: string;
+		VSCODE_GIT_ASKPASS_NODE: string;
+		GIT_ASKPASS: string;
+		INFOPATH: string;
+		HOMEBREW_CELLAR: string;
+		npm_package_devDependencies__types_node: string;
+		npm_package_dependencies__floating_ui_dom: string;
+		npm_package_scripts_prepare: string;
+		npm_package_exports___extend_svelte: string;
+		npm_config_link_workspace_packages: string;
+		npm_package_devDependencies__yushijinhun_three_minifier_rollup: string;
+		npm_package_scripts_check: string;
+		COLORTERM: string;
+		npm_node_execpath: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
