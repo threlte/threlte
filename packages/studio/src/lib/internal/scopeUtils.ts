4185c943a0d56aa36cca003583289701aa4f9b68
Grischa Erbe <grischa.erbe@googlemail.com>
2024-07-10 22:19:07 +0200

    cleanup

--- packages/studio/src/lib/internal/scopeUtils.ts
+++ packages/studio/src/lib/internal/scopeUtils.ts
@@ -1,7 +1,2 @@
 export const scopeId = (namespace: string, scope: string, key: string) =>
   `${namespace}/${scope}:${key}`
-
-export const unscopeId = (scopedId: string): { scope: string; id: string } => {
-  const [scope, id] = scopedId.split(':')
-  return { scope, id }
-}
