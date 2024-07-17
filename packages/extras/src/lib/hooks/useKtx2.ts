4d024783bf3e6cb596b35c8b94028b037b51acb2
Micheal Parks <michealparks1989@gmail.com>
2024-07-16 20:07:23 -0400

    cleanup

--- packages/extras/src/lib/hooks/useKtx2.ts
+++ packages/extras/src/lib/hooks/useKtx2.ts
@@ -4,13 +4,13 @@ import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js'
 const ktxLoaderInstances: Record<string, KTX2Loader> = {}
 
 export const useKtx2 = (transcoderPath: string): KTX2Loader => {
-  const { renderer } = useThrelte()
   const cachedLoader = ktxLoaderInstances[transcoderPath]
 
   if (cachedLoader) {
     return cachedLoader
   }
 
+  const { renderer } = useThrelte()
   const ktx2Loader = new KTX2Loader()
   ktx2Loader.setTranscoderPath(transcoderPath)
   ktx2Loader.detectSupport(renderer)
