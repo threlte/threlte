14de533daaab5afd31bb746adfd83b01e58b38d6
Grischa Erbe <grischa.erbe@googlemail.com>
2024-07-18 12:42:52 +0200

    refactor

--- packages/core/src/lib/components/T/utils/useEvents.ts
+++ packages/core/src/lib/components/T/utils/useEvents.ts
@@ -26,30 +26,26 @@ export const useEvents = <T>(props: Props = {}) => {
     }
   }
 
-  const addedEventListeners: string[] = []
-
-  const cleanupEventListeners = (ref: EventDispatcher) => {
-    for (let i = 0; i < addedEventListeners.length; i++) {
-      const eventName = addedEventListeners[i]
-      ref.removeEventListener(eventName, eventHandlerProxy)
-    }
-    addedEventListeners.length = 0
-  }
-
   const addEventListeners = (ref: EventDispatcher, props: Props) => {
+    const eventNames: string[] = []
+
     for (const eventName of Object.keys(props)) {
       if (eventName.startsWith('on')) {
         ref.addEventListener(eventName.slice(2), eventHandlerProxy)
-        addedEventListeners.push(eventName)
+        eventNames.push(eventName)
+      }
+    }
+
+    return () => {
+      for (let i = 0; i < eventNames.length; i++) {
+        ref.removeEventListener(eventNames[i], eventHandlerProxy)
       }
     }
   }
 
   const updateRef = (ref: MaybeInstance<T>) => {
     if (!isEventDispatcher(ref)) return
-
-    addEventListeners(ref, props)
-    return () => cleanupEventListeners(ref)
+    return addEventListeners(ref, props)
   }
 
   return {
