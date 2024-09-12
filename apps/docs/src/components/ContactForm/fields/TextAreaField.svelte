8be76f646c67e2e296e81cc06b7b2130e3d89451
Marcin Jerzak <jerzakm@gmail.com>
2024-09-12 11:34:15 +0200

    add services page to next

---
+++ apps/docs/src/components/ContactForm/fields/TextAreaField.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Field from './Field.svelte'
+
+  export let label: string
+  export let rows: number = 10
+  export let id: string = label.toLowerCase().replace(' ', '-')
+  export let required: boolean = false
+  export let value: string | undefined = undefined
+</script>
+
+<Field
+  {label}
+  {id}
+  {required}
+>
+  <textarea
+    type="text"
+    name={id}
+    {rows}
+    {required}
+    bind:value
+  />
+</Field>
