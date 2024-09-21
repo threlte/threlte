3adee88e43c16966659c0fe39268a9bafb57cb5c
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-09-20 20:48:18 -0700

    oops. actually add the docs

---
+++ apps/docs/src/examples/extras/detailed/Scene.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { Detailed, OrbitControls } from '@threlte/extras'
+  import { IcosahedronGeometry } from 'three'
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position.z={3}
+>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<Detailed distances={[0, 5, 10]}>
+  <T.Mesh
+    geometry={new IcosahedronGeometry(1, 2)}
+    material.wireframe={true}
+    material.color={0xff_00_00}
+  />
+  <T.Mesh
+    geometry={new IcosahedronGeometry(1, 1)}
+    material.wireframe={true}
+    material.color={0x00_ff_00}
+  />
+  />
+  <T.Mesh
+    geometry={new IcosahedronGeometry(1, 0)}
+    material.wireframe={true}
+    material.color={0x00_00_ff}
+  />
+</Detailed>
