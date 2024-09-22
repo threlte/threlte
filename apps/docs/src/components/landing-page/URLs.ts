bcf182786ff7381a3838cfc88fbf8ceea6cd4315
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-09-23 11:41:11 +1200

    update labels and urls

--- apps/docs/src/components/landing-page/URLs.ts
+++ apps/docs/src/components/landing-page/URLs.ts
@@ -1,10 +1,11 @@
 export const URLs = {
 	docs: {
-		start: '/learn/getting-started/intro',
-		first: '/learn/getting-started/intro',
-		install: '/learn/getting-started/intro',
-		extras: '/learn/getting-started/intro',
-		plugins: '/learn/getting-started/intro'
+		start: '/learn/getting-started/introduction',
+		first: '/learn/getting-started/your-first-scene',
+		install: '/learn/getting-started/installation',
+		extras: '/reference/extras/getting-started',
+		plugins: '/learn/advanced/plugins',
+		reference: '/reference/core/getting-started'
 	},
 	services: '/services',
 	github: 'https://github.com/threlte/threlte',
--- apps/docs/src/components/packages.ts
+++ apps/docs/src/components/packages.ts
@@ -1,12 +1,12 @@
 export const packages = [
-	{ label: 'Learn', href: '/learn/getting-started/intro', path: '/learn' },
-	{ label: 'Examples', href: '/examples', path: '/examples' },
-	{ label: 'Core', href: '/reference/core/components/example/', path: '/reference/core' },
-	{ label: 'Extras', href: '/reference/extras/example', path: '/reference/extras' }
-	// { label: 'GLTF', href: '/reference/gltf/example', path: '/reference/gltf' },
-	// { label: 'Rapier', href: '/reference/rapier/example', path: '/reference/rapier' },
-	// { label: 'Theatre', href: '/reference/theatre/example', path: '/reference/theatre' },
-	// { label: 'XR', href: '/reference/xr/example', path: '/reference/xr' },
-	// { label: 'Flex', href: '/reference/flex/example', path: '/reference/flex' },
-	// { label: 'Studio', href: '/reference/studio/example', path: '/reference/studio' }
+	{ label: 'Learn', href: '/learn/getting-started/introduction', path: '/learn' },
+	{ label: 'Examples', href: '/examples/Examples', path: '/examples' },
+	{ label: 'Core', href: '/reference/core/getting-started', path: '/reference/core' },
+	{ label: 'Extras', href: '/reference/extras/getting-started', path: '/reference/extras' },
+	{ label: 'GLTF', href: '/reference/gltf/getting-started', path: '/reference/gltf' },
+	{ label: 'Rapier', href: '/reference/rapier/getting-started', path: '/reference/rapier' },
+	{ label: 'Theatre', href: '/reference/theatre/getting-started', path: '/reference/theatre' },
+	{ label: 'XR', href: '/reference/xr/getting-started', path: '/reference/xr' },
+	{ label: 'Flex', href: '/reference/flex/getting-started', path: '/reference/flex' },
+	{ label: 'Studio', href: '/reference/studio/getting-started', path: '/reference/studio' }
 ]
