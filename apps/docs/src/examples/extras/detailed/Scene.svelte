316f948fcefbc01e17221ce46f72ae964da7155e
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-09-25 11:04:55 -0700

    add docs about what distance is used. tweak example for better demo

--- apps/docs/src/examples/extras/detailed/Scene.svelte
+++ apps/docs/src/examples/extras/detailed/Scene.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { Detailed, OrbitControls } from '@threlte/extras'
+  import { Detailed } from '@threlte/extras'
   import { IcosahedronGeometry } from 'three'
-  import { T } from '@threlte/core'
+  import { T, useTask } from '@threlte/core'
 
   type DetailItem = {
     color: number
@@ -13,16 +13,21 @@
     { color: 0x00_ff_00, distance: 5 },
     { color: 0x00_00_ff, distance: 10 }
   ]
+
+  let time = $state(0)
+  const z = $derived(5 + 10 * Math.sin(time))
+
+  useTask((delta) => {
+    time += delta
+  })
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-  position.z={3}
->
-  <OrbitControls />
-</T.PerspectiveCamera>
+  position.z={18}
+/>
 
-<Detailed>
+<Detailed position.z={z}>
   {#each items as { color, distance }, i}
     {@const detail = items.length - i - 1}
     <T.Mesh
