f6783d0104a695787ce0bb08a2db14e3ebe70121
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-09-30 15:49:08 +1300

    packages text always white

--- apps/docs/src/components/PackagesNav.svelte
+++ apps/docs/src/components/PackagesNav.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { packages } from './packages'
+import { onMount } from 'svelte'
+import { packages } from './packages'
 
-	let link: HTMLAnchorElement
-	let url = '/learn'
+let link: HTMLAnchorElement
+let url = '/learn'
 
-	onMount(() => {
-		for (let i = 0; i < packages.length; i++) {
-			if (window.location.pathname.startsWith(packages[i].path)) {
-				url = packages[i].href
-				break
-			}
+onMount(() => {
+	for (let i = 0; i < packages.length; i++) {
+		if (window.location.pathname.startsWith(packages[i].path)) {
+			url = packages[i].href
+			break
 		}
-	})
+	}
+})
 </script>
 
 <div class="flex flex-row items-center">
 	<select
-		class="mr-4 flex shrink rounded-md border border-[var(--sl-color-gray-5)] bg-blue-900 px-3 py-2 text-[var(--sl-color-text)] hover:text-white"
+		class="mr-4 flex shrink rounded-md border border-[var(--sl-color-gray-5)] bg-blue-900 px-3 py-2 text-white"
 		aria-label="Packages and Docs Navigation"
 		bind:value={url}
 		on:change={() => {
