f0c06cdd4ea982fa6daff2f7968800426c4fb217
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-10-01 14:49:21 +1300

    more work on package nav for other languages

--- apps/docs/src/components/PackagesNav.svelte
+++ apps/docs/src/components/PackagesNav.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-import { onMount } from 'svelte'
-import { packages } from './packages'
-
 const { lang } = $props()
+const packages = [
+	{ label: 'Learn', href: '/learn/getting-started/introduction', path: '/learn' },
+	{ label: 'Examples', href: '/examples', path: '/examples' },
+	{ label: 'Core', href: '/reference/core/getting-started', path: '/reference/core' },
+	{ label: 'Extras', href: '/reference/extras/getting-started', path: '/reference/extras' },
+	{ label: 'GLTF', href: '/reference/gltf/getting-started', path: '/reference/gltf' },
+	{ label: 'Rapier', href: '/reference/rapier/getting-started', path: '/reference/rapier' },
+	{ label: 'Theatre', href: '/reference/theatre/getting-started', path: '/reference/theatre' },
+	{ label: 'XR', href: '/reference/xr/getting-started', path: '/reference/xr' },
+	{ label: 'Flex', href: '/reference/flex/getting-started', path: '/reference/flex' },
+	{ label: 'Studio', href: '/reference/studio/getting-started/tutorial', path: '/reference/studio' }
+]
 
 let link: HTMLAnchorElement
-let url = '/learn'
+let url = $state('/learn')
+// lang != 'en' ? '/' + url.split('/').slice(2).join('/') : url
 
-onMount(() => {
+$effect(() => {
 	let pathname = window.location.pathname
 	if (lang != 'en') {
 		pathname = '/' + pathname.split('/').slice(2).join('/')
@@ -29,13 +39,9 @@ onMount(() => {
 		bind:value={url}
 		onchange={() => {
 			if (lang != 'en') {
-				console.log(url);
 				url = `/${lang}${url}`
-				console.log(url);
-				link.click()
-			} else {
-				link.click()
 			}
+			link.click()
 		}}
 	>
 		{#each packages as { label, href }}
