7c6bc86fec8886e872b127c18035736df2317eff
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-10-01 13:26:12 +1300

    add some top margin and fmt

--- apps/docs/src/components/Example/SvelteWrapper.svelte
+++ apps/docs/src/components/Example/SvelteWrapper.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import OpenInStackblitz from './OpenInStackblitz.svelte'
+import OpenInStackblitz from './OpenInStackblitz.svelte'
 
-	export let path: string
-	export let files: Record<string, string>
-	export let hideCode: boolean
-	export let hideStackblitz: boolean = false
+export let path: string
+export let files: Record<string, string>
+export let hideCode: boolean
+export let hideStackblitz: boolean = false
 
-	let _class = ''
-	export { _class as class }
+let _class = ''
+export { _class as class }
 </script>
 
 <div
-	class="relative h-[80vh] w-full overflow-hidden rounded-t-md border border-white/20 {hideCode &&
+	class="relative mt-6 h-[80vh] w-full overflow-hidden rounded-t-md border border-white/20 {hideCode &&
 		'!rounded-md'} {_class}"
 >
 	<iframe src="/examples/{path}" title={path}></iframe>
 
 	{#if !hideStackblitz}
 		<div class="absolute bottom-4 right-4">
-			<OpenInStackblitz {files} />
+			<OpenInStackblitz files={files} />
 		</div>
 	{/if}
 </div>
 
 <style>
-	iframe {
-		position: absolute;
-		left: 0;
-		top: 0;
-		height: 100%;
-		width: 100%;
-		border-style: none;
-	}
+iframe {
+	position: absolute;
+	left: 0;
+	top: 0;
+	height: 100%;
+	width: 100%;
+	border-style: none;
+}
 </style>
