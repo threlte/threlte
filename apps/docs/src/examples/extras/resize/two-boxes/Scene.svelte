77714249b1d5c96f037347455a2e9dacd7691597
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-10-15 13:44:39 -0700

    add resize component with docs and examples

---
+++ apps/docs/src/examples/extras/resize/two-boxes/Scene.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { OrbitControls, Resize } from '@threlte/extras'
+  import { T } from '@threlte/core'
+  import { MeshStandardMaterial } from 'three'
+
+  const material = new MeshStandardMaterial({ color: 'hotpink' })
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={5}
+>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<T.AmbientLight intensity={0.2} />
+<T.DirectionalLight position={[1, 5, 3]} />
+
+<Resize position.x={1}>
+  <T.Mesh>
+    <T.BoxGeometry args={[5, 5, 5]} />
+    <T is={material} />
+  </T.Mesh>
+</Resize>
+
+<T.Mesh position.x={-1}>
+  <T.BoxGeometry />
+  <T is={material} />
+</T.Mesh>
