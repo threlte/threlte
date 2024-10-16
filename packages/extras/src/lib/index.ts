9b1adfba28926a02d9f7348c404eb98604096998
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-10-16 15:37:08 -0700

    remove export of top secret next drei port

--- packages/extras/src/lib/index.ts
+++ packages/extras/src/lib/index.ts
@@ -50,7 +50,6 @@ export { default as BakeShadows } from './components/BakeShadows/BakeShadows.sve
 export { default as Detailed } from './components/Detailed/Detailed.svelte'
 export { default as MeshDiscardMaterial } from './components/MeshDiscardMaterial/MeshDiscardMaterial.svelte'
 export { default as Resize } from './components/Resize/Resize.svelte'
-export { default as LinearGradientTexture } from './components/GradientTexture/LinearGradientTexture.svelte'
 
 // suspense
 export { default as Suspense } from './suspense/Suspense.svelte'
