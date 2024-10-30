181c93e87d72bcd999cf4694b02c02fe63a4e8a3
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-10-30 15:56:03 -0700

    wip

--- apps/docs/src/examples/extras/screen-space/App.svelte
+++ apps/docs/src/examples/extras/screen-space/App.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
+  import { Checkbox, Pane } from 'svelte-tweakpane-ui'
+
+  let autoRotate = $state(true)
 </script>
 
 <div>
+  <Pane
+    position="fixed"
+    title="ScreenSpace"
+  >
+    <Checkbox
+      bind:value={autoRotate}
+      label="auto rotate"
+    />
+  </Pane>
   <Canvas>
-    <Scene />
+    <Scene {autoRotate} />
   </Canvas>
 </div>
 
--- apps/docs/src/examples/extras/screen-space/Scene.svelte
+++ apps/docs/src/examples/extras/screen-space/Scene.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
   import { OrbitControls, ScreenSpace } from '@threlte/extras'
   import { T } from '@threlte/core'
+
+  type SceneProps = {
+    autoRotate: boolean
+  }
+
+  let { autoRotate = false }: SceneProps = $props()
 </script>
 
 <T.PerspectiveCamera
   makeDefault
   position.z={5}
 >
-  <OrbitControls />
+  <OrbitControls
+    {autoRotate}
+    autoRotateSpeed={4}
+  />
 </T.PerspectiveCamera>
 
-<ScreenSpace>
+<T.AmbientLight />
+<T.DirectionalLight position={3} />
+
+<T.Mesh position.x={2}>
+  <T.MeshStandardMaterial color="forestgreen" />
+  <T.BoxGeometry />
+</T.Mesh>
+
+<ScreenSpace depth={-5}>
   <T.Mesh>
-    <T.BoxGeometry />
+    <T.MeshStandardMaterial color="orangered" />
+    <T.PlaneGeometry />
   </T.Mesh>
 </ScreenSpace>
