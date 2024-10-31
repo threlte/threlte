2d3553889c91902e9f5bffd71a5c3853914664db
Grischa Erbe <grischa.erbe@googlemail.com>
2024-10-31 11:12:13 +0100

    Add world space compat to ScreenSpace

--- packages/extras/src/lib/components/ScreenSpace/ScreenSpace.svelte
+++ packages/extras/src/lib/components/ScreenSpace/ScreenSpace.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import type { ScreenSpaceProps } from './ScreenSpace'
-  import { Group } from 'three'
   import { T, useCamera, useTask } from '@threlte/core'
+  import { Group, Quaternion, Vector3 } from 'three'
+  import type { ScreenSpaceProps } from './ScreenSpace'
 
   let {
     depth = 1,
@@ -13,10 +13,26 @@
 
   const { camera: sceneCamera } = useCamera()
 
+  const worldPosition = new Vector3()
+  const parentWorldQuaternion = new Quaternion()
+  const worldQuaternion = new Quaternion()
+
   useTask(() => {
+    const parent = ref.parent
+    if (!parent) return
+
     const _camera = camera ?? sceneCamera.current
-    ref.position.copy(_camera.position)
-    ref.quaternion.copy(_camera.quaternion)
+    _camera.getWorldPosition(worldPosition)
+    _camera.getWorldQuaternion(worldQuaternion)
+
+    // Convert world position to local position
+    parent.worldToLocal(worldPosition)
+    ref.position.copy(worldPosition)
+
+    // Convert world quaternion to local quaternion
+    parent.getWorldQuaternion(parentWorldQuaternion)
+    const localQuaternion = worldQuaternion.premultiply(parentWorldQuaternion.invert())
+    ref.quaternion.copy(localQuaternion)
   })
 </script>
 
