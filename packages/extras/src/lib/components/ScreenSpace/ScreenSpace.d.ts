fff55d5ed7a64410897093ce1859092592d9858c
Grischa Erbe <grischa.erbe@googlemail.com>
2024-10-31 10:58:46 +0100

    Fix type def

--- packages/extras/src/lib/components/ScreenSpace/ScreenSpace.d.ts
+++ packages/extras/src/lib/components/ScreenSpace/ScreenSpace.d.ts
@@ -2,7 +2,7 @@ import type { Camera, Group } from 'three'
 import type { Component } from 'svelte'
 import type { Props } from '@threlte/core'
 
-export type ScreenSpaceProps = Omit<Props<Group>, 'position' | 'quaternion'> & {
+export type ScreenSpaceProps = Props<Group> & {
   /**
    * distance from camera
    * @default 1
@@ -13,6 +13,8 @@ export type ScreenSpaceProps = Omit<Props<Group>, 'position' | 'quaternion'> & {
    * @default useCamera().camera.current
    */
   camera?: Camera
+  position?: never
+  quaternion?: never
 }
 
 declare const ScreenSpace: Component<ScreenSpaceProps>
