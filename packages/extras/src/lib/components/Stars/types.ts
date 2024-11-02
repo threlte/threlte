3e5be6d0553f72da0661af4c0a463d93747d95c0
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-11-02 12:07:37 -0700

    Stars

--- packages/extras/src/lib/components/Stars/types.ts
+++ packages/extras/src/lib/components/Stars/types.ts
@@ -1,17 +1,49 @@
 import type { Props } from '@threlte/core'
-import { SvelteComponent } from 'svelte'
 import type { Points } from 'three'
 
 export type StarsProps = Props<Points> & {
+  /**
+   * @default 50
+   */
   radius?: number
+
+  /**
+   * @default 50
+   */
   depth?: number
+
+  /**
+   * @default 5000
+   */
   count?: number
+
+  /**
+   * @default 6
+   */
   factor?: number
+
+  /**
+   * @default 1.0
+   */
   saturation?: number
+
+  /**
+   * @default 0.8
+   */
   lightness?: number
+
+  /**
+   * @default true
+   */
   fade?: boolean
+
+  /**
+   * @default 1
+   */
   speed?: number
+
+  /**
+   * @default 1.0
+   */
   opacity?: number
 }
-
-export default class Stars extends SvelteComponent<StarsProps> {}
