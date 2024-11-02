4e5fb4638c3ba77feda8e4252f33c758c95a8f2d
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-11-02 11:22:45 -0700

    Grid

--- packages/extras/src/lib/components/Grid/types.ts
+++ packages/extras/src/lib/components/Grid/types.ts
@@ -1,22 +1,80 @@
 import type { Props } from '@threlte/core'
-import type { SvelteComponent } from 'svelte'
 import type { ColorRepresentation, Mesh, Side } from 'three'
 
 export type GridProps = Props<Mesh> & {
+  /**
+   * @default 'xz'
+   */
   plane?: 'xz' | 'xy' | 'zy'
+
+  /**
+   * @default '#000000'
+   */
   cellColor?: ColorRepresentation
+
+  /**
+   * @default 1
+   */
   cellSize?: number
+
+  /**
+   * @default 1
+   */
   cellThickness?: number
+
+  /**
+   * @default '#0000ee'
+   */
   sectionColor?: ColorRepresentation
+
+  /**
+   * @default 10
+   */
   sectionSize?: number
+
+  /**
+   * @default 2
+   */
   sectionThickness?: number
+
+  /**
+   * @default '#dadada'
+   */
   backgroundColor?: ColorRepresentation
+
+  /**
+   * @default 0
+   */
   backgroundOpacity?: number
+
+  /**
+   * @default [20, 20]
+   */
   gridSize?: number | [number, number]
+
+  /**
+   * @default false
+   */
   followCamera?: boolean
+
+  /**
+   * @default false
+   */
   infiniteGrid?: boolean
+
+  /**
+   * @default 100
+   */
   fadeDistance?: number
+
+  /**
+   * @default 1
+   */
   fadeStrength?: number
+
+  /**
+   * @default Three.DoubleSide
+   */
   side?: Side
 } & (
     | {
@@ -37,5 +95,3 @@ export type GridProps = Props<Mesh> & {
         sectionDividers?: number
       }
   )
-
-export default class Grid extends SvelteComponent<GridProps> {}
