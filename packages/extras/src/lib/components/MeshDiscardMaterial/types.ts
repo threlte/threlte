577dba314a10c7204bd3362a56e389c4cdaddb89
Grischa Erbe <grischa.erbe@googlemail.com>
2024-11-04 01:20:58 +0100

    fix types

--- packages/extras/src/lib/components/MeshDiscardMaterial/types.ts
+++ packages/extras/src/lib/components/MeshDiscardMaterial/types.ts
@@ -1,6 +1,7 @@
 import type { Props } from '@threlte/core'
 import type { ShaderMaterial } from 'three'
 
-export type MeshDiscardMaterialProps = Props<
-  Omit<ShaderMaterial, 'fragmentShader' | 'vertexShader'>
+export type MeshDiscardMaterialProps = Omit<
+  Props<ShaderMaterial>,
+  'fragmentShader' | 'vertexShader'
 >
