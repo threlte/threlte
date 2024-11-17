40297720b638ab075b9ebcf3e20352835840645c
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-11-18 12:02:54 +1300

    tailwind and css here because so Example styling so overwriting intended styles.

--- apps/docs/src/examples/extras/view/App.svelte
+++ apps/docs/src/examples/extras/view/App.svelte
@@ -2,33 +2,96 @@
   import { Canvas } from '@threlte/core'
   import { View } from '@threlte/extras'
   import Scene from './Scene.svelte'
-  const things: { dom: HTMLElement }[] = [{ dom: undefined }]
+  import * as THREE from 'three'
+  import type { itemType } from './types'
+
+  const items: itemType[] = []
+  const geometries = [
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.SphereGeometry(0.5, 12, 8),
+    new THREE.DodecahedronGeometry(0.5),
+    new THREE.CylinderGeometry(0.5, 0.5, 1, 12)
+  ]
+  for (let i = 0; i < 40; i++) {
+    // add one random mesh to each scene
+    const geometry = geometries[(geometries.length * Math.random()) | 0]!
+
+    const material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color().setHSL(Math.random(), 1, 0.75, THREE.SRGBColorSpace),
+      roughness: 0.5,
+      metalness: 0,
+      flatShading: true
+    })
+    // @ts-ignore
+    items.push({ dom: undefined, geometry, material })
+  }
 </script>
 
-<div>
-  {#each things as thing}
-    <div
-      bind:this={thing.dom}
-      id="view"
-    ></div>
-  {/each}
-  <Canvas>
-    <Scene />
-    {#each things as thing}
-      <View dom={thing.dom}>
-        <Scene />
-      </View>
+<div
+  id="container"
+  class="bg-white"
+>
+  <div
+    id="content"
+    class="relative z-[1] h-full overflow-y-scroll"
+  >
+    {#each items as item, i}
+      <div
+        id="item"
+        class="m-4 inline-block p-4 shadow-md"
+      >
+        <div
+          bind:this={item.dom}
+          class="h-[200px] w-[200px]"
+        ></div>
+        <div class="mt-2 text-[#888]">Scene {i + 1}</div>
+      </div>
     {/each}
-  </Canvas>
+  </div>
+  <div
+    id="canvas"
+    class="absolute top-0 h-full"
+  >
+    <Canvas>
+      {#each items as item}
+        <View
+          dom={item.dom}
+          background={new THREE.Color(0xe0e0e0)}
+        >
+          <Scene {...item} />
+        </View>
+      {/each}
+    </Canvas>
+  </div>
 </div>
 
 <style>
-  div {
+  div#container {
     height: 100%;
+    background: white;
   }
-  div#view {
-    position: absolute;
-    width: 300px;
-    height: 300px;
+  div#content {
+    position: relative;
+    z-index: 1;
+    height: 100%;
+    overflow-y: scroll;
+  }
+  div#item {
+    margin: 1rem;
+    display: inline-block;
+    padding: 1rem;
+    box-shadow:
+      rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+      rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+      rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+      rgba(0, 0, 0, 0.1) 0px 2px 4px -2px;
+  }
+  div#item > div:first-child {
+    height: 200px;
+    width: 200px;
+  }
+  div#item > div:last-child {
+    margin-top: 0.5rem;
+    color: #888;
   }
 </style>
