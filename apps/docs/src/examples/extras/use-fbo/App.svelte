929162f5cf0ab543c3d1b0ad8dc1aaf446831f01
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-11-23 15:13:35 -0800

    alter example to better show off frame buffer use cases

--- apps/docs/src/examples/extras/use-fbo/App.svelte
+++ apps/docs/src/examples/extras/use-fbo/App.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
+  import { Checkbox, Pane } from 'svelte-tweakpane-ui'
+
+  let useRotatingCamera = $state(false)
 </script>
 
+<Pane position="fixed">
+  <Checkbox
+    bind:value={useRotatingCamera}
+    label="use rotating camera"
+  />
+</Pane>
 <div>
   <Canvas>
-    <Scene />
+    <Scene {useRotatingCamera} />
   </Canvas>
 </div>
 
--- apps/docs/src/examples/extras/use-fbo/Scene.svelte
+++ apps/docs/src/examples/extras/use-fbo/Scene.svelte
@@ -1,57 +1,79 @@
 <script lang="ts">
-  import type { Mesh } from 'three'
-  import { OrbitControls, Sky, useFBO } from '@threlte/extras'
+  import { OrbitControls, Sky, useGltf } from '@threlte/extras'
+  import { Group, Mesh, MeshStandardMaterial, PerspectiveCamera, PlaneGeometry } from 'three'
   import { T, useTask, useThrelte } from '@threlte/core'
+  import { useFBO } from '@threlte/extras'
 
-  const { camera, renderer, scene } = useThrelte()
+  let { useRotatingCamera }: { useRotatingCamera: boolean } = $props()
 
-  const renderTarget = useFBO()
+  const useControlsCamera = $derived(!useRotatingCamera)
 
-  let fboPreviewMesh: Mesh
+  const gltf = useGltf('/models/Duck.glb')
 
-  let knotRotation = 0
+  const { renderer, scene } = useThrelte()
+  const fbo = useFBO()
 
+  const group = new Group()
+
+  const cameraMeshHeight = 1
+  const cameraRotationRadius = 5
+
+  // create the plane and other camera in the script tag so that we don't have to bind to the ref and worry about `undefined` cases
+  const rotatingCamera = new PerspectiveCamera()
+
+  const planeMesh = new Mesh(new PlaneGeometry(), new MeshStandardMaterial({ map: fbo.texture }))
+  planeMesh.position.setZ(-1 * (cameraMeshHeight + cameraRotationRadius))
+  planeMesh.scale.setScalar(10)
+
+  let time = 0
   useTask((delta) => {
-    knotRotation += delta
+    time += delta
+    const c = Math.cos(time)
+    const s = Math.sin(time)
+    rotatingCamera.position.set(cameraRotationRadius * c, 2 * c * s, cameraRotationRadius * s)
+    rotatingCamera.lookAt(group.position)
 
-    // save state before updating
-    const previewMeshVisible = fboPreviewMesh.visible
+    const lastVisible = planeMesh.visible
+    planeMesh.visible = false
     const lastRenderTarget = renderer.getRenderTarget()
 
-    // update state
-    renderer.setRenderTarget(renderTarget)
-    fboPreviewMesh.visible = false
+    renderer.setRenderTarget(fbo)
+    renderer.render(scene, rotatingCamera)
 
-    renderer.render(scene, camera.current)
-
-    // restore previous state
-    fboPreviewMesh.visible = previewMeshVisible
+    planeMesh.visible = lastVisible
     renderer.setRenderTarget(lastRenderTarget)
   })
 </script>
 
 <T.PerspectiveCamera
-  makeDefault
-  position={[5, 1, 5]}
+  position.x={5}
+  position.y={5}
+  position.z={10}
+  makeDefault={useControlsCamera}
 >
-  <OrbitControls />
+  <OrbitControls enabled={useControlsCamera} />
 </T.PerspectiveCamera>
+
+<T.AmbientLight intensity={0.5} />
 
 <Sky />
 
-<T.Mesh
-  rotation.x={knotRotation}
-  rotation.z={knotRotation}
+<T
+  is={rotatingCamera}
+  makeDefault={useRotatingCamera}
 >
-  <T.TorusKnotGeometry />
-  <T.MeshStandardMaterial color="orangered" />
-</T.Mesh>
+  <T.Mesh rotation.x={0.5 * Math.PI}>
+    <T.CylinderGeometry
+      args={[0.25 * cameraMeshHeight, 0.5 * cameraMeshHeight, cameraMeshHeight]}
+    />
+    <T.MeshStandardMaterial color="orangered" />
+  </T.Mesh>
+</T>
 
-<T.Mesh
-  position.z={-5}
-  bind:ref={fboPreviewMesh}
-  scale={5}
->
-  <T.PlaneGeometry />
-  <T.MeshStandardMaterial map={renderTarget.texture} />
-</T.Mesh>
+<T is={planeMesh} />
+
+<T is={group}>
+  {#await gltf then { scene }}
+    <T is={scene} />
+  {/await}
+</T>
