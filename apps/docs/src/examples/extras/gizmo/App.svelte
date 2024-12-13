4a599ef25780178df01db4f3859e74dd195062c8
Micheal Parks <michealparks1989@gmail.com>
2024-12-13 14:14:09 -0500

    initial

--- apps/docs/src/examples/extras/gizmo/App.svelte
+++ apps/docs/src/examples/extras/gizmo/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Canvas, T, currentWritable, type CurrentWritable } from '@threlte/core'
-  import { Gizmo, OrbitControls } from '@threlte/extras'
+  import { Gizmo, type GizmoOptions, OrbitControls } from '@threlte/extras'
   import {
     Checkbox,
     Color,
@@ -14,18 +14,18 @@
   import { OrbitControls as ThreeOrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
   import Scene from './Scene.svelte'
 
-  let turnRate = 2 * Math.PI
-  let verticalPlacement: 'top' | 'bottom' = 'bottom'
-  let horizontalPlacement: 'left' | 'right' = 'left'
+  let type = $state('sphere')
+  let speed = $state(1)
+  let placement: GizmoOptions['placement'] = $state('bottom-left')
   let xColor = '#ff3653'
   let yColor = '#8adb00'
   let zColor = '#2c8fff'
-  let toneMapped = false
-  let size = 128
-  let paddingX = 20
-  let paddingY = 20
+  let size = $state(86)
+  let left = $state(0)
+  let top = $state(0)
+  let right = $state(0)
+  let bottom = $state(0)
 
-  let orbitControls: ThreeOrbitControls
   let center: CurrentWritable<[number, number, number]> = currentWritable([0, 0, 0])
 </script>
 
@@ -34,69 +34,68 @@
   position="fixed"
   title="Gizmo"
 >
-  <Folder title="Mechanics">
-    <Ring
-      label="turnRate"
-      bind:value={turnRate}
-      min={0.1}
-      max={20}
-      wide
-    />
-  </Folder>
+  <List
+    label="type"
+    bind:value={type}
+    options={{
+      sphere: 'sphere',
+      cube: 'cube'
+    }}
+  />
+  <Slider
+    label="speed"
+    bind:value={speed}
+    min={0.1}
+    max={1}
+  />
 
-  <Folder title="Colors">
-    <Color
-      bind:value={xColor}
-      label="xColor"
-    />
-    <Color
-      bind:value={yColor}
-      label="yColor"
-    />
-    <Color
-      bind:value={zColor}
-      label="zColor"
-    />
-    <Checkbox
-      bind:value={toneMapped}
-      label="toneMapped"
-    />
-  </Folder>
-
-  <Folder title="Placement">
-    <List
-      label="verticalPlacement"
-      bind:value={verticalPlacement}
-      options={{
-        top: 'top',
-        bottom: 'bottom'
-      }}
-    />
-    <List
-      label="horizontalPlacement"
-      bind:value={horizontalPlacement}
-      options={{
-        left: 'left',
-        right: 'right'
-      }}
-    />
+  <List
+    label="placement"
+    bind:value={placement}
+    options={[
+      'top-left',
+      'top-center',
+      'top-right',
+      'center-left',
+      'center-center',
+      'center-right',
+      'bottom-left',
+      'bottom-center',
+      'bottom-right'
+    ]}
+  />
+  <Slider
+    label="size"
+    bind:value={size}
+    min={20}
+    max={350}
+    step={1}
+  />
+  <Folder title="offset">
     <Slider
-      label="size"
-      bind:value={size}
-      min={20}
-      max={350}
-      step={1}
-    />
-    <Slider
-      label="paddingX"
-      bind:value={paddingX}
+      label="top"
+      bind:value={top}
       min={0}
       max={50}
       step={1}
     />
     <Slider
-      label="paddingY"
-      bind:value={paddingY}
+      label="left"
+      bind:value={left}
+      min={0}
+      max={50}
+      step={1}
+    />
+    <Slider
+      label="right"
+      bind:value={right}
+      min={0}
+      max={50}
+      step={1}
+    />
+    <Slider
+      label="bottom"
+      bind:value={bottom}
       min={0}
       max={50}
       step={1}
@@ -112,28 +111,22 @@
       fov={36}
       target={[0, 0, 0]}
     >
-      <OrbitControls
-        bind:ref={orbitControls}
-        onchange={() => {
-          center.set([orbitControls.target.x, orbitControls.target.y, orbitControls.target.z])
-        }}
-      />
+      <OrbitControls>
+        <Gizmo
+          {type}
+          {speed}
+          {placement}
+          {size}
+          offset={{
+            top,
+            left,
+            bottom,
+            right
+          }}
+        />
+      </OrbitControls>
     </T.PerspectiveCamera>
 
     <Scene center={$center} />
-
-    <Gizmo
-      center={$center}
-      {turnRate}
-      {verticalPlacement}
-      {horizontalPlacement}
-      {xColor}
-      {yColor}
-      {zColor}
-      {toneMapped}
-      {size}
-      {paddingX}
-      {paddingY}
-    />
   </Canvas>
 </div>
