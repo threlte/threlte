d11b7754a1c83c4d835a14a7f62e8ce4c4cb9c52
Grischa Erbe <grischa.erbe@googlemail.com>
2024-12-20 08:29:55 +0100

    Clean up observer

--- packages/extras/src/lib/components/View/OffscreenObserver.svelte.ts
+++ packages/extras/src/lib/components/View/OffscreenObserver.svelte.ts
@@ -1,5 +1,4 @@
 import { observe } from '@threlte/core'
-import { untrack } from 'svelte'
 
 export class OffscreenObserver {
   isOffscreen = $state(false)
@@ -10,13 +9,15 @@ export class OffscreenObserver {
       ([element]) => {
         const observer = new IntersectionObserver((entries) => {
           for (const entry of entries) {
-            untrack(() => {
-              this.isOffscreen = !entry.isIntersecting
-            })
+            this.isOffscreen = !entry.isIntersecting
           }
         })
 
         observer.observe(element)
+
+        return () => {
+          observer.disconnect()
+        }
       }
     )
   }
