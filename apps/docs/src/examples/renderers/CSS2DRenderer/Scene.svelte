7c108f16437bfee7098baf917178471c5b2befd9
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-12-27 17:23:30 -0800

    donezo

--- apps/docs/src/examples/renderers/CSS2DRenderer/Scene.svelte
+++ apps/docs/src/examples/renderers/CSS2DRenderer/Scene.svelte
@@ -1,22 +1,34 @@
 <script lang="ts">
-  import { T, useTask, useThrelte } from '@threlte/core'
-  import { OrbitControls } from '@threlte/extras'
-  import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js'
   import CounterLabel from './CounterLabel.svelte'
   import CssObject from './CssObject.svelte'
+  import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js'
+  import { OrbitControls } from '@threlte/extras'
+  import { T, useTask, useThrelte } from '@threlte/core'
+  import type { ColorRepresentation, Vector3Tuple } from 'three'
 
-  const { scene, size, autoRenderTask, camera } = useThrelte()
+  let { element }: { element: HTMLElement } = $props()
 
-  // Set up the CSS2DRenderer to run in a div placed atop the <Canvas>
-  const element = document.querySelector('#css-renderer-target') as HTMLElement
+  const { autoRenderTask, camera, scene, size } = useThrelte()
+
+  // note that the renderer won't be reactive if `element` updates
+  // you'd have to do `$derived(new CSS2DRenderer({element}))` if you'd want that to be the case
   const cssRenderer = new CSS2DRenderer({ element })
-  $: cssRenderer.setSize($size.width, $size.height)
+
+  $effect(() => {
+    cssRenderer.setSize($size.width, $size.height)
+  })
 
   // We are running two renderers, and don't want to run
   // updateMatrixWorld twice; tell the renderers that we'll handle
   // it manually.
   // https://threejs.org/docs/#api/en/core/Object3D.updateWorldMatrix
+  const last = scene.matrixWorldAutoUpdate
   scene.matrixWorldAutoUpdate = false
+  $effect(() => {
+    return () => {
+      scene.matrixWorldAutoUpdate = last
+    }
+  })
 
   // To update the matrices *once* per frame, we'll use a task that is added
   // right before the autoRenderTask. This way, we can be sure that the
@@ -40,6 +52,30 @@
       autoInvalidate: false
     }
   )
+
+  type CssObjectParams = {
+    color: ColorRepresentation
+    label: string
+    position: Vector3Tuple
+  }
+
+  const params: CssObjectParams[] = [
+    {
+      color: '#4F6FF6',
+      label: 'Hello',
+      position: [-1, 2, 1]
+    },
+    {
+      color: '#6FF64F',
+      label: 'CSS',
+      position: [1, 2, 1]
+    },
+    {
+      color: '#F64F6F',
+      label: 'Renderer',
+      position: [1, 2, -1]
+    }
+  ]
 </script>
 
 <T.PerspectiveCamera
@@ -56,38 +92,18 @@
   <T.MeshStandardMaterial color="#F64F6F" />
 </T.Mesh>
 
-<CssObject
-  position={[-1, 2, 1]}
-  center={[0, 0.5]}
->
-  <CounterLabel label="Hello" />
+{#each params as { color, label, position }}
+  <CssObject
+    {position}
+    center={[0, 0.5]}
+  >
+    {#snippet content()}
+      <CounterLabel {label} />
+    {/snippet}
 
-  <T.Mesh slot="three">
-    <T.SphereGeometry args={[0.25]} />
-    <T.MeshStandardMaterial color="#4F6FF6" />
-  </T.Mesh>
-</CssObject>
-
-<CssObject
-  position={[1, 2, 1]}
-  center={[0, 0.5]}
->
-  <CounterLabel label="CSS" />
-
-  <T.Mesh slot="three">
-    <T.SphereGeometry args={[0.25]} />
-    <T.MeshStandardMaterial color="#6FF64F" />
-  </T.Mesh>
-</CssObject>
-
-<CssObject
-  position={[1, 2, -1]}
-  center={[0, 0.5]}
->
-  <CounterLabel label="Renderer" />
-
-  <T.Mesh slot="three">
-    <T.SphereGeometry args={[0.25]} />
-    <T.MeshStandardMaterial color="#F64F6F" />
-  </T.Mesh>
-</CssObject>
+    <T.Mesh>
+      <T.SphereGeometry args={[0.25]} />
+      <T.MeshStandardMaterial {color} />
+    </T.Mesh>
+  </CssObject>
+{/each}
