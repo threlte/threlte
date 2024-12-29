b4e4023ef09d2f52b5a6b538dacca5844ef213c8
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-12-29 14:07:08 -0800

    add instructions

--- apps/docs/src/examples/meshline/cursor-lines/App.svelte
+++ apps/docs/src/examples/meshline/cursor-lines/App.svelte
@@ -3,6 +3,14 @@
   import Scene from './Scene.svelte'
 </script>
 
+<p>mouse around the canvas</p>
 <Canvas>
   <Scene />
 </Canvas>
+
+<style>
+  p {
+    color: white;
+    position: fixed;
+  }
+</style>
