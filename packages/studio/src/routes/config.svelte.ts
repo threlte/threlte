efcad6aa7855bc369d5c194f306e078ff3c1c8ad
Grischa Erbe <grischa.erbe@googlemail.com>
2025-01-05 01:05:42 +0100

    better example

--- packages/studio/src/routes/config.svelte.ts
+++ packages/studio/src/routes/config.svelte.ts
@@ -1,12 +1,13 @@
 import { StaticState } from '@threlte/studio'
 
 export class SceneConfig extends StaticState {
+  grid = $state({ x: 5, y: 5 })
   /**
    * @min 0
    * @max 5
    * @step 1
    */
-  grid = $state({ x: 5, y: 4 })
-  color = $state('#592020')
+  count = $state(4)
+  color = $state('#ff0000')
   camera = $state({ x: 10, y: 23, z: 10 })
 }
