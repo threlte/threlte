7bbdd485162288471c339e6a677dc513e82528ea
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2025-01-10 17:51:58 -0800

    simplify and loop over cubes in the use task callback

--- apps/docs/src/examples/marching-cubes/lava-lamp/MarchingCube.ts
+++ apps/docs/src/examples/marching-cubes/lava-lamp/MarchingCube.ts
@@ -3,9 +3,9 @@ import { Group } from 'three'
 
 export class MarchingCube extends Group {
   constructor(
+    public color = new Color(),
     public strength = 0.5,
-    public subtract = 12,
-    public color = new Color()
+    public subtract = 12
   ) {
     super()
   }
