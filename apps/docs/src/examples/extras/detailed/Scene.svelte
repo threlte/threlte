f140f38d4b15e17e5353bb625fb1d8986f653824
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2025-01-11 17:35:02 -0800

    simplify camera and distances

--- apps/docs/src/examples/extras/detailed/Scene.svelte
+++ apps/docs/src/examples/extras/detailed/Scene.svelte
@@ -11,8 +11,8 @@
 
   const items: DetailItem[] = [
     { color: 0xff_00_00, distance: 0 },
-    { color: 0x00_ff_00, distance: 5 },
-    { color: 0x00_00_ff, distance: 10 }
+    { color: 0x00_ff_00, distance: 3 },
+    { color: 0x00_00_ff, distance: 6 }
   ]
 
   let detailed = $state<LOD>()
@@ -20,14 +20,9 @@
   let time = 0
   useTask((delta) => {
     time += delta
-    detailed?.position.setZ(5 + 10 * Math.sin(time))
+    detailed?.position.setZ(3 * Math.sin(time))
   })
 </script>
-
-<T.PerspectiveCamera
-  makeDefault
-  position.z={18}
-/>
 
 <Detailed bind:ref={detailed}>
   {#each items as { color, distance }, i}
