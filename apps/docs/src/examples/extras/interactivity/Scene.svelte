2a31f3e7a22975597a4cf09bcc5c981830e91f4c
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2025-01-12 17:51:51 -0800

    updated interactivity example to use Spring classes

--- apps/docs/src/examples/extras/interactivity/Scene.svelte
+++ apps/docs/src/examples/extras/interactivity/Scene.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
+  import type { Vector2Tuple } from 'three'
   import { Grid, interactivity, OrbitControls } from '@threlte/extras'
-  import { spring } from 'svelte/motion'
-  import Box from './Box.svelte'
+  import { Spring } from 'svelte/motion'
+  import { T } from '@threlte/core'
+  import { useCursor } from '@threlte/extras'
 
   interactivity()
 
-  const pos = spring({ x: 0, z: 0 })
-  const setRandomPos = () => {
-    pos.set({
-      x: Math.random() * 10 - 5,
-      z: Math.random() * 10 - 5
-    })
-  }
+  const boxPosition = new Spring<Vector2Tuple>([0, 0])
+  const random = () => 10 * Math.random() - 5
+  const scale = new Spring(1)
+  const boxSize = 1
+  const positionY = $derived(0.5 * boxSize * scale.current)
+
+  const { onPointerEnter, onPointerLeave } = useCursor()
+
+  const notHoveringColor = '#ffffff'
+  const hoveringColor = '#fe3d00'
+  let color = $state(notHoveringColor)
 </script>
 
 <T.OrthographicCamera
   zoom={80}
-  position={[10, 10, 10]}
+  position={10}
   makeDefault
 >
   <OrbitControls />
@@ -26,14 +31,31 @@
 <T.AmbientLight intensity={0.4} />
 <T.DirectionalLight position={[1, 2, 5]} />
 
-<Box
-  onclick={setRandomPos}
-  position.x={$pos.x}
-  position.z={$pos.z}
-/>
+<T.Mesh
+  onclick={() => {
+    boxPosition.target = [random(), random()]
+  }}
+  onpointerenter={() => {
+    onPointerEnter()
+    scale.target = 2
+    color = hoveringColor
+  }}
+  onpointerleave={() => {
+    onPointerLeave()
+    scale.target = 1
+    color = notHoveringColor
+  }}
+  scale={scale.current}
+  position.x={boxPosition.current[0]}
+  position.y={positionY}
+  position.z={boxPosition.current[1]}
+>
+  <T.BoxGeometry args={[boxSize, boxSize, boxSize]} />
+  <T.MeshStandardMaterial {color} />
+</T.Mesh>
 
 <Grid
-  position.y={-0.001}
+  position.y={-1 * 0.5}
   cellColor="#ffffff"
   sectionColor="#ffffff"
   sectionThickness={0}
