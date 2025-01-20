13b4ee8c12ed33fb1c5562c8a4dba6e8f6751523
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2025-01-19 16:04:25 -0800

    feat: add screen-space-quad postprocessing example and docs

---
+++ apps/docs/src/examples/postprocessing/screen-quad/App.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Pane, Slider } from 'svelte-tweakpane-ui'
+  import Scene from './Scene.svelte'
+  import { Canvas } from '@threlte/core'
+
+  /**
+   * how fast to cycle
+   */
+  let frequency = $state(2)
+
+  /**
+   * how much the effect will come through
+   * 0 -> 1 since gl colors are in this range
+   */
+  let amplitude = $state(0.2)
+</script>
+
+<Pane position="fixed">
+  <Slider
+    label="amplitude"
+    bind:value={amplitude}
+    min={0}
+    max={1}
+    step={0.1}
+  />
+  <Slider
+    label="frequency"
+    bind:value={frequency}
+    min={1}
+    max={10}
+    step={1}
+  />
+</Pane>
+
+<Canvas autoRender={false}>
+  <Scene
+    {amplitude}
+    {frequency}
+  />
+</Canvas>
---
+++ apps/docs/src/examples/postprocessing/screen-quad/Scene.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import ScreenQuadGeometry, { vertexShader } from './ScreenQuadGeometry.svelte'
+  import { Scene, Uniform, Vector2 } from 'three'
+  import { T, useTask, useThrelte } from '@threlte/core'
+  import { Environment, OrbitControls, useFBO, useGltf } from '@threlte/extras'
+
+  let { amplitude = 1, frequency = 1 }: { amplitude?: number; frequency?: number } = $props()
+
+  const { camera, renderStage, renderer, scene, size } = useThrelte()
+
+  const fbo = useFBO()
+  const _scene = new Scene()
+
+  const uTime = new Uniform(0)
+  useTask(
+    (delta) => {
+      uTime.value += delta
+      const last = renderer.getRenderTarget()
+      renderer.setRenderTarget(fbo)
+      renderer.render(scene, camera.current)
+      renderer.setRenderTarget(last)
+      renderer.render(_scene, camera.current)
+    },
+    { stage: renderStage }
+  )
+
+  const fragmentShader = `
+		precision highp float;
+		uniform sampler2D uScene;
+		uniform vec2 uResolution;
+		uniform float uTime;
+		uniform float uAmplitude;
+		uniform float uFrequency;
+		void main() {
+			vec2 uv = gl_FragCoord.xy / uResolution.xy;
+			vec3 color = texture2D(uScene, uv).rgb;
+			color.b = uAmplitude * (1.0 + sin(uFrequency * uTime));
+			gl_FragColor = vec4(color, 1.0);
+		}
+	`
+
+  const uScene = new Uniform(fbo.texture)
+  const uResolution = new Uniform(new Vector2())
+
+  $effect(() => {
+    uResolution.value.set($size.width, $size.height)
+  })
+
+  const gltf = useGltf('/public/models/spaceships/Bob.gltf')
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={5}
+>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<!-- this geometry is so simple that frustrum culling it would actually be more work -->
+<T.Mesh
+  frustrumCulled={false}
+  attach={_scene}
+>
+  <T.RawShaderMaterial
+    {vertexShader}
+    {fragmentShader}
+    uniforms={{
+      uAmplitude: {
+        value: 1
+      },
+      uTime: {
+        value: 0
+      },
+      uFrequency: {
+        value: 1
+      }
+    }}
+    uniforms.uScene={uScene}
+    uniforms.uResolution={uResolution}
+    uniforms.uTime={uTime}
+    uniforms.uAmplitude.value={amplitude}
+    uniforms.uFrequency.value={frequency}
+  />
+  <ScreenQuadGeometry />
+</T.Mesh>
+
+{#await gltf then { scene }}
+  <T is={scene} />
+{/await}
+
+<Environment
+  url="/textures/equirectangular/hdr/shanghai_riverside_1k.hdr"
+  isBackground
+/>
