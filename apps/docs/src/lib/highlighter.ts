c4a87fa0cb14c632ebf6e17e2bba185a3f97e78e
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-01-28 15:57:18 +1300

    update shiki

--- apps/docs/src/lib/highlighter.ts
+++ apps/docs/src/lib/highlighter.ts
@@ -1,27 +1,19 @@
-import { getHighlighter, setCDN, type Highlighter } from 'shiki'
+import { createHighlighter } from 'shiki'
 
-setCDN('https://unpkg.com/shiki/')
-
-let highlighter: Promise<Highlighter>
-
-export const getShiki = () => {
-  if (highlighter) return highlighter
-  highlighter = getHighlighter({
-    theme: 'dracula-soft',
-    langs: [
-      'astro',
-      'typescript',
-      'javascript',
-      'svelte',
-      'astro',
-      'markdown',
-      'mdx',
-      'md',
-      'json',
-      'html',
-      'bash',
-      'glsl'
-    ]
-  })
-  return highlighter
-}
+export const highlighter = await createHighlighter({
+  themes: ['dracula-soft'],
+  langs: [
+    'astro',
+    'typescript',
+    'javascript',
+    'svelte',
+    'astro',
+    'markdown',
+    'mdx',
+    'md',
+    'json',
+    'html',
+    'bash',
+    'glsl'
+  ]
+})
