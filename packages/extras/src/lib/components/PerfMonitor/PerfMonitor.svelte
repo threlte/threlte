eab6c4812921e82a17d90805969ad3ea06f3b756
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-04-01 13:54:02 +1300

    use stats-gl for PerfMonitor

--- packages/extras/src/lib/components/PerfMonitor/PerfMonitor.svelte
+++ packages/extras/src/lib/components/PerfMonitor/PerfMonitor.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
   import { useTask, useThrelte } from '@threlte/core'
-  import { Pane, AutoValue, FpsGraph } from 'svelte-tweakpane-ui'
+  import { Pane, AutoValue, Element } from 'svelte-tweakpane-ui'
+  import Stats from 'stats-gl'
+  import type { PerfMonitorProps } from './types'
 
-  const { renderer } = useThrelte()
+  let { extraInfo = false, ...rest }: PerfMonitorProps = $props()
+  const { renderer, dom } = useThrelte()
 
+  const stats = new Stats(rest)
+
+  stats.init(renderer)
+
+  let el: HTMLDivElement | undefined = $state()
   let calls = $state(0)
-
   let geometries = $state(0)
   let textures = $state(0)
   let triangles = $state(0)
   let points = $state(0)
   let lines = $state(0)
+
+  $effect(() => {
+    if (extraInfo) {
+      el?.appendChild(stats.dom)
+    } else {
+      dom.append(stats.dom)
+    }
+  })
 
   useTask(() => {
     const { geometries: geo, textures: t } = renderer.info.memory
@@ -21,45 +36,47 @@
     points = p
     calls = c
     lines = l
+    stats.update()
   })
 </script>
 
-<Pane
-  title="Performance Monitor"
-  position="fixed"
->
-  <FpsGraph
-    disabled
-    rows={2}
-  />
-  <AutoValue
-    value={geometries.toFixed(0)}
-    label="geometries"
-    disabled
-  />
-  <AutoValue
-    value={textures.toFixed(0)}
-    label="textures"
-    disabled
-  />
-  <AutoValue
-    value={triangles.toFixed(0)}
-    label="triangles"
-    disabled
-  />
-  <AutoValue
-    value={points.toFixed(0)}
-    label="points"
-    disabled
-  />
-  <AutoValue
-    value={lines.toFixed(0)}
-    label="lines"
-    disabled
-  />
-  <AutoValue
-    value={calls.toFixed(0)}
-    label="draw calls"
-    disabled
-  />
-</Pane>
+{#if extraInfo}
+  <Pane
+    title="Performance Monitor"
+    position="fixed"
+  >
+    <Element>
+      <div bind:this={el}></div>
+    </Element>
+    <AutoValue
+      value={calls.toFixed(0)}
+      label="draw calls"
+      disabled
+    />
+    <AutoValue
+      value={triangles.toFixed(0)}
+      label="triangles"
+      disabled
+    />
+    <AutoValue
+      value={geometries.toFixed(0)}
+      label="geometries"
+      disabled
+    />
+    <AutoValue
+      value={textures.toFixed(0)}
+      label="textures"
+      disabled
+    />
+    <AutoValue
+      value={points.toFixed(0)}
+      label="points"
+      disabled
+    />
+    <AutoValue
+      value={lines.toFixed(0)}
+      label="lines"
+      disabled
+    />
+  </Pane>
+{/if}
