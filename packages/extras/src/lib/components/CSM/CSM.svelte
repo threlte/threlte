5c54191cd2a028cf4cca8f0b9f42558a68dabf9c
Micheal Parks <michealparks1989@gmail.com>
2025-05-08 14:09:58 +0200

    migrate csm to runes

--- packages/extras/src/lib/components/CSM/CSM.svelte
+++ packages/extras/src/lib/components/CSM/CSM.svelte
@@ -1,118 +1,134 @@
 <script lang="ts">
-  import { currentWritable, useTask, useThrelte, watch } from '@threlte/core'
-  import { onDestroy } from 'svelte'
-  import { writable } from 'svelte/store'
+  import { observe, useTask, useThrelte } from '@threlte/core'
+  import type { Snippet } from 'svelte'
   import type { Camera, ColorRepresentation, Vector3Tuple } from 'three'
   import type { CSMParameters } from 'three/examples/jsm/csm/CSM.js'
   import { CSM } from 'three/examples/jsm/csm/CSM.js'
-  import { useMaterials } from './useMaterials'
+  import { useMaterials } from './useMaterials.svelte'
 
-  /**
-   * Whether or not CSM is enabled. If `enabled={false}`, a slot named
-   * `"disabled"` will be rendered.
-   */
-  export let enabled = true
-  /**
-   * The arguments to pass to the CSM constructor.
-   */
-  export let args: Partial<CSMParameters> = {}
-  /**
-   * The camera to use for CSM. Defaults to the camera set by `makeDefault`.
-   */
-  export let camera: Camera | undefined = undefined
-  /**
-   * A configuration callback, which is triggered when CSM is activated. This
-   * callback facilitates advanced configurations, such as enabling the fade
-   * feature.
-   */
-  export let configure: ((csm: CSM) => void) | undefined = undefined
+  interface Props {
+    /**
+     * Whether or not CSM is enabled. If `enabled={false}`, a slot named
+     * `"disabled"` will be rendered.
+     */
+    enabled: boolean
+    /**
+     * The arguments to pass to the CSM constructor.
+     */
+    args: Partial<CSMParameters>
+    /**
+     * The camera to use for CSM. Defaults to the camera set by `makeDefault`.
+     */
+    camera: Camera | undefined
+    /**
+     * A configuration callback, which is triggered when CSM is activated. This
+     * callback facilitates advanced configurations, such as enabling the fade
+     * feature.
+     */
+    configure: ((csm: CSM) => void) | undefined
 
-  export let lightIntensity: number | undefined = undefined
-  export let lightColor: ColorRepresentation | undefined = undefined
-  export let lightDirection: Vector3Tuple = [1, -1, 1]
+    lightIntensity: number | undefined
+    lightColor: ColorRepresentation | undefined
+    lightDirection: Vector3Tuple
+    children?: Snippet
+    fallback?: Snippet
+  }
 
-  const enabledStore = writable(enabled)
-  $: enabledStore.set(enabled)
+  let {
+    enabled = true,
+    args = {},
+    camera,
+    configure,
+    lightIntensity,
+    lightColor,
+    lightDirection = [1, -1, 1],
+    children,
+    fallback
+  }: Props = $props()
 
   const { camera: defaultCamera, scene, size } = useThrelte()
 
-  const csmStore = currentWritable<CSM | undefined>(undefined)
+  let csm = $state.raw<CSM>()
 
-  useTask(() => $csmStore?.update(), { autoInvalidate: false })
+  useTask(() => csm?.update(), { autoInvalidate: false })
 
   const { onNewMaterial, allMaterials } = useMaterials()
 
   const disposeCsm = () => {
-    $csmStore?.remove()
-    $csmStore?.dispose()
-    $csmStore = undefined
+    csm?.remove()
+    csm?.dispose()
+    csm = undefined
   }
 
-  watch([size, csmStore], ([_, csm]) => {
-    if (!csm) return
-    csm.updateFrustums()
-  })
+  observe(
+    () => [enabled],
+    ([enabled]) => {
+      if (enabled) {
+        const nextCSM = new CSM({
+          camera: camera ?? $defaultCamera,
+          parent: scene,
+          ...args
+        })
+        configure?.(nextCSM)
+        for (const material of allMaterials) {
+          nextCSM.setupMaterial(material)
+        }
+        onNewMaterial((material) => nextCSM.setupMaterial(material))
+        csm = nextCSM
+      } else {
+        onNewMaterial(undefined)
+        disposeCsm()
+      }
+    }
+  )
 
-  const cameraStore = writable<Camera | undefined>(camera)
-  $: cameraStore.set(camera)
+  observe(
+    () => [size, csm],
+    ([_, csm]) => {
+      csm?.updateFrustums()
+    }
+  )
 
   // set any CSM props that require frustum updates
-  watch([defaultCamera, cameraStore, csmStore], ([defaultCamera, camera, csm]) => {
-    if (!csm) return
-    csm.camera = camera ?? defaultCamera
-    if (args.maxFar !== undefined) csm.maxFar = args.maxFar
-    if (args.mode !== undefined) csm.mode = args.mode
-    csm.updateFrustums()
-  })
-
-  watch(enabledStore, (enabled) => {
-    if (enabled) {
-      const csm = new CSM({
-        camera: camera ?? $defaultCamera,
-        parent: scene,
-        ...args
-      })
-      configure?.(csm)
-      for (const material of allMaterials) {
-        csm.setupMaterial(material)
-      }
-      onNewMaterial((material) => csm.setupMaterial(material))
-      csmStore.set(csm)
-    } else {
-      onNewMaterial(undefined)
-      disposeCsm()
+  observe(
+    () => [defaultCamera, camera, csm],
+    ([defaultCamera, camera, csm]) => {
+      if (!csm) return
+      csm.camera = camera ?? defaultCamera
+      if (args.maxFar !== undefined) csm.maxFar = args.maxFar
+      if (args.mode !== undefined) csm.mode = args.mode
+      csm.updateFrustums()
     }
+  )
+
+  observe(
+    () => [csm, lightIntensity, lightColor],
+    ([csm, intensity, color]) => {
+      csm?.lights.forEach((light) => {
+        if (intensity !== undefined) {
+          light.intensity = intensity / Math.PI
+        }
+        if (color !== undefined) {
+          light.color.set(color)
+        }
+      })
+    }
+  )
+
+  observe(
+    () => [csm, lightDirection],
+    ([csm, direction]) => {
+      csm?.lightDirection.set(...direction).normalize()
+    }
+  )
+
+  $effect.pre(() => {
+    return disposeCsm
   })
-
-  const lightIntensityStore = writable<typeof lightIntensity>(lightIntensity)
-  $: lightIntensityStore.set(lightIntensity)
-
-  const lightColorStore = writable<typeof lightColor>(lightColor)
-  $: lightColorStore.set(lightColor)
-
-  watch([csmStore, lightIntensityStore, lightColorStore], ([csm, intensity, color]) => {
-    csm?.lights.forEach((light) => {
-      if (intensity !== undefined) {
-        light.intensity = intensity / Math.PI
-      }
-      if (color !== undefined) {
-        light.color.set(color)
-      }
-    })
-  })
-
-  const lightDirectionStore = writable(lightDirection)
-  $: lightDirectionStore.set(lightDirection)
-
-  watch([csmStore, lightDirectionStore], ([csm, direction]) => {
-    csm?.lightDirection.set(...direction).normalize()
-  })
-
-  onDestroy(disposeCsm)
 </script>
 
-<slot />
+{@render children?.()}
 
 {#if !enabled}
-  <slot name="disabled" />
+  {@render fallback?.()}
 {/if}
