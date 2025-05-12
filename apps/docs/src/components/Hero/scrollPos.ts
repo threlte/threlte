ebf326c07f4ac15a0ea30e0db8cf5d049535260b
Grischa Erbe <grischa.erbe@googlemail.com>
2025-05-12 07:19:27 +0200

    revert scrollPos changes

--- apps/docs/src/components/Hero/scrollPos.ts
+++ apps/docs/src/components/Hero/scrollPos.ts
@@ -1,5 +1,5 @@
 import { currentWritable } from '@threlte/core'
-import { Spring } from 'svelte/motion'
+import { spring } from 'svelte/motion'
 import { derived } from 'svelte/store'
 
 export const scrollPos = currentWritable(0)
@@ -7,7 +7,7 @@ export const scrollPos = currentWritable(0)
 /**
  * do not use, may be negative!
  */
-export const _springScrollPos = new Spring(0, {
+export const _springScrollPos = spring(0, {
   precision: 0.0000001,
   damping: 0.95,
   stiffness: 0.1
@@ -19,4 +19,4 @@ export const springScrollPos = derived(_springScrollPos, (_springScrollPos) =>
 
 export const mouseCoords = currentWritable({ x: 0, y: 0 })
 
-export const mouseCoordsSpring = new Spring({ x: 0, y: 0 })
+export const mouseCoordsSpring = spring({ x: 0, y: 0 })
