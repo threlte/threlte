0817794f50d8c6c746c3d0a9b995033fd759ae8c
Micheal Parks <michealparks1989@gmail.com>
2025-06-15 14:11:29 -0400

    add camera-controls component

---
+++ apps/docs/src/examples/extras/camera-controls/Scene.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Mesh, type ColorRepresentation } from 'three'
+  import { T } from '@threlte/core'
+  import { Grid, CameraControls, type CameraControlsRef } from '@threlte/extras'
+
+  let {
+    color = '#ff3e00',
+    controls = $bindable(),
+    mesh = $bindable()
+  }: {
+    color?: ColorRepresentation
+    controls?: CameraControlsRef
+    mesh?: Mesh
+  } = $props()
+</script>
+
+<CameraControls
+  bind:ref={controls}
+  oncreate={(ref) => ref.setPosition(5, 5, 5)}
+/>
+
+<T.Mesh
+  bind:ref={mesh}
+  position.y={0.5}
+>
+  <T.BoxGeometry />
+  <T.MeshBasicMaterial
+    {color}
+    wireframe
+  />
+</T.Mesh>
+
+<Grid
+  sectionColor={color}
+  sectionThickness={1}
+  cellColor="#cccccc"
+  gridSize={40}
+/>
