e28eed74baa593168031c137edcdeb733bb212b6
Micheal Parks <michealparks1989@gmail.com>
2025-06-17 17:12:10 -0400

    fix memory leak

--- packages/extras/src/lib/interactivity/setupInteractivity.ts
+++ packages/extras/src/lib/interactivity/setupInteractivity.ts
@@ -20,8 +20,6 @@ const DOM_EVENTS = [
   ['pointercancel', true]
 ] as const
 
-type DomEventName = (typeof DOM_EVENTS)[number][0]
-
 export const setupInteractivity = (context: InteractivityContext) => {
   const { handlers } = useInteractivity()
 
@@ -84,145 +82,153 @@ export const setupInteractivity = (context: InteractivityContext) => {
     }
   }
 
-  const getEventHandler = (name: DomEventName): ((event: DomEvent) => void) => {
-    // Deal with cancelation
-    if (name === 'pointerleave' || name === 'pointercancel') {
-      return () => {
-        context.pointerOverTarget.set(false)
-        cancelPointer([])
+  const handlePointerLeave = () => {
+    context.pointerOverTarget.set(false)
+    cancelPointer([])
+  }
+
+  const handlePointerEnter = () => {
+    context.pointerOverTarget.set(true)
+  }
+
+  const handleEvent = (event: DomEvent) => {
+    const name = event.type
+    const isPointerMove = name === 'pointermove'
+    const isClickEvent = name === 'click' || name === 'contextmenu' || name === 'dblclick'
+
+    /**
+     * Will set up the raycaster. The default implementation will use the
+     * mouse position on the renderers domElement.
+     */
+    context.compute(event, context)
+
+    const hits = getHits()
+    const delta = isClickEvent ? calculateDistance(event) : 0
+
+    // Save initial coordinates on pointer-down
+    if (name === 'pointerdown') {
+      context.initialClick = [event.offsetX, event.offsetY]
+      context.initialHits = hits.map((hit) => hit.eventObject)
+    }
+
+    // If a click yields no results, pass it back to the user as a miss
+    // Missed events have to come first in order to establish user-land side-effect clean up
+    if (isClickEvent && hits.length === 0) {
+      if (delta <= 2) {
+        pointerMissed(event, context.interactiveObjects)
       }
     }
 
-    if (name === 'pointerenter') {
-      return () => {
-        context.pointerOverTarget.set(true)
-      }
-    }
+    // Take care of unhover
+    if (isPointerMove) cancelPointer(hits)
 
-    return (event: DomEvent) => {
-      const isPointerMove = name === 'pointermove'
-      const isClickEvent = name === 'click' || name === 'contextmenu' || name === 'dblclick'
+    let stopped = false
 
-      /**
-       * Will set up the raycaster. The default implementation will use the
-       * mouse position on the renderers domElement.
-       */
-      context.compute(event, context)
+    // loop through all hits and dispatch events
+    dispatchEvents: for (const hit of hits) {
+      const events = handlers.get(hit.eventObject)
 
-      const hits = getHits()
-      const delta = isClickEvent ? calculateDistance(event) : 0
+      if (!events) continue
 
-      // Save initial coordinates on pointer-down
-      if (name === 'pointerdown') {
-        context.initialClick = [event.offsetX, event.offsetY]
-        context.initialHits = hits.map((hit) => hit.eventObject)
-      }
-
-      // If a click yields no results, pass it back to the user as a miss
-      // Missed events have to come first in order to establish user-land side-effect clean up
-      if (isClickEvent && hits.length === 0) {
-        if (delta <= 2) {
-          pointerMissed(event, context.interactiveObjects)
-        }
-      }
-
-      // Take care of unhover
-      if (isPointerMove) cancelPointer(hits)
-
-      let stopped = false
-
-      // loop through all hits and dispatch events
-      dispatchEvents: for (const hit of hits) {
-        const intersectionEvent: IntersectionEvent<DomEvent> = {
-          stopped,
-          ...hit,
-          intersections: hits,
-          stopPropagation() {
-            stopped = true
-            intersectionEvent.stopped = true
-            if (
-              context.hovered.size > 0 &&
-              Array.from(context.hovered.values()).some((i) => i.eventObject === hit.eventObject)
-            ) {
-              // Objects cannot flush out higher up objects that have already caught the event
-              const higher = hits.slice(0, hits.indexOf(hit))
-              cancelPointer([...higher, hit])
-            }
-          },
-          camera: context.raycaster.camera,
-          delta,
-          nativeEvent: event,
-          pointer: context.pointer.current,
-          ray: context.raycaster.ray
-        }
-
-        const events = handlers.get(hit.eventObject)
-
-        if (!events) return
-
-        if (isPointerMove) {
-          // Move event ...
-
+      const intersectionEvent: IntersectionEvent<DomEvent> = {
+        stopped,
+        ...hit,
+        intersections: hits,
+        stopPropagation() {
+          stopped = true
+          intersectionEvent.stopped = true
           if (
-            events.onpointerover ||
-            events.onpointerenter ||
-            events.onpointerout ||
-            events.onpointerleave
+            context.hovered.size > 0 &&
+            Array.from(context.hovered.values()).some((i) => i.eventObject === hit.eventObject)
           ) {
-            const id = getIntersectionId(intersectionEvent)
-            const hoveredItem = context.hovered.get(id)
-            if (!hoveredItem) {
-              // If the object wasn't previously hovered, book it and call its handler
-              context.hovered.set(id, intersectionEvent)
-              events.onpointerover?.(intersectionEvent as IntersectionEvent<PointerEvent>)
-              events.onpointerenter?.(intersectionEvent as IntersectionEvent<PointerEvent>)
-            } else if (hoveredItem.stopped) {
-              // If the object was previously hovered and stopped, we shouldn't allow other items to proceed
-              intersectionEvent.stopPropagation()
-            }
+            // Objects cannot flush out higher up objects that have already caught the event
+            const higher = hits.slice(0, hits.indexOf(hit))
+            cancelPointer([...higher, hit])
           }
+        },
+        camera: context.raycaster.camera,
+        delta,
+        nativeEvent: event,
+        pointer: context.pointer.current,
+        ray: context.raycaster.ray
+      }
 
-          // Call pointer move
-          events.onpointermove?.(intersectionEvent as IntersectionEvent<PointerEvent>)
-        } else {
-          // All other events
-          if (events[`on${name}`]) {
-            if (!isClickEvent || context.initialHits.includes(hit.eventObject)) {
-              // Missed events have to come first
-              pointerMissed(
-                event,
-                context.interactiveObjects.filter((object) => !context.initialHits.includes(object))
-              )
+      if (isPointerMove) {
+        // Move event ...
 
-              // Call the event
-              events[`on${name}`]?.(intersectionEvent)
-            }
-          } else {
-            // "Real" click event
-            if (isClickEvent && context.initialHits.includes(hit.eventObject)) {
-              pointerMissed(
-                event,
-                context.interactiveObjects.filter((object) => !context.initialHits.includes(object))
-              )
-            }
+        if (
+          events.onpointerover ||
+          events.onpointerenter ||
+          events.onpointerout ||
+          events.onpointerleave
+        ) {
+          const id = getIntersectionId(intersectionEvent)
+          const hoveredItem = context.hovered.get(id)
+          if (!hoveredItem) {
+            // If the object wasn't previously hovered, book it and call its handler
+            context.hovered.set(id, intersectionEvent)
+            events.onpointerover?.(intersectionEvent as IntersectionEvent<PointerEvent>)
+            events.onpointerenter?.(intersectionEvent as IntersectionEvent<PointerEvent>)
+          } else if (hoveredItem.stopped) {
+            // If the object was previously hovered and stopped, we shouldn't allow other items to proceed
+            intersectionEvent.stopPropagation()
           }
         }
 
-        if (stopped) break dispatchEvents
+        // Call pointer move
+        events.onpointermove?.(intersectionEvent as IntersectionEvent<PointerEvent>)
+      } else {
+        // All other events
+        if (events[`on${name}`]) {
+          if (!isClickEvent || context.initialHits.includes(hit.eventObject)) {
+            // Missed events have to come first
+            pointerMissed(
+              event,
+              context.interactiveObjects.filter((object) => !context.initialHits.includes(object))
+            )
+
+            // Call the event
+            events[`on${name}`]?.(intersectionEvent)
+          }
+        } else {
+          // "Real" click event
+          if (isClickEvent && context.initialHits.includes(hit.eventObject)) {
+            pointerMissed(
+              event,
+              context.interactiveObjects.filter((object) => !context.initialHits.includes(object))
+            )
+          }
+        }
       }
+
+      if (stopped) break dispatchEvents
     }
   }
 
   const disconnect = (target: HTMLElement) => {
-    DOM_EVENTS.forEach(([eventName]) => {
-      target.removeEventListener(eventName, getEventHandler(eventName))
-    })
+    for (const [eventName] of DOM_EVENTS) {
+      if (eventName === 'pointerleave' || eventName === 'pointercancel') {
+        target.removeEventListener(eventName, handlePointerLeave)
+      } else if (eventName === 'pointerenter') {
+        target.removeEventListener(eventName, handlePointerEnter)
+      } else {
+        target.removeEventListener(eventName, handleEvent)
+      }
+    }
   }
 
   const connect = (target: HTMLElement) => {
-    DOM_EVENTS.forEach(([eventName, passive]) => {
-      target.addEventListener(eventName, getEventHandler(eventName), { passive })
-    })
+    for (const [eventName, passive] of DOM_EVENTS) {
+      console.log(eventName, passive)
+
+      if (eventName === 'pointerleave' || eventName === 'pointercancel') {
+        target.addEventListener(eventName, handlePointerLeave, { passive })
+      } else if (eventName === 'pointerenter') {
+        target.addEventListener(eventName, handlePointerEnter, { passive })
+      } else {
+        target.addEventListener(eventName, handleEvent, { passive })
+      }
+    }
   }
 
   watch(context.target, (target) => {
