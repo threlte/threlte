e7f27a3f68bf7cb92f27957c36a7ce78a1b5b5c1
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-08-30 12:02:47 +1200

    begin astro collections to content Layer API migratation

--- apps/docs/src/content.config.ts
+++ apps/docs/src/content.config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 export const componentSignature = z.object({
   pretext: z.string().optional(),
@@ -77,6 +78,7 @@ export const referenceCategories = [
 
 // 2. Define your collection(s)
 export const referenceCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/reference' }),
   schema: z.object({
     schemaType: z.string().default('reference'),
     type: z.enum(['component', 'hook', 'plugin']).optional(),
@@ -94,6 +96,7 @@ export const referenceCollection = defineCollection({
 })
 
 export const learnCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/learn' }),
   schema: z.object({
     schemaType: z.string().default('learn'),
     category: z.enum(['Getting Started', 'Basics', 'Advanced', 'More', 'Preprocessing']),
@@ -105,6 +108,7 @@ export const learnCollection = defineCollection({
 })
 
 export const examplesCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/examples' }),
   schema: z.object({
     schemaType: z.string().default('examples'),
     order: z.number().optional()
@@ -112,6 +116,7 @@ export const examplesCollection = defineCollection({
 })
 
 export const testimonialsCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/testimonials' }),
   schema: z.object({
     schemaType: z.string().default('testimonials'),
     image: z.string(),
@@ -123,6 +128,7 @@ export const testimonialsCollection = defineCollection({
 })
 
 export const showcaseCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/showcase' }),
   schema: z.object({
     schemaType: z.string().default('showcase'),
     image: z.string(),
@@ -135,6 +141,7 @@ export const showcaseCollection = defineCollection({
 })
 
 export const authorsCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/authors' }),
   schema: z.object({
     schemaType: z.string().default('authors'),
     name: z.string(),
@@ -143,6 +150,7 @@ export const authorsCollection = defineCollection({
 })
 
 export const blogCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     schemaType: z.string().default('blog'),
     author: z.string(),
