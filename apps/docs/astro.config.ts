4d7ffd8f851c4f552c2331f27581526f1b37baa8
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-08-31 11:53:58 +1200

    proxySsrExternalModules no longer needed

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -67,11 +67,6 @@ export default defineConfig({
       // "@theatre/core" needs to be externalized in development mode but not in production!
       noExternal: noExternal
     },
-    legacy: {
-      // vite 5 changed how externalized modules work - need to use this flag to keep old behaviour
-      // https://vitejs.dev/guide/migration#ssr-externalized-modules-value-now-matches-production
-      proxySsrExternalModules: true
-    },
     optimizeDeps: {
       esbuildOptions: {
         target: 'esnext'
