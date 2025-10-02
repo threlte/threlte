5e6d2f8c81dc5c0b7c46bc55b7b4b4566f57ae3b
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-10-02 14:26:37 -0500

    update screen-quad example

--- apps/docs/src/examples/postprocessing/screen-quad/Scene.svelte
+++ apps/docs/src/examples/postprocessing/screen-quad/Scene.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
   import ScreenQuadGeometry, { vertexShader } from './ScreenQuadGeometry.svelte'
-  import { Environment, OrbitControls, useGltf } from '@threlte/extras'
-  import { Scene, Uniform, WebGLRenderTarget } from 'three'
+  import { Environment, OrbitControls, useFBO, useGltf } from '@threlte/extras'
+  import { OrthographicCamera, Scene, Uniform } from 'three'
   import { T, useTask, useThrelte } from '@threlte/core'
 
-  const { camera, renderStage, renderer, scene, size } = useThrelte()
+  const { camera, renderStage, renderer, scene } = useThrelte()
 
-  const target = new WebGLRenderTarget(1, 1)
-
-  $effect(() => {
-    target.setSize($size.width, $size.height)
+  useGltf('/models/spaceships/Bob.gltf').then((gltf) => {
+    scene.add(gltf.scene)
   })
 
+  const target = useFBO()
+
+  // this scene will hold the triangle and nothing else. remember, the idea is to render a triangle that uses the main scene as a texture and do interesting effects with it
   const _scene = new Scene()
+
+  // the triangle mesh covers the screen, it doesn't really matter what camera we use but most post-processing passes will use an orthographic camera
+  const ortho = new OrthographicCamera()
 
   useTask(
     () => {
       const last = renderer.getRenderTarget()
+
+      // set and draw to thet target that will be sent into the custom shader as a texture
       renderer.setRenderTarget(target)
       renderer.render(scene, camera.current)
+
+      // restore the previous target and render the triangle scene
       renderer.setRenderTarget(last)
-      renderer.render(_scene, camera.current)
+      renderer.render(_scene, ortho)
     },
     {
       stage: renderStage
@@ -28,51 +36,49 @@
   )
 
   /**
-   * put your interesting effects in this shader.
+   * put your interesting effects in this shader. note that your shader will probaby not need some of the stuff below
    */
   const fragmentShader = `
 		precision highp float;
 
 		uniform sampler2D uScene;
-		uniform vec2 uMouse;
 		uniform float uRadius;
 		varying vec2 vUv;
 
 		void main() {
-
-			vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+			gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
 			vec2 center = vec2(0.5, 0.5);
 
 			if (length(center - vUv) - uRadius < 0.0) {
-				color = texture2D(uScene, vUv);
+				gl_FragColor = texture2D(uScene, vUv);
 			}
-
-			gl_FragColor = color;
 		}
 	`
 
-  const gltf = useGltf('/models/spaceships/Bob.gltf')
-
   const uScene = new Uniform(target.texture)
-
   const uRadius = new Uniform(0)
+  const uniforms = {
+    uScene,
+    uRadius
+  }
 
   let time = 0
   useTask((delta) => {
     time += delta
+
+    // oscillate the radius between 0 and 1
     uRadius.value = 1 - 0.5 * (1 + Math.sin(time))
   })
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-  position={5}
+  position={7}
 >
   <OrbitControls />
 </T.PerspectiveCamera>
 
-<!-- this geometry is so simple that frustrum culling it would actually be more work -->
 <T.Mesh
   frustrumCulled={false}
   attach={_scene}
@@ -80,15 +86,10 @@
   <T.RawShaderMaterial
     {vertexShader}
     {fragmentShader}
-    uniforms.uScene={uScene}
-    uniforms.uRadius={uRadius}
+    {uniforms}
   />
   <ScreenQuadGeometry />
 </T.Mesh>
-
-{#await gltf then { scene }}
-  <T is={scene} />
-{/await}
 
 <Environment
   url="/textures/equirectangular/hdr/shanghai_riverside_1k.hdr"
