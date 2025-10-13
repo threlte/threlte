c1e2b77608012be7aba0e1aee873aa8999a43664
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-10-14 10:51:18 +1300

    move learn content

--- apps/docs/src/content.config.ts
+++ apps/docs/src/content.config.ts
@@ -1,5 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
+import { docsLoader } from '@astrojs/starlight/loaders'
+import { docsSchema } from '@astrojs/starlight/schema'
 import { glob } from 'astro/loaders'
 
 export const componentSignature = z.object({
@@ -163,6 +165,7 @@ export const blogCollection = defineCollection({
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
   reference: referenceCollection,
   learn: learnCollection,
   testimonials: testimonialsCollection,
