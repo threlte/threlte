ffb04016270454d260ae690b25a13c1ac1d21c25
Micheal Parks <michealparks1989@gmail.com>
2026-03-31 13:23:57 -0400

    clean up vite

--- packages/extras/vite.config.js
+++ packages/extras/vite.config.js
@@ -1,4 +1,3 @@
-import { resolve } from 'path'
 import { threeMinifier } from '@yushijinhun/three-minifier-rollup'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
@@ -12,38 +11,7 @@ export default defineConfig(({ mode }) => ({
   ...(mode === 'test' && {
     optimizeDeps: {
       exclude: ['@threlte/core', '@threlte/extras', '@threlte/test'],
-      entries: ['src/lib/interactivity/__tests__/**/*.spec.ts'],
-      include: [
-        'three',
-        'three-mesh-bvh',
-        'three/examples/jsm/controls/OrbitControls.js',
-        'three/examples/jsm/controls/TrackballControls.js',
-        'three/examples/jsm/controls/TransformControls.js',
-        'three/examples/jsm/effects/AsciiEffect.js',
-        'three/examples/jsm/geometries/DecalGeometry.js',
-        'three/examples/jsm/geometries/TextGeometry.js',
-        'three/examples/jsm/loaders/DRACOLoader.js',
-        'three/examples/jsm/loaders/EXRLoader.js',
-        'three/examples/jsm/loaders/FontLoader.js',
-        'three/examples/jsm/loaders/GLTFLoader.js',
-        'three/examples/jsm/loaders/HDRCubeTextureLoader.js',
-        'three/examples/jsm/loaders/KTX2Loader.js',
-        'three/examples/jsm/loaders/RGBELoader.js',
-        'three/examples/jsm/loaders/SVGLoader.js',
-        'three/examples/jsm/libs/meshopt_decoder.module.js',
-        'three/examples/jsm/objects/GroundedSkybox.js',
-        'three/examples/jsm/objects/Sky.js',
-        'three/examples/jsm/shaders/HorizontalBlurShader.js',
-        'three/examples/jsm/shaders/VerticalBlurShader.js',
-        'three/examples/jsm/utils/BufferGeometryUtils.js',
-        'three/examples/jsm/csm/CSM.js',
-        'camera-controls',
-        'three-viewport-gizmo',
-        'three-perf',
-        'troika-three-text',
-        '@threejs-kit/instanced-sprite-mesh',
-        'mitt',
-      ]
+      entries: ['src/lib/**/*.svelte', 'src/lib/**/*.ts']
     }
   }),
   test: {
