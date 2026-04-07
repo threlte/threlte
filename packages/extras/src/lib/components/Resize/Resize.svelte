af8054423c6f04ae301d3536647edefc95d1209f
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2026-04-06 19:32:10 -0500

    prevent resize calculation being called every frame

--- packages/extras/src/lib/components/Resize/Resize.svelte
+++ packages/extras/src/lib/components/Resize/Resize.svelte
@@ -2,86 +2,66 @@
   lang="ts"
   module
 >
-  // reuse box for all instances
   const _box = new Box3()
+  const _diff = new Vector3()
 </script>
 
 <script lang="ts">
-  import {
-    isInstanceOf,
-    observe,
-    T,
-    useStage,
-    useTask,
-    useThrelte,
-    type Plugin
-  } from '@threlte/core'
-  import { Box3, Group } from 'three'
+  import { isInstanceOf, observe, T, useStage, useTask, useThrelte } from '@threlte/core'
+  import { type Plugin } from '@threlte/core'
+  import { Box3, Group, Vector3 } from 'three'
   import InjectPlugin from '../InjectPlugin/InjectPlugin.svelte'
   import type { ResizeProps } from './types'
 
   const { renderStage } = useThrelte()
 
   let {
-    axis,
+    axis = 'max',
     auto = false,
     box = _box,
-    precise,
+    precise = false,
     onresize,
     stage = useStage('<Resize>', { before: renderStage }),
-    ref = $bindable(),
+    ref = $bindable(new Group()),
     children,
     ...props
   }: ResizeProps = $props()
 
-  ref = new Group()
   const inner = new Group()
+
   const outer = new Group()
 
   const doResize = () => {
     outer.matrixWorld.identity()
     const { max, min } = box.setFromObject(inner, precise)
-    const width = max.x - min.x
-    const height = max.y - min.y
-    const depth = max.z - min.z
-
-    const denominator =
-      axis === 'x'
-        ? width
-        : axis === 'y'
-          ? height
-          : axis === 'z'
-            ? depth
-            : Math.max(width, height, depth)
-
-    outer.scale.setScalar(1 / denominator)
+    _diff.subVectors(max, min)
+    const scale =
+      1 / (axis === 'max' ? Math.max(..._diff) : axis === 'min' ? Math.min(..._diff) : _diff[axis])
+    outer.scale.setScalar(scale)
     onresize?.()
   }
 
-  const { start: scheduleResizing, stop } = useTask(
+  const { start, stop } = useTask(
     () => {
       doResize()
       stop()
     },
-    { autoStart: false, stage }
+    { autoStart: false, stage, autoInvalidate: false }
   )
 
   /** Manually trigger resizing */
-  export const resize = scheduleResizing
+  export const resize = start
 
-  observe(() => [axis, precise], scheduleResizing)
+  observe(() => [axis, precise], resize)
 
   const plugin: Plugin = (args) => {
-    if (!isInstanceOf(args.ref, 'Object3D')) return
-    observe.pre(
-      () => [args.ref],
-      () => {
-        if (auto) scheduleResizing()
-        return () => {
-          if (auto) scheduleResizing()
-        }
+    $effect(() => {
+      if (!isInstanceOf(args.ref, 'Object3D')) return
+      resize()
+      return () => {
+        resize()
       }
-    )
+    })
   }
 </script>
 
@@ -95,7 +75,7 @@
         name="resize"
         {plugin}
       >
-        {@render children?.({ ref, resize: scheduleResizing })}
+        {@render children?.({ ref, resize })}
       </InjectPlugin>
     </T>
   </T>
--- packages/extras/src/lib/components/Resize/types.ts
+++ packages/extras/src/lib/components/Resize/types.ts
@@ -10,14 +10,16 @@ export type ResizeProps = Props<Group, [{ ref: Group; resize(): void }]> & {
   /**
    * Specifies which axis to constrain. If not provided, the maximum axis is
    * used
+   * @default 'max'
    */
-  axis?: 'x' | 'y' | 'z'
+  axis?: 'x' | 'y' | 'z' | 'min' | 'max'
   /**
    * Bring your own box to capture the bounding box.
    */
   box?: Box3
   /**
-   * If true, use precise bounding box calculation.
+   * If true, use precise bounding box calculation which is less performant but more accurate
+   * @default false
    */
   precise?: boolean
   /**
