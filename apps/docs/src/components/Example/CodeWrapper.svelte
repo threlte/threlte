fa2e65ed1729a482fc358da31c79e9baaee1ff78
Micheal Parks <michealparks1989@gmail.com>
2026-04-30 00:47:47 -0400

    add fullscreen docs button

--- apps/docs/src/components/Example/CodeWrapper.svelte
+++ apps/docs/src/components/Example/CodeWrapper.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { CodeXml } from 'lucide-svelte'
   import CodeExplorer from './CodeExplorer.svelte'
   import { fade } from 'svelte/transition'
   import type { Snippet } from 'svelte'
@@ -80,21 +81,7 @@
         class="border-orange/10 text-orange z-10 flex flex-row items-center justify-center gap-3 rounded-xs border bg-orange-800/50 px-2 py-1 text-sm backdrop-blur-md hover:bg-orange-800/70 hover:text-orange-400 focus:outline-hidden"
         onclick={() => (expanded = true)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="76"
-          height="76"
-          viewBox="0 0 256 256"
-          class="h-5 w-5 fill-current"
-        >
-          <path
-            d="M240,128l-48,40H64L16,128,64,88H192Z"
-            opacity="0.2"
-          />
-          <path
-            d="M69.12,94.15,28.5,128l40.62,33.85a8,8,0,1,1-10.24,12.29l-48-40a8,8,0,0,1,0-12.29l48-40a8,8,0,0,1,10.24,12.3Zm176,27.7-48-40a8,8,0,1,0-10.24,12.3L227.5,128l-40.62,33.85a8,8,0,1,0,10.24,12.29l48-40a8,8,0,0,0,0-12.29ZM162.73,32.48a8,8,0,0,0-10.25,4.79l-64,176a8,8,0,0,0,4.79,10.26A8.14,8.14,0,0,0,96,224a8,8,0,0,0,7.52-5.27l64-176A8,8,0,0,0,162.73,32.48Z"
-          />
-        </svg>
+        <CodeXml class="h-5 w-5" />
         <span>Show Code</span>
       </button>
     </div>
