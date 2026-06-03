a298f36076f28b9ff8909879ef9f843c14965e5f
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-06-04 10:35:29 +1200

    removing funbit dep

--- apps/docs/src/examples/geometry/random-placement/variable-poisson-random/assets/tree.svelte
+++ apps/docs/src/examples/geometry/random-placement/variable-poisson-random/assets/tree.svelte
@@ -1,79 +1,47 @@
 <script lang="ts">
-  import * as THREE from 'three'
   import { T } from '@threlte/core'
-  import { useGltf, useTexture, InstancedMesh, Instance } from '@threlte/extras'
+  import { InstancedMesh, Instance } from '@threlte/extras'
 
   interface Props {
     transformData?: [number, number, number, number][]
   }
 
   let { transformData = [] }: Props = $props()
-
-  type GLTFResult = {
-    nodes: {
-      Cylinder001: THREE.Mesh
-      Cylinder001_1: THREE.Mesh
-    }
-    materials: {
-      NormalTree_Bark: THREE.MeshStandardMaterial
-      NormalTree_Leaves: THREE.MeshStandardMaterial
-    }
-  }
-
-  const assets = Promise.all([
-    useGltf<GLTFResult>('https://fun-bit.vercel.app/Ultimate-Stylized-Nature/NormalTree_1.gltf'),
-    useTexture('https://fun-bit.vercel.app/Ultimate-Stylized-Nature/Textures/NormalTree_Bark.png'),
-    useTexture(
-      'https://fun-bit.vercel.app/Ultimate-Stylized-Nature/Textures/NormalTree_Leaves.png'
-    ),
-    useTexture(
-      'https://fun-bit.vercel.app/Ultimate-Stylized-Nature/Textures/NormalTree_Bark_Normal.png'
-    )
-  ])
 </script>
 
-{#await assets then [$gltf, $texture1, $texture2, $normalMap1]}
-  <InstancedMesh castShadow>
-    <T is={$gltf.nodes.Cylinder001.geometry} />
-    <T.MeshStandardMaterial
-      map={$texture1}
-      map.wrapS={THREE.RepeatWrapping}
-      map.wrapT={THREE.RepeatWrapping}
-      normalMap={$normalMap1}
-      normalMap.wrapS={THREE.RepeatWrapping}
-      normalMap.wrapT={THREE.RepeatWrapping}
-    />
-    {#each transformData as randomValues}
-      {@const x = randomValues[0] - 10}
-      {@const z = randomValues[1] - 10}
-      {@const rot = randomValues[2] * Math.PI * 2}
-      {@const scale = randomValues[3] * 2 + 1}
+<InstancedMesh castShadow>
+  <T.BoxGeometry args={[0.2, 2, 0.2]} />
+  <T.MeshStandardMaterial color="brown" />
+  {#each transformData as randomValues}
+    {@const x = randomValues[0] - 10}
+    {@const z = randomValues[1] - 10}
+    {@const rot = randomValues[2] * Math.PI * 2}
+    {@const scale = randomValues[3] * 2 + 1}
+    <T.Group position.y={scale}>
       <Instance
         position.x={x}
         position.z={z}
         rotation.y={rot}
         {scale}
       />
-    {/each}
-  </InstancedMesh>
-  <InstancedMesh castShadow>
-    <T is={$gltf.nodes.Cylinder001_1.geometry} />
-    <T.MeshStandardMaterial
-      map={$texture2}
-      side={THREE.DoubleSide}
-      alphaTest={0.5}
-    />
-    {#each transformData as randomValues}
-      {@const x = randomValues[0] - 10}
-      {@const z = randomValues[1] - 10}
-      {@const rot = randomValues[2] * Math.PI * 2}
-      {@const scale = randomValues[3] * 2 + 1}
+    </T.Group>
+  {/each}
+</InstancedMesh>
+<InstancedMesh castShadow>
+  <T.SphereGeometry />
+  <T.MeshStandardMaterial color="#033c01" />
+  {#each transformData as randomValues}
+    {@const x = randomValues[0] - 10}
+    {@const z = randomValues[1] - 10}
+    {@const rot = randomValues[2] * Math.PI * 2}
+    {@const scale = randomValues[3] * 2 + 1}
+    <T.Group position.y={scale * 2}>
       <Instance
         position.x={x}
         position.z={z}
         rotation.y={rot}
         {scale}
       />
-    {/each}
-  </InstancedMesh>
-{/await}
+    </T.Group>
+  {/each}
+</InstancedMesh>
